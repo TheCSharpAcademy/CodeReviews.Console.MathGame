a888149dd15fdabbe2360563ad36ad9c27b1f50a
RickySin11 <rajvir11singh@outlook.com>
2023-01-21 14:02:43 -0800

    Create a MathGameHistory class to track all math games.

---
+++ SinghxRaj.MathGame/MathGameHistory.cs
@@ -0,0 +1,13 @@
+﻿namespace SinghxRaj.MathGame.MathGame;
+internal class MathGameHistory 
+{
+    public List<IMathGame> Games { get; set; } = new List<IMathGame>();
+
+    public void ViewHistory()
+    {
+        // TODO
+
+        throw new NotImplementedException();
+    }
+
+}
