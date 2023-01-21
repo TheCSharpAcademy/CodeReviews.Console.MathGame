e73d4dbc7b2c4d8a42773ec5412bc8968f28ef70
RickySin11 <rajvir11singh@outlook.com>
2023-01-21 14:21:48 -0800

    Rename class name from Subtract to SubtractGame.

--- SinghxRaj.MathGame/MathGame/SubtractGame.cs
+++ SinghxRaj.MathGame/MathGame/SubtractGame.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SinghxRaj.MathGame.MathGame;
 
-internal class Subtract : IMathGame
+internal class SubtractGame : IMathGame
 {
     public void PlayGame()
     {
