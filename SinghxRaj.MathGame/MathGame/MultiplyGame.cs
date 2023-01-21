6b7b21b25cef3dbc113ce8144e7f2de86d2aa098
RickySin11 <rajvir11singh@outlook.com>
2023-01-21 14:02:11 -0800

    Create all MathGame classes (Add, Subtract, Multiply, Divide).

---
+++ SinghxRaj.MathGame/MathGame/MultiplyGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinghxRaj.MathGame.MathGame;
+
+internal class MultiplyGame : IMathGame
+{
+    public void PlayGame()
+    {
+        throw new NotImplementedException();
+    }
+}
