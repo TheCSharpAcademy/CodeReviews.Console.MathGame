323f51b0901efedea5d7c7e04e3ac3db06cd2ba1
RickySin11 <rajvir11singh@outlook.com>
2023-01-21 13:59:57 -0800

    Create Project and Create Controller for MathGame

---
+++ SinghxRaj.MathGame/MathGameController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinghxRaj.MathGame;
+
+internal class MathGameController
+{
+    public static void Run()
+    {
+        // TODO
+
+        // Display the application intro
+
+        // Display the menu / let them choose an option
+
+        // Create an instance of that MathGame (add, subtract, multiply, divide)
+
+        // Play the Game
+
+        throw new NotImplementedException();
+    }
+}
