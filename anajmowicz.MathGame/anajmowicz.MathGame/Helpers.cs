eb8912e091d907398a88ce075e77d0272e799024
anajmowicz <dorsha37@gmail.com>
2023-01-31 21:32:38 +0100

    removed empty lines

--- anajmowicz.MathGame/anajmowicz.MathGame/Helpers.cs
+++ anajmowicz.MathGame/anajmowicz.MathGame/Helpers.cs
@@ -1,77 +1,77 @@
 ﻿using anajmowicz.MathGame.Models;
 
-namespace anajmowicz.MathGame
+namespace anajmowicz.MathGame;
+
+internal class Helpers
 {
-    internal class Helpers
+    static List<Game> games = new();
+
+    internal static void AddToHistory(GameType type, int gameScore)
     {
-        static List<Game> games = new();
-
-        internal static void AddToHistory(GameType type, int gameScore)
+        games.Add(new Game
         {
-            games.Add(new Game
-            {
-                Date = DateTime.Now,
-                Score = gameScore,
-                Type = type,
-            });
+            Date = DateTime.Now,
+            Score = gameScore,
+            Type = type,
+        });
+    }
+
+    internal static void PrintGames()
+    {
+        Console.Clear();
+        Console.WriteLine("Games history:");
+        Console.WriteLine("-----------------------");
+
+        foreach (Game game in games)
+        {
+            Console.WriteLine($"{game.Date} - {game.Type}: {game.Score} pts");
         }
 
-        internal static void PrintGames()
+        Console.WriteLine("-----------------------\n");
+        Console.WriteLine("Press any key to return to the main menu");
+        Console.ReadLine();
+    }
+
+    internal static string ValidateResult(string result)
+    {
+        while(string.IsNullOrEmpty(result) || !Int32.TryParse(result,out _))
         {
-            Console.Clear();
-            Console.WriteLine("Games history:");
-            Console.WriteLine("-----------------------");
-
-            foreach (Game game in games)
-            {
-                Console.WriteLine($"{game.Date} - {game.Type}: {game.Score} pts");
-            }
-
-            Console.WriteLine("-----------------------\n");
-            Console.WriteLine("Press any key to return to the main menu");
-            Console.ReadLine();
-        }
-
-        internal static string ValidateResult(string result)
-        {
-            while(string.IsNullOrEmpty(result) || !Int32.TryParse(result,out _))
-            {
-                Console.WriteLine("Your answers has to be an integer. Please enter a valid number.");
-                Console.Write("> ");
-                result = Console.ReadLine();
-            }
-            return result;
-        }
-
-        internal static string ValidateSelection(string selection)
-        {
-            string[] possibleAnswers = {"a", "s", "m", "d", "v", "q"};
-            while(!possibleAnswers.Contains(selection.Trim().ToLower())){
-                Console.WriteLine("Incorrect choice. Try again");
-                Console.Write("> ");
-                selection = Console.ReadLine().Trim().ToLower();
-            }
-            return selection;
-        }
-
-        internal static string GetName()
-        {
-            Console.WriteLine("Welcome to Math Game. What's your name?");
+            Console.WriteLine("Your answers has to be an integer. Please enter a valid number.");
             Console.Write("> ");
-            string name = Console.ReadLine();
-
-            while(string.IsNullOrEmpty(name))
-            {
-                Console.WriteLine("Name can't be empty.");
-                Console.Write("> ");
-                name = Console.ReadLine();
-            }
-            return name;
+            result = Console.ReadLine();
         }
+        return result;
+    }
 
-        internal static void ShowIntro()
+    internal static string ValidateSelection(string selection)
+    {
+        string[] possibleAnswers = {"a", "s", "m", "d", "v", "q"};
+        while(!possibleAnswers.Contains(selection.Trim().ToLower())){
+            Console.WriteLine("Incorrect choice. Try again");
+            Console.Write("> ");
+            selection = Console.ReadLine().Trim().ToLower();
+        }
+        return selection;
+    }
+
+    internal static string GetName()
+    {
+        Console.WriteLine("Welcome to Math Game. What's your name?");
+        Console.Write("> ");
+        string name = Console.ReadLine();
+
+        while(string.IsNullOrEmpty(name))
         {
-            Console.WriteLine(@"
+            Console.WriteLine("Name can't be empty.");
+            Console.Write("> ");
+            name = Console.ReadLine();
+        }
+        return name;
+    }
+
+    internal static void ShowIntro()
+    {
+        Console.WriteLine(@"
                  __  __       _   _        ____                      
                 |  \/  | __ _| |_| |__    / ___| __ _ _ __ ___   ___ 
                 | |\/| |/ _` | __| '_ \  | |  _ / _` | '_ ` _ \ / _ \
@@ -79,7 +79,6 @@ namespace anajmowicz.MathGame
                 |_|  |_|\__,_|\__|_| |_|  \____|\__,_|_| |_| |_|\___|
                                                      
                             Press any key to continue");
-            Console.ReadLine();
-        }
+        Console.ReadLine();
     }
 }
--- anajmowicz.MathGame/anajmowicz.MathGame/Menu.cs
+++ anajmowicz.MathGame/anajmowicz.MathGame/Menu.cs
@@ -1,18 +1,17 @@
 ﻿using anajmowicz.MathGame.Models;
 
-namespace anajmowicz.MathGame
+namespace anajmowicz.MathGame;
+
+internal class Menu
 {
-    internal class Menu
+    GameEngine engine = new GameEngine();
+    internal void ShowMenu(string name, DateTime date)
     {
-        
-        GameEngine engine = new GameEngine();
-        internal void ShowMenu(string name, DateTime date)
+        bool isGameOn = true;
+        do
         {
-            bool isGameOn = true;
-            do
-            {
-                Console.Clear();
-                Console.WriteLine(@$"What game would you like to play today, {name}? Please select from the options below:
+            Console.Clear();
+            Console.WriteLine(@$"What game would you like to play today, {name}? Please select from the options below:
 
     A - Addition
     S - Subtraction
@@ -22,39 +21,37 @@ namespace anajmowicz.MathGame
     V - View previous games
     Q - Quit the game
 ");
-                Console.Write("> ");
+            Console.Write("> ");
 
-                string gameSelected = Helpers.ValidateSelection(Console.ReadLine());
+            string gameSelected = Helpers.ValidateSelection(Console.ReadLine());
 
-                switch (gameSelected.Trim().ToLower())
-                {
-                    case "a":
-                        engine.AdditionGame(GameType.Addition);
-                        break;
-                    case "s":
-                        engine.SubtractionGame(GameType.Subtraction);
-                        break;
-                    case "m":
-                        engine.MultiplicationGame(GameType.Multiplication);
-                        break;
-                    case "d":
-                        engine.DivisionGame(GameType.Division);
-                        break;
-                    case "v":
-                        Helpers.PrintGames();
-                        break;
-                    case "q":
-                        Console.WriteLine("Goodbye!");
-                        isGameOn = false;
-                        Environment.Exit(1);
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input");
-                        Environment.Exit(1);
-                        break;
-                }
-            } while (isGameOn);
-
-        }
+            switch (gameSelected.Trim().ToLower())
+            {
+                case "a":
+                    engine.AdditionGame(GameType.Addition);
+                    break;
+                case "s":
+                    engine.SubtractionGame(GameType.Subtraction);
+                    break;
+                case "m":
+                    engine.MultiplicationGame(GameType.Multiplication);
+                    break;
+                case "d":
+                    engine.DivisionGame(GameType.Division);
+                    break;
+                case "v":
+                    Helpers.PrintGames();
+                    break;
+                case "q":
+                    Console.WriteLine("Goodbye!");
+                    isGameOn = false;
+                    Environment.Exit(1);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    Environment.Exit(1);
+                    break;
+            }
+        } while (isGameOn);
     }
 }
