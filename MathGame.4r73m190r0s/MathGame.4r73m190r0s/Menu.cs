eeccbb3d80cb324cfffb74125df1cf81e4c17ecb
4r73m190r0s <artemidoros.euthymius@protonmail.com>
2023-02-17 17:35:42 +0100

    Finish MathGame Project

---
+++ MathGame.4r73m190r0s/MathGame.4r73m190r0s/Menu.cs
@@ -0,0 +1,53 @@
+﻿namespace MathGame._4r73m190r0s;
+
+internal class Menu
+{
+    internal void ShowMenu(string name, DateTime date)
+    {
+        Console.WriteLine("----------------------");
+        Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}.");
+
+        bool isGameON = true;
+
+        do
+        {
+            Console.WriteLine();
+            Console.WriteLine($@"Choose the game:
+                    A - Addition
+                    B - Subtraction
+                    C - Multiplication
+                    D - Division
+                    V - View Previous Games
+                    Q - Quit");
+            Console.WriteLine("----------------------");
+
+            string gameSelected = Console.ReadLine();
+
+            switch (gameSelected.Trim().ToUpper())
+            {
+                case "A":
+                    GameEngine.AdditionGame();
+                    break;
+                case "B":
+                    GameEngine.SubtractionGame();
+                    break;
+                case "C":
+                    GameEngine.MultiplicationGame();
+                    break;
+                case "D":
+                    GameEngine.DivisionGame();
+                    break;
+                case "V":
+                    Helpers.GetGames();
+                    break;
+                case "Q":
+                    Console.WriteLine("Goodbye");
+                    isGameON = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+        } while (isGameON);
+    }
+}
---
+++ MathGame.4r73m190r0s/MathGame.4r73m190r0s/Program.cs
@@ -0,0 +1,8 @@
+﻿using MathGame._4r73m190r0s;
+
+var menu = new Menu();
+
+string name = Helpers.GetName();
+var date = DateTime.UtcNow;
+
+menu.ShowMenu(name, date);
