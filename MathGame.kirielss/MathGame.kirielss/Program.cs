27f2700d02885a249eb3a0450499025b1772588d
skiriel <gabrieldllopes@gmail.com>
2023-03-31 22:07:39 -0300

    feat: base Program.cs structure

--- MathGame.kirielss/MathGame.kirielss/Program.cs
+++ MathGame.kirielss/MathGame.kirielss/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("WELCOME TO KIRIEL'S MATH GAME");
+            Console.WriteLine();
+
+            Console.WriteLine("CHOOSE YOUR OPERATION");
+            Console.WriteLine();
+
+            Console.WriteLine("1 - ADDITION");
+            Console.WriteLine("2 - SUBTRACTION");
+            Console.WriteLine("3 - MULTIPLICATION");
+            Console.WriteLine("4 - DIVISION");
+            Console.WriteLine("0 - EXIT");
+            Console.WriteLine();
+
+            int selection = int.Parse(Console.ReadLine());
+
+            switch (selection)
+            {
+                case 1:
+                    Console.WriteLine();
+                    break;
+            }
+
         }
     }
 }
