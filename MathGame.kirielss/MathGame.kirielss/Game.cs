a1917db75cee3582ab3fa8811904c9f70dfa77a7
skiriel <gabrieldllopes@gmail.com>
2023-03-31 23:32:59 -0300

    feat: Base Math Game implemented

---
+++ MathGame.kirielss/MathGame.kirielss/Game.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace MathGame.kirielss
+{
+    internal class Game
+    {
+        static public int MAX = 1000;
+
+        public Random A = new Random();
+
+        public Random B = new Random();
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public int Selector { get; private set; }
+
+        public int Result { get; private set; }
+
+        public Game(int selector) {
+            Selector = selector;
+        }
+
+        public void Operation()
+        {
+
+            switch (Selector)
+            {
+                case 1:
+                    X = A.Next(MAX);
+                    Y = B.Next(MAX);
+                    Result = X + Y;
+                    Console.Write($"{X} + {Y} = ");
+                    break;
+
+                case 2:
+                    X = A.Next(MAX);
+                    Y = B.Next(MAX);
+                    Result = X - Y;
+                    Console.Write($"{X} - {Y} = ");
+                    break;
+
+                case 3:
+                    X = A.Next(MAX);
+                    Y = B.Next(100);
+                    Result = X * Y;
+                    Console.Write($"{X} x {Y} = ");
+                    break;
+
+                case 4:
+                    X = A.Next(100);
+                    Y = B.Next(1,100);
+                    Result = X / Y;
+                    Console.Write($"{X} / {Y} = ");
+                    break;
+                    
+            }
+
+        }
+
+
+
+
+    }
+}
--- MathGame.kirielss/MathGame.kirielss/Program.cs
+++ MathGame.kirielss/MathGame.kirielss/Program.cs
@@ -4,28 +4,62 @@
     {
         static void Main(string[] args)
         {
+            bool uptime = true;
+            int score = 0;
+
             Console.WriteLine("WELCOME TO KIRIEL'S MATH GAME");
             Console.WriteLine();
 
-            Console.WriteLine("CHOOSE YOUR OPERATION");
-            Console.WriteLine();
-
-            Console.WriteLine("1 - ADDITION");
-            Console.WriteLine("2 - SUBTRACTION");
-            Console.WriteLine("3 - MULTIPLICATION");
-            Console.WriteLine("4 - DIVISION");
-            Console.WriteLine("0 - EXIT");
-            Console.WriteLine();
-
-            int selection = int.Parse(Console.ReadLine());
-
-            switch (selection)
+            while (uptime)
             {
-                case 1:
-                    Console.WriteLine();
-                    break;
-            }
 
+                Console.WriteLine("CHOOSE YOUR OPERATION");
+                Console.WriteLine();
+
+                Console.WriteLine("1 - ADDITION");
+                Console.WriteLine("2 - SUBTRACTION");
+                Console.WriteLine("3 - MULTIPLICATION");
+                Console.WriteLine("4 - DIVISION");
+                Console.WriteLine("0 - EXIT");
+                Console.WriteLine();
+
+                int selector = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                if (selector >= 1 && selector <= 4)
+                {
+                    Game game = new Game(selector);
+                    game.Operation();
+                    int answer = int.Parse(Console.ReadLine());
+                    if (answer == game.Result)
+                    {
+                        Console.WriteLine("CORRECT! YOU ROCK!!!");
+                        score++;
+                        Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine("CURRENT SCORE: " + score);
+                        Console.WriteLine();
+                    } else
+                    {
+                        Console.WriteLine("TOO BAD! THE ANSWER IS " + game.Result);
+                        Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine("CURRENT SCORE: " + score);
+                        Console.WriteLine();
+                    }
+
+                }
+                else if (selector != 0)
+                {
+                    Console.WriteLine("ERROR! PLEASE INSERT A VALID NUMBER BETWEEN 1 - 4, OR 0 TO EXIT");
+                }
+                else
+                {
+                    Console.WriteLine("THANK YOU FOR PLAYING!");
+                    uptime = false;
+                }
+
+            }
         }
     }
 }
