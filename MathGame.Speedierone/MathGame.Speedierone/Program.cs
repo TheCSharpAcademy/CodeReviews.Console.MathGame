9c41c42af9b06e74a42a18af335e63a72562dca8
simon davies <daviessimon21@gmail.com>
2023-04-08 02:02:31 +0100

    Final 2

---
+++ MathGame.Speedierone/MathGame.Speedierone/Program.cs
@@ -0,0 +1,16 @@
+﻿using Math_Game;
+var menu = new Menu();
+
+var date = DateTime.UtcNow;
+
+var games = new List<string>();
+
+string name = GetName();
+
+menu.ShowMenu(name, date);
+static string GetName()
+{
+    Console.WriteLine("Please type your name");
+    var name = Console.ReadLine();
+    return name;
+}
