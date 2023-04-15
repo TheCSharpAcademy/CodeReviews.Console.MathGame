66cb33f39610a8848b7d4684d2573a0ab8b40f9c
barakisbrown <lokislayer@gmail.com>
2023-04-15 18:04:38 -0500

    Added Model and way to display its properties.

---
+++ MathGame.barakisbrown/MathGame.barakisbrown/Model.cs
@@ -0,0 +1,30 @@
+﻿namespace MathGame.barakisbrown;
+
+internal class Model
+{
+    public int Score { get; set; }
+    public GameType Type { get; set; }
+    public DateTime Date { get; set; }
+    public Diffuclty_Levels Levels { get; set; }
+
+    public override string ToString()
+    {
+        return $"Game: {Type,-20} Score: {Score,-10} Date {Date,-15:f} \t Diffculty Level {Levels, -20} ";
+    }
+}
+
+
+internal enum GameType
+{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division
+}
+
+internal enum Diffuclty_Levels
+{
+    Beginnger,
+    Moderate,
+    Expert
+}
