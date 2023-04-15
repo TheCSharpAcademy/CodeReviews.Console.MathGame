46f26a60fc65a9f1b4109a0e564790490d4e0eef
barakisbrown <lokislayer@gmail.com>
2023-04-15 18:11:48 -0500

    Helpers Class for useful utility functions that are static

---
+++ MathGame.barakisbrown/MathGame.barakisbrown/Helpers.cs
@@ -0,0 +1,15 @@
+﻿namespace MathGame.barakisbrown;
+
+internal class Helpers
+{
+    internal static string ValidateResults(string result, string prompt)
+    {
+        while (string.IsNullOrEmpty(result) || !Int32.TryParse(result, out _))
+        {
+            Console.WriteLine("Your answer needs to be an integer. Try Again.");
+            Console.Write(prompt);
+            result = Console.ReadLine();
+        }
+        return result;
+    }
+}
