bb19f0ddaeeab61dfda44ad70936419e094904b0
Ian McNerlin <IanMcNerlin@hotmail.co.uk>
2023-04-16 14:07:59 +0100

    Update Program.cs

--- MathGame.IanMcNerlin/MathGame.IanMcNerlin/Program.cs
+++ MathGame.IanMcNerlin/MathGame.IanMcNerlin/Program.cs
@@ -39,33 +39,33 @@ while (menuIn != 0)
 
         case 1:
             Console.WriteLine("You have chosen 1 - addition from the menu");
-            decimal addValue1 = getInput();
-            decimal addValue2 = getInput();
-            decimal resultAdd = addition(addValue1, addValue2);
+            decimal addValue1 = GetInput();
+            decimal addValue2 = GetInput();
+            decimal resultAdd = Addition(addValue1, addValue2);
             listResults.Add(resultAdd);
             Console.WriteLine($"The result is:{resultAdd}");
             break;
         case 2:
             Console.WriteLine("You have chosen 2 - subtraction from the menu");
-            decimal subValue1 = getInput();
-            decimal subValue2 = getInput();
-            decimal resultSub = subtraction(subValue1, subValue2);
+            decimal subValue1 = GetInput();
+            decimal subValue2 = GetInput();
+            decimal resultSub = Subtraction(subValue1, subValue2);
             listResults.Add(resultSub);
             Console.WriteLine($"The result is:{resultSub}");
             break;
         case 3:
             Console.WriteLine("You have chosen 3 - multiplication from the menu");
-            decimal multValue1 = getInput();
-            decimal multValue2 = getInput();
-            decimal resultMult = multiplication(multValue1, multValue2);
+            decimal multValue1 = GetInput();
+            decimal multValue2 = GetInput();
+            decimal resultMult = Multiplication(multValue1, multValue2);
             listResults.Add(resultMult);
             Console.WriteLine($"The result is:{resultMult}");
             break;
         case 4:
             Console.WriteLine("You have chosen 4 - division from the menu");
-            decimal divValue1 = getInput();
-            decimal divValue2 = getInput();
-            decimal resultDiv = Math.Round(division(divValue1, divValue2), 3);
+            decimal divValue1 = GetInput();
+            decimal divValue2 = GetInput();
+            decimal resultDiv = Math.Round(Division(divValue1, divValue2), 3);
             listResults.Add(resultDiv);
             Console.WriteLine($"The result is:{resultDiv}");
             break;
@@ -93,7 +93,7 @@ while (menuIn != 0)
 Console.WriteLine("You have chosen 0 - exit the Maths Game");
 Console.WriteLine("The game has now ended.");
 
-static decimal getInput()
+static decimal GetInput()
 {
     Console.WriteLine("Enter in a number:");
     var isNumber = decimal.TryParse(Console.ReadLine(), out decimal n);
@@ -108,25 +108,25 @@ static decimal getInput()
     Console.WriteLine($"You have entered in a value of :{output}");
     return output;
 }
-static decimal addition(decimal value1, decimal value2)
+static decimal Addition(decimal value1, decimal value2)
 {
     decimal output = value1 + value2;
     return output;
 }
 
-static decimal subtraction(decimal value1, decimal value2)
+static decimal Subtraction(decimal value1, decimal value2)
 {
     decimal output = value1 - value2;
     return output;
 }
 
-static decimal division(decimal value1, decimal value2)
+static decimal Division(decimal value1, decimal value2)
 {
     decimal output = value1 / value2;
     return output;
 }
 
-static decimal multiplication(decimal value1, decimal value2)
+static decimal Multiplication(decimal value1, decimal value2)
 {
     decimal output = value1 * value2;
     return output;
