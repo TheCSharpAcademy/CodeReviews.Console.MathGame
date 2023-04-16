82706305c58a5ad2fa9e0edd66ed27b45fa95a3a
barakisbrown <lokislayer@gmail.com>
2023-04-16 17:52:47 -0500

    Game Completed

--- MathGame.barakisbrown/MathGame.barakisbrown/Problems/Addition.cs
+++ MathGame.barakisbrown/MathGame.barakisbrown/Problems/Addition.cs
@@ -6,7 +6,51 @@ internal class Addition : IProblem
 
     Model IProblem.Calc(Diffuclty_Levels levels)
     {
-        Console.WriteLine("Doing Additional Problem");
-        return null;
+        int score = 0;
+        bool quit = false;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the Addition Game");          
+            int left  = _random.Next(0, (int)levels + 1);
+            int right = _random.Next(0, (int)levels + 1);
+            int actualAnswer = left + right;
+            var problem = string.Format("{0} + {1}", left, right);
+            
+            Console.WriteLine(problem);
+            Console.Write("Guess => ");
+            
+            var guess = Console.ReadLine();
+            guess = Helpers.ValidateResults(guess, "Guess => ");
+            int userAnswer = Helpers.ConvertToInt(guess);
+
+            bool correct = (userAnswer == actualAnswer);
+
+            if (correct)
+            {
+                score++;
+                Console.WriteLine("Congrats. You guess the right answer");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, Your answer was incorrect. The actualy answer was {actualAnswer} ");
+            }
+
+            Console.Write("Play Again (Y/N)");
+
+            char key = Console.ReadKey(true).KeyChar;
+
+            if (key == 'n' || key == 'N')
+                quit = true;
+
+        } while (!quit);
+
+        return new Model
+        {
+            Type = GameType.Addition,
+            Levels = levels,
+            Score = score,
+            Date = DateTime.Now
+        };
     }
 }
--- MathGame.barakisbrown/MathGame.barakisbrown/Problems/Division.cs
+++ MathGame.barakisbrown/MathGame.barakisbrown/Problems/Division.cs
@@ -5,7 +5,51 @@ internal class Division : IProblem
     private readonly Random _random = new();
     Model IProblem.Calc(Diffuclty_Levels levels)
     {
-        Console.WriteLine("Division Problem");
-        return null;
+        int score = 0;
+        bool quit = false;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the Addition Game");
+            int left = _random.Next(0, (int)levels + 1);
+            int right = _random.Next(0, 101);
+            int actualAnswer = left / right;
+            var problem = string.Format("{0} / {1}", left, right);
+
+            Console.WriteLine(problem);
+            Console.Write("Guess => ");
+
+            var guess = Console.ReadLine();
+            guess = Helpers.ValidateResults(guess, "Guess => ");
+            int userAnswer = Helpers.ConvertToInt(guess);
+
+            bool correct = (userAnswer == actualAnswer);
+
+            if (correct)
+            {
+                score++;
+                Console.WriteLine("Congrats. You guess the right answer");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, Your answer was incorrect. The actualy answer was {actualAnswer} ");
+            }
+
+            Console.Write("Play Again (Y/N)");
+
+            char key = Console.ReadKey(true).KeyChar;
+
+            if (key == 'n' || key == 'N')
+                quit = true;
+
+        } while (!quit);
+
+        return new Model
+        {
+            Type = GameType.Division,
+            Levels = levels,
+            Score = score,
+            Date = DateTime.Now
+        };
     }
 }
