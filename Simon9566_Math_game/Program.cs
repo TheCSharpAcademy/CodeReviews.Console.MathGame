a7cd05f93d4f3bce9ff7070687287f4cbf490b34
Simon9566 <123582058+Simon9566@users.noreply.github.com>
2023-04-20 21:13:30 +0200

    Looks, fine still need testing

--- Simon9566_Math_game/Program.cs
+++ Simon9566_Math_game/Program.cs
@@ -5,20 +5,22 @@ string player_name = Console.ReadLine();
 var date = DateTime.Now;
 Console.Clear();
 
-// Main menu
-
 Console.WriteLine($"Hello {player_name}, {date} is a good time to practice your mathematical skills. Are you ready?");
-Console.WriteLine();
-Console.WriteLine("Select your desired difficulty level");
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("E - Easy, addition only");
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine("M - Medium, addition and subtraction");
-Console.ForegroundColor = ConsoleColor.DarkYellow;
-Console.WriteLine("H - Hard, addition subtraction and multiplication");
-Console.ForegroundColor = ConsoleColor.Red;
-Console.WriteLine("S- Superb,  addition has been replaced with division, all results are to be given with 2 decimal spaces, even if those are zeroes :)");
-Console.ResetColor();
+// Main menu
+void Print_menu()
+{
+    Console.WriteLine();
+    Console.WriteLine("Select your desired difficulty level");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("E - Easy, addition only");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("M - Medium, addition and subtraction");
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("H - Hard, addition subtraction and multiplication");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("S- Superb,  addition has been replaced with division, all results are to be given with 2 decimal spaces, even if those are zeroes :)");
+    Console.ResetColor();
+}
 
 // Getting the difficulty and constructing main play loops based on them
 
@@ -26,12 +28,9 @@ string difficulty = "tmp";
 var difficulties = new Dictionary<string, string>();
 difficulties.Add("E", "easy");
 difficulties.Add("M", "medium");
-difficulties.Add("H", "hard");
+difficulties.Add("D", "Difficult");
 difficulties.Add("S", "special");
-while (difficulties.ContainsKey(difficulty) == false)
-{
-    difficulty = Console.ReadLine().ToUpper();
-}
+
 
 int a = 0;
 int b = 0;
@@ -55,17 +54,16 @@ void Ask_continue()
     }
 }
 
-void Get_large()
+int Get_large()
 {
     int tmp = random.Next(100, 999);
-    return tmp
+    return tmp;
 }
 
-void Get_small()
+int Get_small()
 {
         int tmp = random.Next(3, 100);
-    return tmp
-    }
+    return tmp;
 }
 
 void Validate_guess()
@@ -99,8 +97,8 @@ void Ask_subtraction()
 {
     a = 0;
     b = 0;
-    while a <= b
-     {
+    while (a <= b)
+    { 
         a = Get_large();
         b = Get_large();
     }
@@ -126,7 +124,7 @@ void Ask_multiplication()
 {
     a = Get_small();
     b = Get_small();
-    result = $"{a * b}"
+    result = $"{a * b}";
     Console.WriteLine($"What is {a} * {b}?");
     Validate_guess(); 
     if (guess == a * b)
@@ -147,7 +145,7 @@ void Ask_division()
 {
     a = Get_large();
     b = Get_small();
-    result =  $"{a / b}"
+    result = $"{a / b}";
     Console.WriteLine($"What is {a} / {b}?");
     Validate_guess();
     if (guess == a/b)
@@ -167,13 +165,18 @@ void Ask_division()
 
 while (playing)
 {
+    Print_menu();
+    while (difficulties.ContainsKey(difficulty) == false)
+    {
+        difficulty = Console.ReadLine().ToUpper();
+    }
+
     if (difficulties[difficulty] == "easy")
     {
         for (int i = 0; i < 10; i++)
         {
             Ask_addition();
         }
-        Ask_continue();
     }
     else if (difficulties[difficulty] == "medium")
     {
@@ -184,7 +187,6 @@ while (playing)
             {
                 Ask_addition();
             }
-            Ask_continue();
         }
         else
         {
@@ -192,7 +194,6 @@ while (playing)
             {
                 Ask_subtraction();
             }
-            Ask_continue();
         }
     }
     else if (difficulties[difficulty] == "hard")
@@ -204,7 +205,6 @@ while (playing)
             {
                 Ask_addition();
             }
-            Ask_continue();
         }
         else if (operation == 2)
         {
@@ -212,7 +212,6 @@ while (playing)
             {
                 Ask_subtraction();
             }
-            Ask_continue();
         }
         else
         {
@@ -220,7 +219,6 @@ while (playing)
             {
                 Ask_multiplication();
             }
-            Ask_continue();
         }
     }
     else
@@ -232,7 +230,6 @@ while (playing)
             {
                 Ask_division();
             }
-            Ask_continue();
         }
         else if (operation == 2)
         {
@@ -240,7 +237,6 @@ while (playing)
             {
                 Ask_subtraction();
             }
-            Ask_continue();
         }
         else
         {
@@ -248,7 +244,10 @@ while (playing)
             {
                 Ask_multiplication();
             }
-            Ask_continue();
         }
     }
+    Console.WriteLine("What do you want to do now,\n H -  acces history");
+    Ask_continue();
+    difficulties.Add("H", "history");
+    Print_menu();
 }
