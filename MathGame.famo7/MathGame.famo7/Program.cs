91f5291d42d1fbf91be1cca06bc7d043d884c37f
famo1901 <famo1901@gmail.com>
2023-04-26 12:14:12 +0200

    Clear console and fixed divistion issue.

--- MathGame.famo7/MathGame.famo7/Program.cs
+++ MathGame.famo7/MathGame.famo7/Program.cs
@@ -3,12 +3,15 @@ var game = new Game();
 
 while (true)
 {
+
     Menu();
     char gametype = Console.ReadLine()[0];
+
     if (gametype == 'Q') break;
     if (gametype == 'V')
     {
         game.ViewHistory();
+        KeyPress();
         continue;
     }
     ParentGame gameChoice = gametype switch
@@ -33,19 +36,33 @@ while (true)
                 {
                     Console.WriteLine("Correct");
                     gameChoice.Point++;
+                    KeyPress();
                 }
-                else Console.WriteLine("Wrong");
+                else
+                {
+                    Console.WriteLine("Wrong");
+                    KeyPress();
+                }
                 continue;
+
             case 'S':
+                Console.Clear();
                 Console.WriteLine($"{a} - {b}");
                 answer = int.Parse(Console.ReadLine());
+
                 if (CheckAnswer(answer, a - b))
                 {
                     Console.WriteLine("Correct");
                     gameChoice.Point++;
+                    KeyPress();
                 }
-                else Console.WriteLine("Wrong");
+                else
+                {
+                    Console.WriteLine("Wrong");
+                    KeyPress();
+                }
                 continue;
+
             case 'M':
                 Console.WriteLine($"{a} * {b}");
                 answer = int.Parse(Console.ReadLine());
@@ -53,24 +70,39 @@ while (true)
                 {
                     Console.WriteLine("Correct");
                     gameChoice.Point++;
+                    KeyPress();
                 }
-                else Console.WriteLine("Wrong");
+                else
+                {
+                    Console.WriteLine("Wrong");
+                    KeyPress();
+                }
                 continue;
+
             case 'D':
+                a = new Random().Next(0, 101);
                 Console.WriteLine($"{a} / {b}");
                 answer = int.Parse(Console.ReadLine());
                 if (CheckAnswer(answer, a / b))
                 {
                     Console.WriteLine("Correct");
                     gameChoice.Point++;
+                    KeyPress();
                 }
-                else Console.WriteLine("Wrong");
+                else
+                {
+                    Console.WriteLine("Wrong");
+                    KeyPress();
+                }
                 continue;
+
             default:
                 break;
         }
     }
+    Console.Clear();
     game.AddHistory(gameChoice);
+
 
 }
 
@@ -84,7 +116,13 @@ void Menu()
     Console.WriteLine("M - Multiplication");
     Console.WriteLine("D - Division");
     Console.WriteLine("Q - Quit the program");
+}
 
+void KeyPress()
+{
+    Console.WriteLine("Press any Key to Continue");
+    Console.ReadKey();
+    Console.Clear();
 }
 
 Tuple<int, int> GetValues()
