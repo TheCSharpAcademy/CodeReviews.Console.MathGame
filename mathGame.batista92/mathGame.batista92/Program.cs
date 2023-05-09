d0ebc69cb8a7a511dd7e13070963ada802f202f0
Bruno Batista <bruno.2.s@live.com>
2023-05-09 00:55:30 -0300

    Update program.cs
    Implementing the main menu

--- mathGame.batista92/mathGame.batista92/Program.cs
+++ mathGame.batista92/mathGame.batista92/Program.cs
@@ -4,6 +4,30 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Clear();
+        Console.WriteLine("Please type your name");
+
+        string name = Console.ReadLine();
+        DateTime date = DateTime.UtcNow;
+
+        Console.WriteLine("---------------------------------------------------");
+        Console.WriteLine($"Hello {name.ToUpper()}. It's {date}. This is your math's game. That's great that you're working on improving yourself");
+        Console.WriteLine();
+        Console.WriteLine($@"What game would you like to play today? Choose from the options below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+        Console.WriteLine("---------------------------------------------------");
+
+        string gameSelected = Console.ReadLine();
+        switch (gameSelected.Trim())
+        {
+            case "a":
+                Console.WriteLine("Addition Game Selected");
+                break;
+        }
+
     }
 }
