7edde35b1ef0cc286ce06e679d3b68ec941b1ba4
AlainNshimirimana <alain.nshimirimana@gmail.com>
2023-05-09 20:37:56 -0700

    built math game's menu

--- MathGame.AlainNshimirimana/Program.cs
+++ MathGame.AlainNshimirimana/Program.cs
@@ -1,2 +1,16 @@
-﻿Console.WriteLine("hello world");
+﻿Console.WriteLine("Please type your name");
+
+var name = Console.ReadLine();
+var date = DateTime.Now;
+
+Console.WriteLine("-----------------------------");
+Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. This is your math's game. That's great that you're working on improving yourself\n");
+Console.WriteLine($@"What game would you like to play today? Choose from the options below:
+A - Addition 
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+Console.WriteLine("-----------------------------");
+
 Console.ReadLine();
