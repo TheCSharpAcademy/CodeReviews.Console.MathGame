9905cdc81f95d5dc863de579144ee37fa0f1598c
AlainNshimirimana <alain.nshimirimana@gmail.com>
2023-05-14 16:08:07 -0700

    for loop and random numbers

--- MathGame.AlainNshimirimana/Program.cs
+++ MathGame.AlainNshimirimana/Program.cs
@@ -52,6 +52,8 @@ void AdditionGame(string message)
     Console.WriteLine(message);
 
     var random = new Random();
+    var score = 0;
+
     int firstNumber;
     int secondNumber;
 
@@ -66,23 +68,79 @@ void AdditionGame(string message)
         if (int.Parse(result) == firstNumber + secondNumber)
         {
             Console.WriteLine("Your answer is correct!");
+            score++;
         }
         else
         {
             Console.WriteLine("Your answer is incorrect");
         }
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}");
     }
 }
 void SubtractionGame(string message)
 {
     Console.WriteLine(message);
+
+    var random = new Random();
+    var score = 0;
+
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+        Console.WriteLine($"{firstNumber} - {secondNumber}");
+
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber - secondNumber)
+        {
+            Console.WriteLine("Your answer is correct!");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Your answer is incorrect");
+        }
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}");
+    }
 }
+
 void MultiplicationGame(string message)
 {
     Console.WriteLine(message);
+
+    var random = new Random();
+    var score = 0;
+
+    int firstNumber;
+    int secondNumber;
+
+    for (int i = 0; i < 5; i++)
+    {
+        firstNumber = random.Next(1, 9);
+        secondNumber = random.Next(1, 9);
+        Console.WriteLine($"{firstNumber} * {secondNumber}");
+
+        var result = Console.ReadLine();
+
+        if (int.Parse(result) == firstNumber * secondNumber)
+        {
+            Console.WriteLine("Your answer is correct!");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Your answer is incorrect");
+        }
+        if (i == 4) Console.WriteLine($"Game over. Your final score is {score}");
+    }
 }
 
 void DivisionGame(string message)
 {
     Console.WriteLine(message);
+
 }
