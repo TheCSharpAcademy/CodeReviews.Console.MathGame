aa18da289a65f2727fecc10ce8adc70bbf7e6dbc
CWolffles <ruben.e.gimon@gmail.com>
2023-05-16 16:28:22 -0400

    Started GameLog class.

---
+++ MathGame.Wolffles/GameLog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Math_Game;
+
+internal class GameLog
+{
+    enum GameType
+    {
+        addition,
+        substraction,
+        multiplication,
+        division,
+        total_gameTypes
+    }
+    struct History
+    {
+        int m_id;
+        DateTime m_date;
+        GameType m_gameType;
+    }
+
+    
+}
