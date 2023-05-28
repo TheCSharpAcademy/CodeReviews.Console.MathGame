7479c077cce436f40be9e8163c6455204df3f423
Vincenty Armstrong <vincenty.armstrong@icloud.com>
2023-05-28 20:32:47 +0100

    Game completed

---
+++ MathGame.VincentyArmstrong/MathGame.VincentyArmstrong/Game.cs
@@ -0,0 +1,80 @@
+﻿namespace MathGame.VincentyArmstrong;
+
+public class Game
+{
+    private int result;
+
+    public GameType Type { get; }
+    public int Questions { get; }
+    public bool HardMode { get; }
+    public int Score { get; private set; }
+
+    public Game(GameType type, int questions, bool hardMode)
+    {
+        Questions = questions;
+        HardMode = hardMode;
+        Type = type;
+        Play();
+    }
+
+    private void Play()
+    {
+        int firstNumber;
+        int secondNumber;
+        for (int i = 0; i < Questions; i++)
+        {
+            Random random = new();
+            if (HardMode)
+            {
+                firstNumber = random.Next(1, 999);
+                secondNumber = random.Next(1, 999);
+            }
+            else
+            {
+                firstNumber = random.Next(1, 99);
+                secondNumber = random.Next(1, 99);
+            }
+
+            Console.Clear();
+            Console.WriteLine($"Round {i + 1}");
+            Console.WriteLine("Calculate:");
+
+            if (Type.Equals(GameType.Addition))
+            {
+                result = firstNumber + secondNumber;
+                Console.WriteLine($"{firstNumber} + {secondNumber}");
+            }
+            if (Type.Equals(GameType.Subtraction))
+            {
+                result = firstNumber - secondNumber;
+                Console.WriteLine($"{firstNumber} - {secondNumber}");
+            }
+            if (Type.Equals(GameType.Multiplication))
+            {
+                result = firstNumber * secondNumber;
+                Console.WriteLine($"{firstNumber} x {secondNumber}");
+            }
+            if (Type.Equals(GameType.Division))
+            {
+                int[] divisors = Utilities.GetDevisors(firstNumber);
+                secondNumber = divisors[random.Next(divisors.Length)];
+                result = firstNumber / secondNumber;
+                Console.WriteLine($"{firstNumber} ÷ {secondNumber}");
+            }
+
+            bool parsed;
+            int userResult;
+            do
+            {
+                parsed = int.TryParse(Console.ReadLine(), out userResult);
+                if (!parsed) Console.WriteLine("Please enter a number");
+            } while (!parsed);
+            if (userResult == result) Score++;
+        }
+        Console.WriteLine($"Game finished. Your final score is {Score} out of {Questions}");
+        Console.WriteLine($"Press any key to return to Main Menu");
+        Console.ReadLine();
+    }
+}
+
+public enum GameType { Addition, Subtraction, Multiplication, Division };
--- MathGame.VincentyArmstrong/MathGame.VincentyArmstrong/Program.cs
+++ MathGame.VincentyArmstrong/MathGame.VincentyArmstrong/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MathGame.VincentyArmstrong;
+
+UserInterface userInterface = new();
