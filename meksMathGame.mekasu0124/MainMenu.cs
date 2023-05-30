6dcdecae9dddd96de02332975af141a33f8049b3
Mekasu <96821081+mekasu0124@users.noreply.github.com>
2023-05-30 03:33:57 -0500

    uploading files;

---
+++ meksMathGame.mekasu0124/MainMenu.cs
@@ -0,0 +1,188 @@
+﻿using MeksMathGame;
+
+class MainMenu
+{
+    public int tries = 3;
+
+    public void InputInvalid(string error)
+    {
+        if (tries > 0)
+        {
+            tries--;
+            Console.WriteLine("\n" + error + " " + tries + " Tries Left.");
+        }
+        else
+        {
+            Console.WriteLine("\nMaximum Number Attempts Reached. Exiting Program.");
+            Thread.Sleep(2000);
+            Environment.Exit(0);
+        }
+    }
+
+    public void OperationInvalid(string error)
+    {
+        if (tries > 0)
+        {
+            tries--;
+            Console.WriteLine("\n" + error + " " + tries + " Tries Left.");
+        }
+        else
+        {
+            Console.WriteLine("\nMaximum Number Attempts Reached. Exiting Program.");
+            Thread.Sleep(2000);
+            Environment.Exit(0);
+        }
+    }
+
+    public void DifficultySelection()
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+
+        List<string> gameDiffs = new()
+        {
+            "easy",
+            "medium",
+            "hard"
+        };
+
+        foreach (string text in gameDiffs)
+        {
+            Console.WriteLine(text);
+        };
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("\nPlease Select A Difficulty: ");
+
+        string userSelectedDiff = "";
+
+        for (int i = 0; i < 3; i++)
+        {
+            userSelectedDiff = Console.ReadLine();
+
+            if (gameDiffs.Contains(userSelectedDiff))
+            {
+                break;
+            }
+            else
+            {
+                InputInvalid("Enter Either easy, medium, or hard");
+            }
+        }
+
+        Console.WriteLine("\nDifficulty: " + userSelectedDiff);
+        Thread.Sleep(2000);
+
+        GameSelection(userSelectedDiff);
+    }
+
+    public void GameSelection(string difficulty)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("\n");
+
+        List<string> gameTypes = new()
+        {
+            "Addition",
+            "Subtraction",
+            "Multiplication",
+            "Division"
+        };
+
+        foreach (string text in gameTypes)
+        {
+            Console.WriteLine(text);
+        };
+
+        Console.Write("\nPlease Select A Game To Play: ");
+
+        string userSelectedGame = "";
+
+        for (int i = 0; i < 3; i++)
+        {
+            userSelectedGame = Console.ReadLine();
+
+            if (gameTypes.Contains(userSelectedGame, StringComparer.OrdinalIgnoreCase))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Enter A Game Choice: addition, subtraction, multiplication" +
+                    " Or division.");
+            }
+        }
+
+        Console.WriteLine("\nDifficulty: " + difficulty + "\nGame: " + userSelectedGame);
+        Thread.Sleep(2000);
+
+        GetDesiredQuestions(difficulty, userSelectedGame.ToLower());
+    }
+
+    public void GetDesiredQuestions(string difficulty, string userSelectedGame)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("\nPlease Enter The Amount Of Questions You'd Like To Answer (1-100): ");
+
+        int numOfQuestions = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (!int.TryParse(Console.ReadLine(), out numOfQuestions))
+            {
+                Console.WriteLine("Enter An Integer, or Whole Number For Your Choice.");
+            }
+            else if (numOfQuestions < 1 || numOfQuestions > 100)
+            {
+                Console.WriteLine("Enter A Number Between 1 and 100");
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("\nDifficulty: " + difficulty + "\nGame: " + userSelectedGame + "\nQuestions: " + numOfQuestions);
+        Thread.Sleep(2000);
+        LaunchGameScreen(difficulty, userSelectedGame, numOfQuestions);
+    }
+
+    public void LaunchGameScreen(string difficulty, string game, int numOfQuest)
+    {
+        switch (game)
+        {
+            case "addition":
+                Console.WriteLine("\nLaunching Addition Game.");
+                Thread.Sleep(2000);
+                Console.Clear();
+
+                AdditionGame addGame = new();
+                addGame.StartGame(difficulty, numOfQuest);
+                break;
+
+            case "subtraction":
+                Console.WriteLine("\nLaunching Subtraction Game.");
+                Thread.Sleep(2000);
+                Console.Clear();
+                break;
+
+            case "multiplication":
+                Console.WriteLine("\nLaunching Multiplication Game.");
+                Thread.Sleep(2000);
+                Console.Clear();
+                break;
+
+            case "division":
+                Console.WriteLine("\nLaunching Division Game.");
+                Thread.Sleep(2000);
+                Console.Clear();
+                break;
+
+            default:
+                for (int i = 0; i < 3; i++)
+                {
+                    OperationInvalid("Operation Failed To Launch Game. Trying Again.");
+                }
+                break;
+        }
+    }
+}
---
+++ meksMathGame.mekasu0124/PreviousHistory.cs
@@ -0,0 +1,89 @@
+﻿public static class PreviousHistory
+{
+    public static int gameCount = 0;
+
+    public static List<string> addGames = new();
+    public static List<string> subGames = new();
+    public static List<string> mulGames = new();
+    public static List<string> divGames = new();
+
+    public static void PrintHistory()
+    {
+        List<string> history = new();
+
+        history.Add("\nAddition Games");
+        history.AddRange(addGames);
+        history.Add("\nSubtraction Games");
+        history.AddRange(subGames);
+        history.Add("\nMultiplication Games");
+        history.AddRange(mulGames);
+        history.Add("\nDivision Games");
+        history.AddRange(divGames);
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Your Previous Games");
+
+        for (int index = 0; index < history.Count; index++)
+        {
+            Console.WriteLine(history[index]);
+        };
+
+        Console.WriteLine("\nWould You Like To Play Again or Exit Program?");
+
+        string playCheck = "";
+
+        for (int i = 0; i < 3; i++)
+        {
+            playCheck = Console.ReadLine();
+
+            if (playCheck.ToLower() == "play again")
+            {
+                break;
+            }
+            else if (playCheck.ToLower() == "exit program")
+            {
+                Console.WriteLine("Exiting Program. Please Wait. . .");
+                Thread.Sleep(2000);
+                Environment.Exit(0);
+            }    
+        };
+
+        Console.WriteLine("Launching Main Menu. Please Wait. . .");
+        Thread.Sleep(2000);
+        Console.Clear();
+
+        MainMenu mainMenu = new();
+        mainMenu.DifficultySelection();
+    }
+
+    public static void AddHistory(int num1, string operatorSymbol, int num2, int solution, bool rightWrong)
+    {
+        gameCount++;
+        string writeString = $"Game {gameCount}: {num1} {operatorSymbol} {num2} = {solution} -> solution {(rightWrong ? "correct" : "wrong")}";
+
+        switch (operatorSymbol)
+        {
+            case "+":
+                addGames.Add(writeString);
+                break;
+
+            case "-":
+                subGames.Add(writeString);
+                break;
+
+            case "*":
+                mulGames.Add(writeString);
+                break;
+
+            case "/":
+                divGames.Add(writeString);
+                break;
+
+            default:
+                Console.WriteLine("Invalid Operation Writing Game History.");
+                Thread.Sleep(2000);
+                Environment.Exit(0);
+                break;
+        }
+    }
+}
