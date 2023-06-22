9dba08c086dd318214614bd09ba1cc7d40a7d2c2
Moisés Macedo <mo3ses404@gmail.com>
2023-06-22 14:07:06 -0300

    met 3 of the 5 challenge requirements

--- MathGame.Mo3ses/Program.cs
+++ MathGame.Mo3ses/Program.cs
@@ -1,2 +1,72 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Math-Game");
+
+bool PlayGame = true;
+
+while (PlayGame)
+{
+
+    Random random = new Random();
+
+    int number1 = random.Next(0, 100);
+    int number2 = random.Next(1, 100);
+    int result = 0;
+    int answerMenu = 0;
+    int answer = 0;
+
+    string actualOperation;
+
+    Console.WriteLine("------------------ Menu ------------------");
+    Console.WriteLine("1 - Addition");
+    Console.WriteLine("2 - Subtraction");
+    Console.WriteLine("3 - Multiplication");
+    Console.WriteLine("4 - Division");
+    Console.WriteLine("5 - Exit");
+    Console.WriteLine("------------------------------------------");
+
+
+
+    answerMenu = Convert.ToInt32(System.Console.ReadLine());
+
+
+
+    switch (answerMenu)
+    {
+        case 1:
+            actualOperation = $"{number1} + {number2}";
+            System.Console.WriteLine(actualOperation);
+            answer = Convert.ToInt32(System.Console.ReadLine());
+            result = number1 + number2;
+            break;
+        case 2:
+            actualOperation = $"{number1} - {number2}";
+            System.Console.WriteLine(actualOperation);
+            answer = Convert.ToInt32(System.Console.ReadLine());
+            result = number1 - number2;
+            break;
+        case 3:
+            actualOperation = $"{number1} * {number2}";
+            System.Console.WriteLine(actualOperation);
+            answer = Convert.ToInt32(System.Console.ReadLine());
+            result = number1 * number2;
+            break;
+        case 4:
+            actualOperation = $"{number1} / {number2}";
+            System.Console.WriteLine(actualOperation);
+            answer = Convert.ToInt32(System.Console.ReadLine());
+            result = number1 / number2;
+            break;
+        case 5:
+            PlayGame = false;
+            break;
+        default:
+            System.Console.WriteLine("Invalid Option, Try Again.");
+            break;
+    }
+
+    if (result == answer)   {
+        System.Console.WriteLine("Correct Answer");
+    } else {
+        System.Console.WriteLine($"Incorrect Answer Correct Value = {result}");
+    }
+}
--- MathGame.Mo3ses/README.md
+++ MathGame.Mo3ses/README.md
@@ -4,9 +4,9 @@
 In your first project you’ll build a very simple Math Game app. If this is your first time building a project on your own, I suggest you use the C# Academy's series from our C# Foundation area, where we walk you through how to create a console app from scratch, while explaining important concepts in C#. If you have already watched it, or created applications before, this will be a breeze. But it’s still helpful to reinforce your knowledge.
 
 ## Requirements
- -  [ ] You need to create a Math game containing the 4 basic operations
- -  [ ] The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100.
- -  [ ] Users should be presented with a menu to choose an operation
+ -  [x] You need to create a Math game containing the 4 basic operations
+ -  [x] The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100.
+ -  [x] Users should be presented with a menu to choose an operation
  -  [ ] You should record previous games in a List and there should be an option in the menu for the user to visualize a history of previous games.
  -  [ ] You don't need to record results on a database. Once the program is closed the results will be deleted.
 
