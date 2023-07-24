b9057de8728d40f15c859b9c6229e06fa5526f3f
Luis Ramos <luisramosmaldonado@gmail.com>
2023-07-23 23:29:03 -0600

    Removed unnecessary comments

--- MathGame.ramosluis/MyFirstProgram/Helpers.cs
+++ MathGame.ramosluis/MyFirstProgram/Helpers.cs
@@ -6,26 +6,9 @@ namespace MyFirstProgram
 {
     internal class Helpers
     {
-        internal static List<Game> games = new List<Game>()
-        {
-            //new Game {Date = DateTime.Now.AddDays(1), Type = GameType.Addition, Score = 5 },
-            //new Game {Date = DateTime.Now.AddDays(2), Type = GameType.Multiplication, Score = 4 },
-            //new Game {Date = DateTime.Now.AddDays(3), Type = GameType.Division, Score = 4 },
-            //new Game {Date = DateTime.Now.AddDays(4), Type = GameType.Subtraction, Score = 3 },
-            //new Game {Date = DateTime.Now.AddDays(5), Type = GameType.Addition, Score = 1 },
-            //new Game {Date = DateTime.Now.AddDays(6), Type = GameType.Multiplication, Score = 2 },
-            //new Game {Date = DateTime.Now.AddDays(7), Type = GameType.Division, Score = 3 },
-            //new Game {Date = DateTime.Now.AddDays(8), Type = GameType.Subtraction, Score = 4 },
-            //new Game {Date = DateTime.Now.AddDays(9), Type = GameType.Addition, Score = 4 },
-            //new Game {Date = DateTime.Now.AddDays(10), Type = GameType.Multiplication, Score = 1 },
-            //new Game {Date = DateTime.Now.AddDays(11), Type = GameType.Subtraction, Score = 0 },
-            //new Game {Date = DateTime.Now.AddDays(12), Type = GameType.Division, Score = 2 },
-            //new Game {Date = DateTime.Now.AddDays(13), Type = GameType.Subtraction, Score = 5 },
-        };
+        internal static List<Game> games = new List<Game>();
         internal static void PrintGames()
         {
-            // var gamesToPrint = games.Where(x => x.Type == GameType.Addition).OrderByDescending(x => x.Score);
-
             Console.Clear();
             Console.WriteLine("Games History");
             Console.WriteLine("---------------------------");
@@ -47,8 +30,6 @@ namespace MyFirstProgram
             var random = new Random();
             var firstNumber = random.Next(1, 99);
             var secondNumber = random.Next(1, 99);
-
-            // the following line is the same as int[] result = new int[2];
             var result = new int[2];
 
             while (firstNumber % secondNumber != 0)
