d1d7aea9f5b30ad88bf2f964022fb91b5a91856d
Santox96 <51293531+Santox96@users.noreply.github.com>
2023-07-26 21:48:03 -0400

    project finished!!!

--- SantoMathGame/SantoMathGame/Models/Game.cs
+++ SantoMathGame/SantoMathGame/Models/Game.cs
@@ -2,12 +2,6 @@
 {
     internal class Game
     {
-        //public int Score
-        //{
-        //    get { return _score; }
-        //    set { _score = value; }
-        //}
-
         internal DateTime Date { get; set; }
         internal int Score { get; set; }
         internal GameType Type { get; set; }
