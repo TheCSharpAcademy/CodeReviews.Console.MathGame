5f5ec939ca3d3857b79e8220eda9a8f7a086a5c2
Maicosoft <141550764+Maicosoft@users.noreply.github.com>
2023-08-09 20:07:02 +0200

    Add files via upload

---
+++ MathGame.Maicosoft/MathGame/Calculation.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal class Calculation
+    {
+        static readonly Random _random = new();
+        public static int score;
+
+        internal static void Addition()
+        {
+            Console.WriteLine("You chose addition\n");
+
+            var questions = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                int number1 = _random.Next(1, 10);
+                int number2 = _random.Next(1, 10);
+                Console.WriteLine($"{number1} + {number2}");
+                int answer = int.Parse(Console.ReadLine());
+
+                if (answer == number1 + number2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("That is correct!\n");
+                    score++;
+                    questions++;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Wrong answer!\n");
+                    questions++;
+                }                
+                Console.WriteLine($"Score {score}/{questions}\n");
+            }
+            Console.WriteLine($"Your final score is: {score}\n");          
+        }
+        internal static void Subtraction()
+        {
+            Console.WriteLine("You chose subtraction\n");
+
+            var questions = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                int number1 = _random.Next(2, 20);
+                int number2 = _random.Next(1, 19);
+
+                while (number1 < number2)                
+                {
+                    number2 = _random.Next(1, 20);
+                }
+
+                Console.WriteLine($"{number1} - {number2}");
+                int answer = int.Parse(Console.ReadLine());
+
+                if (answer == number1 - number2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("That is correct!\n");
+                    score++;
+                    questions++;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Wrong answer!\n");
+                    questions++;
+                }
+                Console.WriteLine($"Score {score}/{questions}\n");
+            }
+            Console.WriteLine($"Your final score is: {score}\n");
+        }
+        internal static void Multiply()
+        {
+            Console.WriteLine("You chose addition\n");
+
+            var questions = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                int number1 = _random.Next(1, 10);
+                int number2 = _random.Next(1, 10);
+                Console.WriteLine($"{number1} * {number2}");
+                int answer = int.Parse(Console.ReadLine());
+
+                if (answer == number1 * number2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("That is correct!\n");
+                    score++;
+                    questions++;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Wrong answer!\n");
+                    questions++;
+                }
+                Console.WriteLine($"Score {score}/{questions}\n");
+            }
+            Console.WriteLine($"Your final score is: {score}\n");
+        }
+        internal static void Divide()
+        {
+            Console.WriteLine("You chose Divide\n");
+
+            var questions = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                int number1 = _random.Next(1, 10);
+                int number2 = _random.Next(1, 10);
+
+                Console.WriteLine($"{number1*number2} / {number2}");
+                int answer = int.Parse(Console.ReadLine());
+
+                if (answer == ((number1*number2) / number2 ))
+                {
+                    Console.Clear();
+                    Console.WriteLine("That is correct!\n");
+                    score++;
+                    questions++;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Wrong answer!\n");
+                    questions++;
+                }
+                Console.WriteLine($"Score {score}/{questions}\n");
+            }
+            Console.WriteLine($"Your final score is: {score}\n");
+        }
+    }
+}
---
+++ MathGame.Maicosoft/MathGame/Score.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal class Score
+    {
+        public string? Name { get; set; }
+        public int FinalScore { get; set; } = 0;
+        public DateTime Date { get; set; } = new DateTime();
+        public static List<Score>? Scores = new List<Score>();
+    }
+}
