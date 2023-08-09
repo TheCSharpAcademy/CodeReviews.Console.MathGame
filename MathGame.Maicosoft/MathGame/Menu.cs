f6728f2bc0c5456ee88f6f3be4fa717f82de65bb
Maico Silva <maico1978@outlook.com>
2023-08-09 20:31:43 +0200

    fixed usings

--- MathGame.Maicosoft/MathGame/Menu.cs
+++ MathGame.Maicosoft/MathGame/Menu.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MathGame
+﻿namespace MathGame
 {
     internal class Menu
     {
--- MathGame.Maicosoft/MathGame/Score.cs
+++ MathGame.Maicosoft/MathGame/Score.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MathGame
+﻿namespace MathGame
 {
     internal class Score
     {
         public string? Name { get; set; }
-        public int FinalScore { get; set; } = 0;
+        public int FinalScore { get; set; };
         public DateTime Date { get; set; } = new DateTime();
         public static List<Score>? Scores = new List<Score>();
     }
