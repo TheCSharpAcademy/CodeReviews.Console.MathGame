2eb6e082e2aa971b509090532ba234d446a98d30
Maico Silva <maico1978@outlook.com>
2023-08-11 11:45:10 +0200

    removed semi colon

--- MathGame.Maicosoft/MathGame/Score.cs
+++ MathGame.Maicosoft/MathGame/Score.cs
@@ -3,8 +3,8 @@
     internal class Score
     {
         public string? Name { get; set; }
-        public int FinalScore { get; set; };
+        public int FinalScore { get; set; }
         public DateTime Date { get; set; } = new DateTime();
-        public static List<Score>? Scores = new List<Score>();
+        public static List<Score>? Scores = new();
     }
 }
