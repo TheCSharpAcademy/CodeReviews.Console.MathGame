09c401f28b443417abf9b435e583137440acf6f6
James Makela <jamesilmarimakela@outlook.com>
2023-08-22 14:38:07 +0800

    add list to display results

--- MathGame.fantasyman/Program.cs
+++ MathGame.fantasyman/Program.cs
@@ -1,35 +1,64 @@
 ﻿// Math game program to play math games
+List<string> results = new List<string>();
+
 
 // Function to generate numbers
-int[] GenerateNumbers(int low, int high)
+int[] GenerateNumbers(int level)
 {
     Random rnd = new Random();
-    int[] numberArray = { rnd.Next(low, high + 1), rnd.Next(low, high + 1) };
-    return numberArray;
+    int[] numberArray;
+    if (level == 1)
+    {
+        numberArray = new int[2] { rnd.Next(1, 9), rnd.Next(1, 9)};
+        return numberArray;
+    }
+    else if (level == 2)
+    {
+        numberArray = new int[2] { rnd.Next(10, 99), rnd.Next(10, 99) };
+        return numberArray;
+    }
+    else
+    {
+        numberArray = new int[2] { rnd.Next(100, 999), rnd.Next(100, 999) };
+        return numberArray;
+    }
+}
+
+// Function to play game
+void PlayGame()
+{
+    Console.Write("What maths operator would you like to use? ");
+    string userOperator = Console.ReadLine();
+    if (userOperator == "+")
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            int[] numbers = GenerateNumbers(1);
+            PlayAddition(numbers);
+        }
+        foreach (string item in results)
+        {
+            Console.WriteLine(item);
+        }
+    }
 }
 
 // Function to play addition problem
-bool PlayAddition(int level)
+void PlayAddition(int[] numbers)
 {
-    if (level == 1)
+    string outputString = $"{numbers[0]} + {numbers[1]} = ";
+    string answer = $"{numbers[0] + numbers[1]}";
+    Console.Write(outputString);
+    int.TryParse(Console.ReadLine(), out int userAnswer);
+    if (userAnswer == (numbers[0] + numbers[1]))
     {
-        int[] numbers = GenerateNumbers(1, 9);
-        Console.Write($"{numbers[0]} + {numbers[1]} = ");
-        int.TryParse(Console.ReadLine(), out int answer);
-        if (answer == (numbers[0] + numbers[1]))
-        {
-            return true;
-        }
-        return false;
+        results.Add($"{outputString}{answer} Result: Passed");
     }
-    return false; // TODO: add more levels
+    else
+    {
+        results.Add($"{outputString} {answer} Result: Failed with {userAnswer}");
+    }
 }
 
-if (PlayAddition(1) == true)
-{
-    Console.WriteLine("Correct");
-}
-else
-{
-    Console.WriteLine("Incorrect");
-}
+
+PlayGame();
