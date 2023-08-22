400ea2e54729fd99701246a0e5b9cf74368e5dc9
James Makela <jamesilmarimakela@outlook.com>
2023-08-22 13:07:13 +0800

    Implement basic addition game

--- MathGame.fantasyman/Program.cs
+++ MathGame.fantasyman/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Math game program to play math games
 
+// Function to generate numbers
+int[] GenerateNumbers(int low, int high)
+{
+    Random rnd = new Random();
+    int[] numberArray = { rnd.Next(low, high + 1), rnd.Next(low, high + 1) };
+    return numberArray;
+}
+
+// Function to play addition problem
+bool PlayAddition(int level)
+{
+    if (level == 1)
+    {
+        int[] numbers = GenerateNumbers(1, 9);
+        Console.Write($"{numbers[0]} + {numbers[1]} = ");
+        int.TryParse(Console.ReadLine(), out int answer);
+        if (answer == (numbers[0] + numbers[1]))
+        {
+            return true;
+        }
+        return false;
+    }
+    return false; // TODO: add more levels
+}
+
+if (PlayAddition(1) == true)
+{
+    Console.WriteLine("Correct");
+}
+else
+{
+    Console.WriteLine("Incorrect");
+}
