f0d962a92fdfc5f135ba4d377d00d65228034b3f
James Makela <jamesilmarimakela@outlook.com>
2023-08-23 09:45:43 +0800

    requirements implemented

--- MathGame.fantasyman/Program.cs
+++ MathGame.fantasyman/Program.cs
@@ -1,64 +1,161 @@
 ﻿// Math game program to play math games
+
+// Initialising a list to store results
 List<string> results = new List<string>();
 
 
 // Function to generate numbers
-int[] GenerateNumbers(int level)
+int[] GenerateNumbers(int flag)
 {
     Random rnd = new Random();
     int[] numberArray;
-    if (level == 1)
+    if (flag == 1)
     {
-        numberArray = new int[2] { rnd.Next(1, 9), rnd.Next(1, 9)};
-        return numberArray;
-    }
-    else if (level == 2)
-    {
-        numberArray = new int[2] { rnd.Next(10, 99), rnd.Next(10, 99) };
+        numberArray = new int[2] { rnd.Next(1, 99), rnd.Next(1, 99)};
         return numberArray;
     }
     else
     {
-        numberArray = new int[2] { rnd.Next(100, 999), rnd.Next(100, 999) };
-        return numberArray;
+        while (true)
+        {
+            numberArray = new int[2] { rnd.Next(1, 99), rnd.Next(1, 99) };
+            if ((numberArray[0] % numberArray[1] == 0) && (numberArray[0] != numberArray[1]))
+            {
+                return numberArray;
+            }
+        }
     }
 }
 
 // Function to play game
 void PlayGame()
 {
-    Console.Write("What maths operator would you like to use? ");
-    string userOperator = Console.ReadLine();
-    if (userOperator == "+")
+    Console.Clear();
+    Console.WriteLine(@$"What game would you like to play today?:
+    A - Addition
+    S - Subtraction
+    M - Multiplication
+    D - Division
+    P - Display Results
+    Q - Quit the program");
+    Console.WriteLine("---------------------------------");
+
+    char userOperator = Console.ReadKey(true).KeyChar;
+    int[] numbers = GenerateNumbers(1);
+    if (userOperator == 'a')
     {
-        for (int i = 0; i < 5; i++)
-        {
-            int[] numbers = GenerateNumbers(1);
-            PlayAddition(numbers);
-        }
+        Console.Clear();
+        Console.WriteLine("Addition game selected");
+        PlayAddition(numbers);
+    }
+    else if (userOperator == 's')
+    {
+        Console.Clear();
+        Console.WriteLine("Subtraction game selected");
+        PlaySubtraction(numbers);
+    }
+    else if (userOperator == 'm')
+    {
+        Console.Clear();
+        Console.WriteLine("Multiplication game selected");
+        PlayMultiplacation(numbers);
+    }
+    else if (userOperator == 'd')
+    {
+        Console.WriteLine("Division game selected");
+        numbers = GenerateNumbers(2);
+        PlayDivision(numbers);
+    }
+    else if (userOperator == 'p')
+    {
+        Console.Clear();
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine("Results: ");
         foreach (string item in results)
         {
             Console.WriteLine(item);
         }
+        Console.WriteLine("--------------------------------");
+        Console.ReadKey();
     }
-}
-
-// Function to play addition problem
-void PlayAddition(int[] numbers)
-{
-    string outputString = $"{numbers[0]} + {numbers[1]} = ";
-    string answer = $"{numbers[0] + numbers[1]}";
-    Console.Write(outputString);
-    int.TryParse(Console.ReadLine(), out int userAnswer);
-    if (userAnswer == (numbers[0] + numbers[1]))
+    else if (userOperator == 'q')
     {
-        results.Add($"{outputString}{answer} Result: Passed");
+        Console.Clear();
+        Console.WriteLine("Goodbye");
+        Environment.Exit(1);
     }
     else
     {
-        results.Add($"{outputString} {answer} Result: Failed with {userAnswer}");
+        Console.WriteLine("Invalid input");
+    }
+}
+
+void PlayAddition(int[] numbers)
+{
+    string outputString = $"{numbers[0]} + {numbers[1]} = ";
+    int answer = numbers[0] + numbers[1];
+    Console.Write(outputString);
+    int.TryParse(Console.ReadLine(), out int userAnswer);
+    if (userAnswer == answer)
+    {
+        results.Add($"{outputString}{answer} Passed");
+    }
+    else
+    {
+        results.Add($"{outputString}{answer} Failed ({userAnswer})");
+    }
+}
+
+void PlaySubtraction(int[] numbers)
+{
+    string outputString = $"{numbers[0]} - {numbers[1]} = ";
+    int answer = numbers[0] - numbers[1];
+    Console.Write(outputString);
+    int.TryParse(Console.ReadLine(), out int userAnswer);
+    if (userAnswer == answer)
+    {
+        results.Add($"{outputString}{answer} Passed");
+    }
+    else
+    {
+        results.Add($"{outputString}{answer} Failed ({userAnswer})");
+    }
+}
+
+void PlayMultiplacation(int[] numbers)
+{
+    string outputString = $"{numbers[0]} x {numbers[1]} = ";
+    int answer = numbers[0] * numbers[1];
+    Console.Write(outputString);
+    int.TryParse(Console.ReadLine(), out int userAnswer);
+    if (userAnswer == answer)
+    {
+        results.Add($"{outputString}{answer} Passed");
+    }
+    else
+    {
+        results.Add($"{outputString}{answer} Failed ({userAnswer})");
+    }
+}
+
+void PlayDivision(int[] numbers)
+{
+    string outputString = $"{numbers[0]} / {numbers[1]} = ";
+    int answer = numbers[0] / numbers[1];
+    Console.Write(outputString);
+    int.TryParse(Console.ReadLine(), out int userAnswer);
+    if (userAnswer == answer)
+    {
+        results.Add($"{outputString}{answer} Passed");
+    }
+    else
+    {
+        results.Add($"{outputString}{answer} Failed ({userAnswer})");
     }
 }
 
 
-PlayGame();
+while (true)
+{
+    PlayGame();
+}
