c203759f7e6ef5d451ff181c235a7ec77378187f
Tom Donegan <tomdonegan@live.co.uk>
2023-08-27 22:55:55 +0100

    Game completed with additional challenges. All previous commits were to personal github

---
+++ MathGame.TomDonegan/MathGame.TomDonegan/Program.cs
@@ -0,0 +1,14 @@
+﻿using MathGameConsole;
+
+var menu = new Menu();
+
+string name = Helpers.GetName();
+
+var date = DateTime.UtcNow;
+
+var games = new List<string>();
+
+menu.ShowMenu(name, date);
+
+
+
