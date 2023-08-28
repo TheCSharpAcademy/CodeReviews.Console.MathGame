846c5dbbbf482c5eb083835abdadcdbd87a6ddaf
strawfervor <strawfervor@gmail.com>
2023-08-28 13:24:32 +0100

    add games history

--- MathGame.strawfervor/MathGame.strawfervor/Program.cs
+++ MathGame.strawfervor/MathGame.strawfervor/Program.cs
@@ -12,27 +12,86 @@ namespace MathGame
 {
     public class Game
     {
+        int goodAnswers = 0;
+        int questions = 0;
+        ArrayList questionsList = new ArrayList();
         public char mainMenu() //function is returning upper case char and this means selection of type of challange.
         {
             char playerChoice = ' ';
             Console.WriteLine("=========================");
+            Console.WriteLine($"Questions asked: {questions}   correct answers: {goodAnswers}");
             Console.WriteLine("Welcome to the Math Game!");
-            Console.WriteLine("Please chose what kind of math problem would you like to solve:");
+            Console.WriteLine("Please chose what kind of math problem would you like to solve:\n");
             Console.WriteLine("A. Addition");
             Console.WriteLine("B. Substraction");
             Console.WriteLine("C. Multiplication");
-            Console.WriteLine("D. Division");
+            Console.WriteLine("D. Division\n");
+            Console.WriteLine("H. Show games log");
             Console.WriteLine("X. Exit");
             Console.WriteLine($"\nYour choice: ");
-            playerChoice = char.Parse(Console.ReadLine()!);
-            char.ToUpper(playerChoice);
+            string input = Console.ReadLine()!;
+            try
+            {
+                playerChoice = char.Parse(input);
+                playerChoice = char.ToUpper(playerChoice);
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine($"Please enter correct letter.");
+            }
             return playerChoice;
+        }
+
+        public void showGamesHistory()
+        {
+            Console.WriteLine("=========================");
+            Console.WriteLine("Games History:");
+            foreach(var item in  questionsList) 
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Press any key...");
+            Console.ReadLine();
         }
 
         public void additionChallange()
         {
+            questions++;
+            int a, b;
+            Random rng = new Random();
+            a = rng.Next(10);
+            b = rng.Next(10);
             Console.WriteLine("========================");
-
+            string question = $"{a} + {b} = ?";
+            questionsList.Add(question);
+            Console.WriteLine($"Yor question is:\n{question}\nYour answer:");
+            for (int i = 0; i < 3; i++) 
+            {
+                string input = Console.ReadLine()!;
+                int answerNum = 0;
+                try
+                {
+                    answerNum = int.Parse(input);
+                    if (answerNum == (a + b))
+                    {
+                        Console.WriteLine($"Correct!\n{a} + {b} = {a + b}!");
+                        questionsList.Add($"Correct answer ({answerNum})");
+                        goodAnswers++;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not correct, please try again!");
+                        questionsList.Add($"Wrong answer ({answerNum})");
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"{e.Message}");
+                    Console.WriteLine("Not correct, please try again!");
+                    questionsList.Add($"Wrong answer (wrong input ex. character instead of number)");
+                }
+            }
         }
     }
 
@@ -41,9 +100,23 @@ namespace MathGame
         public static void Main(string[] args)
         {
             Game game = new Game();
-            while (true)
+            bool gameLoop = true;
+            while (gameLoop)
             {
-                game.mainMenu();
+                char userSelection = game.mainMenu();
+                if (userSelection == 'A')
+                {
+                    game.additionChallange();
+                }
+                else if (userSelection == 'X')
+                {
+                    gameLoop = false;
+                }
+                else if (userSelection == 'H')
+                {
+                    game.showGamesHistory();
+                }
+                Console.WriteLine($"\n\n");
             }
         }
     }
