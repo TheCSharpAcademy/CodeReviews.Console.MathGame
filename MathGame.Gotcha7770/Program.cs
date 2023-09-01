58a7d54689d3801b3a477052121ab02c6b2246fc
Gotcha7770 <gbakanov.box@gmail.com>
2023-09-01 12:14:57 +0300

    player can play again or quit

--- MathGame.Gotcha7770/Program.cs
+++ MathGame.Gotcha7770/Program.cs
@@ -4,23 +4,40 @@
 using MathGame.Gotcha7770;
 using Spectre.Console;
 
-AnsiConsole.Write(new FigletText("Math Game project for The C# Academy")
-    .Centered()
-    .Color(Color.Green));
+Choice whatsNext;
 
-var input = AnsiConsole.Prompt(new SelectionPrompt<MathOperation>()
-    .Title("Choose Math operation:")
-    .AddChoices(Enum.GetValues<MathOperation>()));
-
-var example = Game.GetExample(input);
-AnsiConsole.WriteLine(example.Representation);
-int answer = AnsiConsole.Ask<int>("Enter the answer:");
-
-if (answer == example.Value)
+do
 {
-    AnsiConsole.MarkupLine($"Good job! {answer} is correct :thumbs_up:");
-}
-else
+    AnsiConsole.Write(new FigletText("Math Game project for The C# Academy").Centered()
+        .Color(Color.Green));
+
+    var input = AnsiConsole.Prompt(new SelectionPrompt<MathOperation>().Title("Choose Math operation:")
+        .AddChoices(Enum.GetValues<MathOperation>()));
+
+    var example = Game.GetExample(input);
+    AnsiConsole.WriteLine(example.Representation);
+    int answer = AnsiConsole.Ask<int>("Enter the answer:");
+
+    if (answer == example.Value)
+    {
+        AnsiConsole.MarkupLine($"Good job! {answer} is correct :thumbs_up:");
+    }
+    else
+    {
+        AnsiConsole.MarkupLine($"Sorry, but answer is {example.Value} :pensive_face:");
+    }
+    
+    AnsiConsole.WriteLine();
+    whatsNext = AnsiConsole.Prompt(new SelectionPrompt<Choice>()
+        .Title("What`s next?")
+        .AddChoices(Enum.GetValues<Choice>()));
+
+    AnsiConsole.Clear();
+
+} while (whatsNext == Choice.PlayAgain);
+
+public enum Choice
 {
-    AnsiConsole.MarkupLine($"Sorry, but answer is {example.Value} :pensive_face:");
+    PlayAgain,
+    Quit
 }
