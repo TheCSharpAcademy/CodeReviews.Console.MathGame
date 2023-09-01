080e8953e2b9a4a219923079878f022497016175
Gotcha7770 <gbakanov.box@gmail.com>
2023-09-01 11:41:24 +0300

    implement addition

---
+++ MathGame.Gotcha7770/Game.cs
@@ -0,0 +1,36 @@
+﻿namespace MathGame.Gotcha7770;
+
+public enum MathOperation 
+{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division
+}
+
+public static class Game
+{
+    private static readonly Random Random = new Random((int)DateTime.Now.Ticks);
+
+    public static string GetExample(MathOperation operation)
+    {
+        return operation switch
+        {
+            MathOperation.Addition => Addition.GetNext(Random),
+            // MathOperation.Subtraction => Subtraction.GetNext(Random),
+            // MathOperation.Multiplication => Multiplication.GetNext(Random),
+            // MathOperation.Division => Division.GetNext(Random),
+            _ => throw new ArgumentOutOfRangeException(nameof(operation))
+        };
+    }
+    
+    public class Addition
+    {
+        public static string GetNext(Random random)
+        {
+            int x = random.Next(0, 999);
+            int y = random.Next(0, 999);
+            return $"{x} + {y}";
+        }
+    }
+}
---
+++ MathGame.Gotcha7770/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://spectreconsole.net/ for spectre.console documentation
+
+using MathGame.Gotcha7770;
+using Spectre.Console;
+
+AnsiConsole.Write(new FigletText("Math Game project for The C# Academy")
+    .Centered()
+    .Color(Color.Green));
+
+var input = AnsiConsole.Prompt(new SelectionPrompt<MathOperation>()
+    .Title("Choose Math operation:")
+    .AddChoices(Enum.GetValues<MathOperation>()));
+
+var example = Game.GetExample(input);
+AnsiConsole.WriteLine(example);
+int answer = AnsiConsole.Ask<int>("Enter the answer:");
