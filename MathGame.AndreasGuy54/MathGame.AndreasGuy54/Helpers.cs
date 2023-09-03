bb17c237b6a3250d2168653ab84fe36893554c99
Andreas Abaidoo <abaidooandy@gmail.com>
2023-09-03 18:15:09 +0000

    Removed comments

--- MathGame.AndreasGuy54/MathGame.AndreasGuy54/Helpers.cs
+++ MathGame.AndreasGuy54/MathGame.AndreasGuy54/Helpers.cs
@@ -4,28 +4,10 @@ namespace MathGame.AndreasGuy54
 {
     internal class Helpers
     {
-        static List<Game> games = new List<Game>
-        {
-            //new Game { Date = DateTime.Now.AddDays(1), Type = GameType.Addition, Score = 5 },
-            //new Game { Date = DateTime.Now.AddDays(2), Type = GameType.Multiplication, Score = 4 },
-            //new Game { Date = DateTime.Now.AddDays(3), Type = GameType.Division, Score = 4 },
-            //new Game { Date = DateTime.Now.AddDays(4), Type = GameType.Subtraction, Score = 3 },
-            //new Game { Date = DateTime.Now.AddDays(5), Type = GameType.Addition, Score = 1 },
-            //new Game { Date = DateTime.Now.AddDays(6), Type = GameType.Multiplication, Score = 2 },
-            //new Game { Date = DateTime.Now.AddDays(7), Type = GameType.Division, Score = 3 },
-            //new Game { Date = DateTime.Now.AddDays(8), Type = GameType.Subtraction, Score = 4 },
-            //new Game { Date = DateTime.Now.AddDays(9), Type = GameType.Addition, Score = 4 },
-            //new Game { Date = DateTime.Now.AddDays(10), Type = GameType.Multiplication, Score = 1 },
-            //new Game { Date = DateTime.Now.AddDays(11), Type = GameType.Subtraction, Score = 0 },
-            //new Game { Date = DateTime.Now.AddDays(12), Type = GameType.Division, Score = 2 },
-            //new Game { Date = DateTime.Now.AddDays(13), Type = GameType.Subtraction, Score = 5 }
-        };
+        static List<Game> games = new();
 
         internal static void PrintGames()
         {
-            //var gamesToPrint = games.Where(g => g.Date > new DateTime(2023, 09, 10))
-            //                        .OrderByDescending(g => g.Score);
-
             Console.Clear();
             Console.WriteLine("Games History\n--------------------------------------------------------------");
             
