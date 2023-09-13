4a9c756088d7467d439a59069d367c67ac6755c6
MiroiuGabriel <miroiugabriel1231@gmail.com>
2023-09-13 21:28:41 +0300

    Project Finished!

---
+++ MiroiuDev.MathGame/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace MiroiuDev.MathGame;
+
+enum GameType
+{
+    Addition,
+    Substraction,
+    Multiplication,
+    Division,
+    Random
+}
+
+internal class Game
+{
+    public int Score { get; set; }
+    public DateTime Date { get; set; }
+    public GameType Type { get; set; }
+}
