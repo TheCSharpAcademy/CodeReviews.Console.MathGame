3f39dd6073b56b7a7c8d863ff7f2725828208a46
Matija Sedlaček <matijasedlacek@gmail.com>
2023-09-16 19:31:22 +0200

    logic for multiplication, main menu features

--- MathGame.Matija87/MathGame.Matija87/GameEngine.cs
+++ MathGame.Matija87/MathGame.Matija87/GameEngine.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.Design;
-
-namespace MathGame.Matija87
+﻿namespace MathGame.Matija87
 {
     internal static class GameEngine
     {
@@ -29,13 +27,15 @@ namespace MathGame.Matija87
                 if (int.Parse(result) == firstNumber + secondNumber)
                 {
                     score++;
-                    Console.WriteLine($"Correct! Your current score is {score} / {i + 1}");
+                    Console.WriteLine($"Correct! Your score is {score} / {i + 1}");
                 }
                 else
                 {
-                    Console.WriteLine($"Wrong answer! Your current score is {score} / {i + 1}");
+                    Console.WriteLine($"Wrong answer! Your score is {score} / {i + 1}");
                 }
             }
+            Console.WriteLine("\nPress any key to go back to main menu");
+            Console.ReadKey();
         }
 
         public static void Subtraction()
@@ -63,13 +63,15 @@ namespace MathGame.Matija87
                 if (int.Parse(result) == firstNumber - secondNumber)
                 {
                     score++;
-                    Console.WriteLine($"Correct! Your current score is {score} / {i + 1}");
+                    Console.WriteLine($"Correct! Your score is {score} / {i + 1}");
                 }
                 else
                 {
-                    Console.WriteLine($"Wrong answer! Your current score is {score} / {i + 1}");
+                    Console.WriteLine($"Wrong answer! Your score is {score} / {i + 1}");
                 }
             }
+            Console.WriteLine("\nPress any key to go back to main menu");
+            Console.ReadKey();
         }
 
         public static void Multiplication()
@@ -97,13 +99,74 @@ namespace MathGame.Matija87
                 if (int.Parse(result) == firstNumber * secondNumber)
                 {
                     score++;
-                    Console.WriteLine($"Correct! Your current score is {score} / {i + 1}");
+                    Console.WriteLine($"Correct! Your score is {score} / {i + 1}");
                 }
                 else
                 {
-                    Console.WriteLine($"Wrong answer! Your current score is {score} / {i + 1}");
+                    Console.WriteLine($"Wrong answer! Your score is {score} / {i + 1}");
                 }
             }
+            Console.WriteLine("\nPress any key to go back to main menu");
+            Console.ReadKey();
+        }
+
+        public static void Division()
+        {
+            Random random = new Random();
+            Console.WriteLine("Division game");
+            Console.WriteLine("-------------");
+
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int score = 0;
+            string? result;
+
+            for (int i = 0; i < 5; i++)
+            {
+                int[] divisionNumbers = GetDivisionNumbers();
+                firstNumber = divisionNumbers[0];
+                secondNumber = divisionNumbers[1];
+
+
+                Console.WriteLine($"{firstNumber} / {secondNumber}");
+
+                do
+                {
+                    result = Console.ReadLine();
+                } while (string.IsNullOrEmpty(result) || !Int32.TryParse(result, out _));
+
+                if (int.Parse(result) == firstNumber / secondNumber)
+                {
+                    score++;
+                    Console.WriteLine($"Correct! Your score is {score} / {i + 1}");
+                }
+                else
+                {
+                    Console.WriteLine($"Wrong answer! Your score is {score} / {i + 1}");
+                }
+            }
+            Console.WriteLine("\nPress any key to go back to main menu");
+            Console.ReadKey();
+        }
+
+        static int[] GetDivisionNumbers()
+        {
+            Random random = new Random();
+            int firstNumber = random.Next(0, 99);
+            int secondNumber = random.Next(1, 99); //Prevents dividing by 0
+
+            int[] result = new int[2];
+
+            while (firstNumber % secondNumber != 0)
+            {
+                firstNumber = random.Next(0, 99);
+                secondNumber = random.Next(1, 99); 
+            }
+
+            result[0] = firstNumber;
+            result[1] = secondNumber;
+
+            return result;
         }
     }
 }
--- MathGame.Matija87/MathGame.Matija87/Menu.cs
+++ MathGame.Matija87/MathGame.Matija87/Menu.cs
@@ -1,53 +1,63 @@
 ﻿using MathGame.Matija87;
-using System;
 
 public class Menu
 {
     public void DisplayMenu(string name, DateTime dateTime)
     {
         char gameSelected;
+        bool isGameOn = true;
+
         Console.WriteLine($"Hi {name}! Welcome to Math Game!");
         Console.WriteLine($"Today is {dateTime}");
         Console.WriteLine("Press any key to start the game!");
         Console.ReadKey();
-        Console.Clear();
-        Console.WriteLine("-------------------------------------------------------");
-        Console.WriteLine(@"Choose your game: 
+        
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine(@"Choose your game: 
 Addition (A)
 Subtraction (S)
 Multiplication (M)
 Division (D)
 Quit (Q)");
 
-        bool correctInput = false;
-        do
-        {
-            Console.WriteLine("-------------------------------------------------------");
-            gameSelected = Console.ReadKey().KeyChar;
-            gameSelected = Char.ToLower(gameSelected);
-            Console.WriteLine();
-            switch (gameSelected)
+            bool correctInput = false;
+            do
             {
-                case 'a':
-                    GameEngine.Addition();
-                    correctInput = true;
-                    break;
-                case 's':
-                    GameEngine.Subtraction();
-                    correctInput = true;
-                    break;
-                case 'm':
-                    GameEngine.Multiplication();
-                    correctInput= true;
-                    break;
-                case 'q':
-                    Console.WriteLine("Thanks for playing!");
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.WriteLine("Wrong selection! Try Again!");
-                    break;
-            };
-        } while (!correctInput);
+                Console.WriteLine("-------------------------------------------------------");
+                gameSelected = Console.ReadKey().KeyChar;
+                gameSelected = Char.ToLower(gameSelected);
+                Console.WriteLine();
+                switch (gameSelected)
+                {
+                    case 'a':
+                        GameEngine.Addition();
+                        correctInput = true;
+                        break;
+                    case 's':
+                        GameEngine.Subtraction();
+                        correctInput = true;
+                        break;
+                    case 'm':
+                        GameEngine.Multiplication();
+                        correctInput = true;
+                        break;
+                    case 'd':
+                        GameEngine.Division();
+                        correctInput = true;
+                        break;
+                    case 'q':
+                        Console.WriteLine("Thanks for playing!");
+                        isGameOn = false;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong selection! Try again!");
+                        break;
+                };
+            } while (!correctInput && isGameOn);
+
+        } while (isGameOn);
     }
 }
