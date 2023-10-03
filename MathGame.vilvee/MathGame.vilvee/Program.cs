1ce010901b280ae4074c29cdea56ce8b4c5591ec
Veronika <116470467+vilvee@users.noreply.github.com>
2023-10-03 18:40:30 -0400

    created repository

---
+++ MathGame.vilvee/MathGame.vilvee/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MathGame.vilvee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
