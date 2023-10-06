fccd079a410ae7691eefd58636fb2bccb0b364cd
Marius <mariuswaseige@protonmail.com>
2023-10-06 09:12:03 +0200

    Removed commented code

--- Math Game.Flabidouf/Math Game.Flabidouf/Models/Game.cs
+++ Math Game.Flabidouf/Math Game.Flabidouf/Models/Game.cs
@@ -2,14 +2,6 @@ namespace Math_Game_v2.Models;
 
 public class Game
 {
-    /*private int _score;
-
-    public int Score
-    {
-        get { return _score;}
-        set { _score = value; }
-
-    }*/
     public DateTime Date { get; set; }
     
     public int Score { get; set; }
