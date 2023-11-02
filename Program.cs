df3ba6e59fbf468773932c7dcc98f6247a15e699
Mr LighTz <LightBR@outlook.com>
2023-11-01 23:00:37 -0300

    Implement operation methods

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MathGame {
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace MathGame {
     internal class Program {
         static void Main(string[] args) {
 
@@ -43,20 +45,132 @@
             }
         }
 
-        private static void Division() {
-            throw new NotImplementedException();
+        //Start of methods
+
+        static int GenerateRandomNumber(int min, int limit) {
+            Random random = new Random();
+            int randomNumber = random.Next(min, limit);
+
+            return randomNumber;
         }
 
+        private static int[] GetDivisionNumbers() {
+            int firstNumber = GenerateRandomNumber(1, 99);
+            int secondNumber = GenerateRandomNumber(1, 99);
+
+            int[] result = new int[2];
+
+            while(firstNumber % secondNumber != 0) {
+                firstNumber = GenerateRandomNumber(1, 99);
+                secondNumber = GenerateRandomNumber(1, 99);
+            }
+
+            result[0] = firstNumber;
+            result[1] = secondNumber;
+
+            return result;
+        }
+
+        private static void Division() {
+            Console.WriteLine("You choose division mode!");
+
+            int score = 0;
+
+            for (int i = 0; i <= 4; i++) {
+                int[] divisionNumbers = GetDivisionNumbers();
+                int firstNumber = divisionNumbers[0];
+                int secondNumber = divisionNumbers[1];
+
+                Console.WriteLine($"{firstNumber} / {secondNumber}");
+                int? result = int.Parse(Console.ReadLine());
+
+                if (result == firstNumber / secondNumber) {
+                    Console.WriteLine("Your answer was correct! Type any key for the next question.");
+                    score++;
+                    Console.ReadLine();
+                }
+                else {
+                    Console.WriteLine("Your answer was incorrect! Type any key for the next question. Type any key for the next question.Type any key for the next question.");
+                    Console.ReadLine();
+                }
+            }
+        }
+        
+
         private static void Multiplication() {
-            throw new NotImplementedException();
+            int score = 0;
+
+            for (int i = 0; i <= 4; i++) {
+                Console.Clear();
+                Console.WriteLine("You choose multiplication mode!");
+
+                int firstNumber = GenerateRandomNumber(1, 9);
+                int secondNumber = GenerateRandomNumber(1, 9);
+
+                Console.WriteLine($"{firstNumber} * {secondNumber}");
+                int? result = int.Parse(Console.ReadLine());
+
+                if (result == firstNumber + secondNumber) {
+                    Console.WriteLine("Your answer was correct! Type any key for the next question.");
+                    score++;
+                    Console.ReadLine();
+                }
+                else {
+                    Console.WriteLine("Your answer was incorrect! Type any key for the next question.");
+                    Console.ReadLine();
+                }
+            }
         }
 
         private static void Subtraction() {
-            throw new NotImplementedException();
+            Console.WriteLine("You choose subtraction mode!");
+
+            int score = 0;
+
+            for (int i = 0; i <= 4; i++) {
+
+                int firstNumber = GenerateRandomNumber(1, 9);
+                int secondNumber = GenerateRandomNumber(1, 9);
+
+                Console.WriteLine($"{firstNumber} - {secondNumber}");
+                int? result = int.Parse(Console.ReadLine());
+
+                if (result == firstNumber + secondNumber) {
+                    Console.WriteLine("Your answer was correct! Type any key for the next question.");
+                    score++;
+                    Console.ReadLine();
+                }
+                else {
+                    Console.WriteLine("Your answer was incorrect! Type any key for the next question.");
+                    Console.ReadLine();
+                }
+            }
         }
 
         private static void Addition() {
-            throw new NotImplementedException();
+            Console.WriteLine("You choose addition mode!");
+
+            int score = 0;
+
+            for (int i = 0; i <= 4; i++) {
+
+                int firstNumber = GenerateRandomNumber(1, 9);
+                int secondNumber = GenerateRandomNumber(1, 9);
+
+                Console.WriteLine($"{firstNumber} + {secondNumber}");
+                int? result = int.Parse(Console.ReadLine());
+
+                if (result == firstNumber + secondNumber) {
+                    Console.WriteLine("Your answer was correct! Type any key for the next question.");
+                    score++;
+                    Console.ReadLine();
+                }
+                else {
+                    Console.WriteLine("Your answer was incorrect! Type any key for the next question.");
+                    Console.ReadLine();
+                }
+            }
         }
+        //End of methods
     }
 }
