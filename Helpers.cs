a3c17b63c9bbadaba1f880008c584a71131578b8
Mr LighTz <LightBR@outlook.com>
2023-11-08 13:40:40 -0300

    Implement Linq as a test

--- Helpers.cs
+++ Helpers.cs
@@ -1,10 +1,30 @@
 ﻿using MathGame.Models;
+using System.Linq;
 
 namespace MathGame {
     internal static class Helpers {
 
-        internal static List<Game> games = new();
+        internal static List<Game> games = new() {
+            new Game { Date = DateTime.Now.AddDays(1), Type = GameType.Addition, Score = 5 },
+            new Game { Date = DateTime.Now.AddDays(2), Type = GameType.Multiplication, Score = 4 },
+            new Game { Date = DateTime.Now.AddDays(3), Type = GameType.Division, Score = 4 },
+            new Game { Date = DateTime.Now.AddDays(4), Type = GameType.Subtraction, Score = 3 },
+            new Game { Date = DateTime.Now.AddDays(5), Type = GameType.Addition, Score = 1 },
+            new Game { Date = DateTime.Now.AddDays(6), Type = GameType.Multiplication, Score = 2 },
+            new Game { Date = DateTime.Now.AddDays(7), Type = GameType.Division, Score = 3 },
+            new Game { Date = DateTime.Now.AddDays(8), Type = GameType.Subtraction, Score = 4 },
+            new Game { Date = DateTime.Now.AddDays(9), Type = GameType.Addition, Score = 4 },
+            new Game { Date = DateTime.Now.AddDays(10), Type = GameType.Multiplication, Score = 1 },
+            new Game { Date = DateTime.Now.AddDays(11), Type = GameType.Subtraction, Score = 0 },
+            new Game { Date = DateTime.Now.AddDays(12), Type = GameType.Division, Score = 2 },
+            new Game { Date = DateTime.Now.AddDays(13), Type = GameType.Subtraction, Score = 5 },
+        };
+
         internal static void PrintGames() {
+            // IEnumerable<Game> gamesToPrint = games.Where(x => x.Type == GameType.Division);
+            // IEnumerable<Game> gamesToPrint = games.Where(x => x.Date > new DateTime(2023, 11, 07));
+            // IEnumerable<Game> gamesToPrint = games.Where(x => x.Date > new DateTime(2022, 08, 09) && x.Score > 3);
+            // IEnumerable<Game> gamesToPrint = games.Where(x => x.Date > new DateTime(2023, 11, 07)).OrderByDescending(x => x.Score);
 
             Console.Clear();
             Console.WriteLine("----------------------------------");
