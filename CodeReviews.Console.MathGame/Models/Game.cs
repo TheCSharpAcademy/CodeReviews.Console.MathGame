368bfaedaec149a491573a19dac3dd679794924f
damianramm <98427322+damianramm@users.noreply.github.com>
2023-11-20 16:42:07 -0300

    Update Game.cs

--- CodeReviews.Console.MathGame/Models/Game.cs
+++ CodeReviews.Console.MathGame/Models/Game.cs
@@ -3,11 +3,6 @@ namespace MyFirstProgram.Models;
 
     internal class Game
     {
-        //private int _score;
-
-        //public int Score 
-        //{get { return _score; } 
-        //set { _score = value; }}
 
     public int Score { get; set; }
     public GameType Type { get; set; }
