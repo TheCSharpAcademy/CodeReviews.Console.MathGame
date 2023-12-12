3f45425996c24d33922d3b591f7ed78c5aa5256b
ndthuy112 <ndthuy112@gmail.com>
2023-12-12 20:59:09 +0700

    remove directives

--- MathGame.ndthuy112/MathGameLibrary/GameEngine.cs
+++ MathGame.ndthuy112/MathGameLibrary/GameEngine.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Channels;
-using System.Threading.Tasks;
-using System.Threading.Tasks.Sources;
-
-namespace MathGameLibrary
+﻿namespace MathGameLibrary
 {
     public class GameEngine
     {
