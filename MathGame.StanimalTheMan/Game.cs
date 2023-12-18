d13bf93402e560cef19a031e77df0bd8bdf8d8c2
Stan Choi <sjc591@nyu.edu>
2023-12-17 22:25:59 -0500

    init commit

---
+++ MathGame.StanimalTheMan/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame.StanimalTheMan
+{
+    internal class Game
+    {
+    }
+}
