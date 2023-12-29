2fea67d83ecb95f2d2dca21bc422547b5feb5f26
Lawrence C <lawrencecaudle870@gmail.com>
2023-12-28 20:22:49 -0500

    added code

---
+++ MathGame.ltc870/Helpers.cs
@@ -0,0 +1,163 @@
+﻿namespace MathGame.ltc870
+{
+    internal class Helpers
+    {
+        //@ Get operands method
+        public int[] GetOperands(char difficultyLevel, char gameType)
+        {
+            int topRange = 0;
+
+            switch (difficultyLevel)
+            {
+                case 'e':
+                    topRange = gameType == 'd' ? 99 : 9;
+                    break;
+                case 'm':
+                    topRange = gameType == 'd' ? 999 : 99;
+                    break;
+                case 'h':
+                    topRange = gameType == 'd' ? 9999 : 999;
+                    break;
+            }
+
+            Random random = new Random();
+            int num1 = random.Next(1, topRange);
+            int num2 = random.Next(1, topRange);
+
+            int[] operands = new int[2];
+
+            while (num1 % num2 != 0)
+            {
+                num1 = random.Next(1, 9);
+                num2 = random.Next(1, 9);
+            }
+
+            operands[0] = num1;
+            operands[1] = num2;
+
+            return operands;
+        }
+
+        //@ Get difficulty level methods
+        public char GetDifficultyLevel()
+        {
+            Console.WriteLine(@"Choose your difficulty level:
+        E - Easy
+        M - Medium
+        H - Hard");
+
+            char difficultyLevel = char.ToLower(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+            char[] options = { 'e', 'm', 'h' };
+
+            while (!options.Contains(difficultyLevel))
+            {
+                Console.WriteLine("Invalid option. Try again.");
+                difficultyLevel = char.ToLower(Console.ReadKey().KeyChar);
+            }
+            return difficultyLevel;
+        }
+
+        //@ Get game history method
+        public List<string> gameHistory = new List<string>();
+
+        public void ViewScoreHistory()
+        {
+            foreach (string score in gameHistory)
+            {
+                Console.WriteLine(score);
+            }
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        //@ Validate name method
+        public string ValidateName()
+        {
+            string name;
+            bool isInputValid;
+
+            do
+            {
+                name = Console.ReadLine();
+                isInputValid = !string.IsNullOrEmpty(name);
+
+                if (!isInputValid)
+                {
+                    Console.WriteLine("Please enter a valid name.");
+                }
+            } while (!isInputValid);
+            return name;
+        }
+
+        //@ Validate menu input method
+        public char ValidateMenuInput()
+        {
+            char userInput;
+            bool isInputValid;
+
+            do
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                userInput = char.ToLower(keyInfo.KeyChar);
+
+                isInputValid = userInput == 'a' || userInput == 's' || userInput == 'm' || userInput == 'd' || userInput == 'r' || userInput == 'v' || userInput == 'q';
+
+                if (!isInputValid)
+                {
+                    Console.WriteLine("Invalid option. Please try again.");
+                }
+
+            } while (!isInputValid);
+            return userInput;
+        }
+
+        //@ Validate number to play method
+        public int ValidateNumToPlay()
+        {
+            int numToPlay;
+            bool isInputValid;
+
+            do
+            {
+                isInputValid = int.TryParse(Console.ReadLine(), out numToPlay);
+
+                if (!isInputValid)
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                    isInputValid = false;
+                }
+                else if (numToPlay < 1)
+                {
+                    Console.WriteLine("Please enter a number greater than 0.");
+                    isInputValid = false;
+                }
+                else if (numToPlay > 10)
+                {
+                    Console.WriteLine("Please enter a number less than 10.");
+                    isInputValid = false;
+                }
+            } while (!isInputValid);
+            return numToPlay;
+        }
+
+        //@ Validate answer input method
+        public int ValidateAnswerInput()
+        {
+            int answer;
+            bool isInputValid;
+
+            do
+            {
+                isInputValid = int.TryParse(Console.ReadLine(), out answer);
+
+                if (!isInputValid)
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    isInputValid = false;
+                }
+            } while (!isInputValid);
+            return answer;
+        }
+    }
+}
---
+++ MathGame.ltc870/Program.cs
@@ -0,0 +1,131 @@
+﻿namespace MathGame.ltc870
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Helpers helpers = new Helpers();
+            Console.WriteLine("What is your name?");
+            string name = helpers.ValidateName();
+            DateTime date = DateTime.Now;
+            char userOption;
+            bool gameIsOn = true;
+
+
+            Console.WriteLine($"Hello {name}! Today is {date} let's play our Math Game! Press any key to begin...");
+            Console.ReadKey();
+
+            do
+            {
+                Console.WriteLine(@"Which game would you like to play?
+      A - Addition
+      S - Subtraction
+      M - Multiplication
+      D - Division
+      R - Random
+      V - View Score History
+      Q - Quit");
+
+                userOption = helpers.ValidateMenuInput();
+                Console.Clear();
+
+                switch (char.ToLower(userOption))
+                {
+                    case 'a':
+                        PlayGame('a');
+                        break;
+                    case 's':
+                        PlayGame('s');
+                        break;
+                    case 'm':
+                        PlayGame('m');
+                        break;
+                    case 'd':
+                        PlayGame('d');
+                        break;
+                    case 'r':
+                        PlayGame('r', true);
+                        break;
+                    case 'v':
+                        helpers.ViewScoreHistory();
+                        break;
+                    case 'q':
+                        Console.WriteLine("Thanks for playing! Goodbye!");
+                        gameIsOn = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            } while (gameIsOn);
+
+            void PlayGame(char gameType, bool isRandom = false)
+            {
+                char[] gameTypes = { 'a', 's', 'm', 'd' };
+
+                Console.Clear();
+
+                int score = 0;
+
+                Console.WriteLine("How many times would you like to play?");
+
+                int numTimes = helpers.ValidateNumToPlay();
+
+                char difficultyLevel = helpers.GetDifficultyLevel();
+
+                var startTime = DateTime.Now;
+
+                for (int i = 0; i < numTimes; i++)
+                {
+                    if (isRandom)
+                    {
+                        var random = new Random();
+                        var gameIndex = random.Next(0, 4);
+                        gameType = gameTypes[gameIndex];
+                    }
+
+                    string sign = gameType switch
+                    {
+                        'a' => "+",
+                        's' => "-",
+                        'm' => "*",
+                        'd' => "/",
+                    };
+
+                    Func<int, int, int> operation = gameType switch
+                    {
+                        'a' => (x, y) => x + y,
+                        's' => (x, y) => x - y,
+                        'm' => (x, y) => x * y,
+                        'd' => (x, y) => x / y,
+                    };
+
+                    var operands = helpers.GetOperands(difficultyLevel, gameType);
+                    int firstNumber = operands[0];
+                    int secondNumber = operands[1];
+
+                    Console.WriteLine($"{firstNumber} {sign} {secondNumber}");
+                    int answer = helpers.ValidateAnswerInput();
+
+                    if (answer == operation(firstNumber, secondNumber))
+                    {
+                        Console.WriteLine("Correct!");
+                        score++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect!");
+                    }
+                }
+
+                var endTime = DateTime.Now;
+                TimeSpan totalTime = endTime - startTime;
+
+                Console.WriteLine($"You got {score} out of {numTimes} correct in {totalTime.TotalSeconds} seconds!");
+
+                helpers.gameHistory.Add($"{gameType} - {score} out of {numTimes} correct in {totalTime.TotalSeconds} seconds!");
+            }
+        }
+    }
+}
