20fa2a42ee1637846b9b062376c5ca4f7340361b
willN123 <w.niland@live.com.au>
2024-01-23 17:47:39 +0800

    Fixed bad string formatting when saving games. Split code into smaller functions. Though I think I may have gone overboard, particularly in GameLoop()?

--- MathGame.willN123/Program.cs
+++ MathGame.willN123/Program.cs
@@ -4,27 +4,24 @@
 //Initialize variables.
 Random random = new();
 
-int firstNumber, secondNumber, questionCount, score;
+int firstNumber, secondNumber, questionCount, score, timeTaken;
 int questionLimit = 5;
 int lowerRange = 2;
 int upperRange = 24;
-int calculationResult = 0;
 int gamesPlayed = 0;
+double calculationResult = 0;
+double answer = 0;
 
 string? readResult;
 string operationLabel = "";
 string separator = "".PadLeft(25, '-');
+string outcomeText;
+string[] gameResults = new string[50];
 
-string[] gameResults = new string[20];
+TimeSpan startTime, endTime;
 
-TimeSpan startTime;
-TimeSpan endTime;
-int timeTaken;
-
-bool validInput;
+bool validInput, gameRunning, randomMode;
 bool programRunning = true;
-bool gameRunning;
-bool randomMode;
 
 MainMenu();
 
@@ -32,242 +29,23 @@ void MainMenu()
 {
     do
     {
-        Console.Clear();
-        Console.WriteLine(
-@$"Math Game Collection
-Menu
-{separator}
-1. Addition
-2. Subtraction
-3. Multiplication
-4. Division
-5. Random Mode
-6. Game History
-7. Settings
-8. Exit
-(Type a number and press Enter to choose)");
+        DisplayMainMenuOptions();
 
         do
         {
             validInput = true;
-
-            readResult = Console.ReadLine();
-            if (readResult != null)
-            {
-                switch (readResult)
-                {
-                    case "1":
-                        GameScreen("+");
-                        break;
-                    case "2":
-                        GameScreen("-");
-                        break;
-                    case "3":
-                        GameScreen("x");
-                        break;
-                    case "4":
-                        GameScreen("/");
-                        break;
-                    case "5":
-                        GameScreen("random");
-                        break;
-                    case "6":
-                        GameHistory();
-                        break;
-                    case "7":
-                        Settings();
-                        break;
-                    case "8":
-                        Console.WriteLine("Exiting game.");
-                        programRunning = false;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input. Please enter a number from 1-8.");
-                        validInput = false;
-                        break;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid input. Please enter a number from 1-8.");
-                validInput = false;
-            }
+            ChooseMainMenuOption();
         } while (!validInput);
     } while (programRunning);
 }
 
 void GameScreen(string operation)
 {
-    questionCount = 0;
-    score = 0;
+    GameSetup(operation);
 
-    startTime = DateTime.Now.TimeOfDay;
+    SetOperationLabel(operation);
 
-    randomMode = operation == "random";
-    gameRunning = true;
-
-    Console.Clear();
-
-    do
-    {
-        Console.WriteLine("Math Game Collection");
-        GetNumbers();
-
-        if (randomMode)
-        {
-            Console.WriteLine("Random Mode");
-
-            int pickOperation = random.Next(0, 4);
-
-            switch (pickOperation)
-            {
-                case 0:
-                    operation = "+";
-                    break;
-                case 1:
-                    operation = "-";
-                    break;
-                case 2:
-                    operation = "x";
-                    break;
-                case 3:
-                    operation = "/";
-                    break;
-            }
-        }
-        else
-        {
-            Console.WriteLine($"{operationLabel} Game");
-        }
-
-        Console.WriteLine(separator);
-        CalculateAnswer(operation);
-        Console.WriteLine($"{firstNumber} {operation} {secondNumber} = ?");
-
-        validInput = false;
-        do
-        {
-            readResult = Console.ReadLine();
-            if (readResult != null && Int32.TryParse(readResult, out int answer))
-            {
-                validInput = true;
-
-                if (answer == calculationResult)
-                {
-                    Console.Write("Correct! ");
-                    score++;
-                }
-                else
-                {
-                    Console.Write("Incorrect! ");
-                }
-
-                questionCount++;
-
-                Console.WriteLine($"{firstNumber} {operation} {secondNumber} = {calculationResult}\nScore: {score}/{questionCount}");
-                Console.WriteLine("Press Enter to continue.");
-                Console.ReadLine();
-
-                if (questionCount == questionLimit)
-                {
-                    endTime = DateTime.Now.TimeOfDay;
-                    timeTaken = (int)(endTime - startTime).TotalSeconds;
-
-                    SaveScore(randomMode ? "R" : operation);
-
-                    gameRunning = false;
-                    gamesPlayed++;
-
-                    Console.WriteLine(separator);
-                    Console.WriteLine($"Game Over! Final Score: {score}/{questionCount}. Time: {timeTaken}s");
-                    Console.WriteLine("Press Enter to return to Menu.");
-                    Console.ReadLine();
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid input. Please enter a number.");
-            }
-        } while (!validInput);
-
-        Console.Clear();
-
-    } while (gameRunning);
-}
-
-void GetNumbers()
-{
-    firstNumber = random.Next(lowerRange, upperRange);
-    secondNumber = random.Next(lowerRange, upperRange);
-}
-
-void CalculateAnswer(string operation)
-{
-    switch (operation)
-    {
-        case "+":
-            operationLabel = "Addition";
-            calculationResult = firstNumber + secondNumber;
-            break;
-        case "-":
-            operationLabel = "Subtraction";
-            calculationResult = firstNumber - secondNumber;
-            break;
-        case "x":
-            operationLabel = "Multiplication";
-            calculationResult = firstNumber * secondNumber;
-            break;
-        case "/":
-            operationLabel = "Division";
-            bool wholeNumber = false;
-            do
-            {
-                if (firstNumber % secondNumber == 0 && firstNumber != secondNumber)
-                {
-                    calculationResult = firstNumber / secondNumber;
-                    wholeNumber = true;
-                }
-                else
-                {
-                    GetNumbers();
-                }
-            } while (!wholeNumber);
-            break;
-    }
-}
-
-void SaveScore(string operation)
-{
-    if (gamesPlayed < 9)
-    {
-        gameResults[gamesPlayed] = $" {gamesPlayed + 1} |  {operation}   |  {score}/{questionCount}  |  {timeTaken}";
-    }
-    else if (gamesPlayed >= 9)
-    {
-        gameResults[gamesPlayed] = $" {gamesPlayed + 1}|  {operation}   |  {score}/{questionCount}  |  {timeTaken}";
-    }
-}
-
-void GameHistory()
-{
-    Console.Clear();
-    Console.WriteLine(
-@$"Math Game Collection
-Game History
-{separator}
- # | Type | Score | Time(secs) ");
-
-    foreach (string game in gameResults)
-    {
-        if (game != null)
-        {
-            Console.WriteLine(game);
-        }
-    }
-
-    Console.WriteLine(separator);
-    Console.WriteLine("Press Enter to return to menu.");
-    Console.ReadLine();
+    GameLoop(operation);
 }
 
 void Settings()
@@ -313,6 +91,310 @@ Settings
     } while (!validInput);
 }
 
+void GameHistory()
+{
+    Console.Clear();
+    Console.WriteLine(
+@$"Math Game Collection
+Game History
+{separator}");
+    Console.WriteLine("   #\t|  Type\t| Score\t| Time(secs)");
+
+    foreach (string game in gameResults)
+    {
+        if (game != null)
+        {
+            Console.WriteLine(game);
+        }
+    }
+
+    Console.WriteLine(separator);
+    Console.WriteLine("Press Enter to return to menu.");
+    Console.ReadLine();
+}
+
+//Used in MainMenu() -
+void DisplayMainMenuOptions()
+{
+    Console.Clear();
+    Console.WriteLine(
+@$"Math Game Collection
+Menu
+{separator}
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Random Mode
+6. Game History
+7. Settings
+8. Exit
+(Type a number and press Enter to choose)");
+}
+
+void ChooseMainMenuOption()
+{
+    readResult = Console.ReadLine();
+
+    if (readResult != null)
+    {
+        switch (readResult)
+        {
+            case "1":
+                GameScreen("+");
+                break;
+            case "2":
+                GameScreen("-");
+                break;
+            case "3":
+                GameScreen("x");
+                break;
+            case "4":
+                GameScreen("/");
+                break;
+            case "5":
+                GameScreen("random");
+                break;
+            case "6":
+                GameHistory();
+                break;
+            case "7":
+                Settings();
+                break;
+            case "8":
+                Console.WriteLine("Exiting game.");
+                programRunning = false;
+                break;
+            default:
+                Console.WriteLine("Invalid input. Please enter a number from 1-8.");
+                validInput = false;
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input. Please enter a number from 1-8.");
+        validInput = false;
+    }
+}
+
+//Used in GameScreen() -
+void GameLoop(string operation)
+{
+    do
+    {
+        questionCount++;
+
+        DisplayGameHeader();
+
+        if (randomMode)
+        {
+            operation = PickRandomOperation(operation);
+        }
+
+        GetNumbers(operation);
+
+        Console.WriteLine($"{firstNumber} {operation} {secondNumber} = ?");
+
+        CalculateCorrectAnswer(operation);
+
+        GetUserAnswer();
+
+        SetAnswerText();
+
+        IncreaseScore();
+
+        DisplayAnswerResult(operation);
+
+        if (questionCount == questionLimit)
+        {
+            GetTime();
+
+            SaveScore(randomMode ? "R" : operation);
+
+            DisplayScore();
+
+            EndCurrentGame();
+        }
+
+        Console.Clear();
+    } while (gameRunning);
+} //Most of program happens here.
+
+void GameSetup(string operation)
+{
+    questionCount = 0;
+    score = 0;
+
+    startTime = DateTime.Now.TimeOfDay;
+
+    randomMode = operation == "random";
+    gameRunning = true;
+
+    Console.Clear();
+}
+
+void SetOperationLabel(string operation)
+{
+    switch (operation)
+    {
+        case "+":
+            operationLabel = "Addition";
+            break;
+        case "-":
+            operationLabel = "Subtraction";
+            break;
+        case "x":
+            operationLabel = "Multiplication";
+            break;
+        case "/":
+            operationLabel = "Division";
+            break;
+        case "random":
+            operationLabel = "Random";
+            break;
+    }
+}
+
+void DisplayGameHeader()
+{
+    Console.WriteLine(@$"Math Game Collection
+{operationLabel} Game
+{separator}");
+}
+
+void GetNumbers(string operation)
+{
+    if (operation == "/")
+    {
+        do
+        {
+            firstNumber = random.Next(lowerRange, upperRange);
+            secondNumber = random.Next(lowerRange, upperRange);
+        } while (firstNumber % secondNumber != 0 && firstNumber != secondNumber);
+    }
+    else
+    {
+        firstNumber = random.Next(lowerRange, upperRange);
+        secondNumber = random.Next(lowerRange, upperRange);
+    }
+}
+
+string PickRandomOperation(string operation)
+{
+    int pickOperation = random.Next(0, 4);
+
+    switch (pickOperation)
+    {
+        case 0:
+            operation = "+";
+            break;
+        case 1:
+            operation = "-";
+            break;
+        case 2:
+            operation = "x";
+            break;
+        case 3:
+            operation = "/";
+            break;
+    }
+
+    return operation;
+}
+
+void CalculateCorrectAnswer(string operation)
+{
+    switch (operation)
+    {
+        case "+":
+            calculationResult = firstNumber + secondNumber;
+            break;
+        case "-":
+            calculationResult = firstNumber - secondNumber;
+            break;
+        case "x":
+            calculationResult = firstNumber * secondNumber;
+            break;
+        case "/":
+            calculationResult = firstNumber / secondNumber;
+            break;
+    }
+}
+
+double GetUserAnswer()
+{
+    validInput = false;
+
+    do
+    {
+        readResult = Console.ReadLine();
+        if (readResult != null && Double.TryParse(readResult, out answer))
+        {
+            validInput = true;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a number.");
+        }
+    } while (!validInput);
+
+    return answer;
+}
+
+string SetAnswerText()
+{
+    if (answer == calculationResult)
+    {
+        outcomeText = "Correct!";
+    }
+    else
+    {
+        outcomeText = "Incorrect!";
+    }
+
+    return outcomeText;
+}
+
+void IncreaseScore()
+{
+    if (answer == calculationResult)
+        score++;
+}
+
+void DisplayAnswerResult(string operation)
+{
+    Console.WriteLine($"{outcomeText} {firstNumber} {operation} {secondNumber} = {calculationResult}\nScore: {score}/{questionCount}");
+    Console.WriteLine("Press Enter to continue.");
+    Console.ReadLine();
+}
+
+void GetTime()
+{
+    endTime = DateTime.Now.TimeOfDay;
+    timeTaken = (int)(endTime - startTime).TotalSeconds;
+}
+
+void SaveScore(string operation)
+{
+    gameResults[gamesPlayed] = $"   {gamesPlayed + 1}\t|   {operation}\t|  {score}/{questionCount}\t|  {timeTaken}";
+}
+
+void DisplayScore()
+{
+    Console.WriteLine(separator);
+    Console.WriteLine($"Game Over! Final Score: {score}/{questionCount}. Time: {timeTaken}s");
+}
+
+void EndCurrentGame()
+{
+    gameRunning = false;
+    gamesPlayed++;
+    Console.WriteLine("Press Enter to return to Menu.");
+    Console.ReadLine();
+}
+
+//Used in Settings() -
 void SetDifficulty()
 {
     Console.WriteLine(
@@ -334,22 +416,18 @@ Difficulty
             {
                 case "1":
                     upperRange = 24;
-
                     Console.WriteLine("Difficulty set to \"Easy\"");
                     break;
                 case "2":
                     upperRange = 48;
-
                     Console.WriteLine("Difficulty set to \"Medium\"");
                     break;
                 case "3":
                     upperRange = 96;
-
                     Console.WriteLine("Difficulty set to \"Hard\"");
                     break;
                 default:
                     validInput = false;
-
                     Console.WriteLine("Invalid input. Please enter a number from 1-3.");
                     break;
             }
@@ -372,6 +450,7 @@ Enter the number of questions per round.");
     do
     {
         readResult = Console.ReadLine();
+
         if (readResult != null && Int32.TryParse(readResult, out questionLimit))
         {
             validInput = true;
@@ -400,12 +479,14 @@ Clear game history? (y/n)");
     do
     {
         readResult = Console.ReadLine();
+
         if (readResult != null)
         {
             switch (readResult.ToLower())
             {
                 case "y":
                     gameResults = new string[20];
+                    gamesPlayed = 0;
                     validInput = true;
 
                     Console.WriteLine("Deleting game history.");
