27ee91e4713b7d07a87c3ce14ac337e72738b9d7
Pugnetta <115831639+Pugnetta@users.noreply.github.com>
2024-02-04 16:33:47 +0100

    removed using statements

--- Pugnetta.MathGame/Pugnetta.MathGame/Game.cs
+++ Pugnetta.MathGame/Pugnetta.MathGame/Game.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pugnetta.MathGame.Game;
+﻿namespace Pugnetta.MathGame.Game;
 
 public record Game
 {
