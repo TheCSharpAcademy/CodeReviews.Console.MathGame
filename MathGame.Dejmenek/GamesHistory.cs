7f86e4b89956f7307c5dd9a525c9da61badeacd6
Dejmenek <dejmen14567@gmail.com>
2024-02-05 14:41:20 +0100

    Add option to retrieve player's games history

---
+++ MathGame.Dejmenek/GamesHistory.cs
@@ -0,0 +1,33 @@
+﻿namespace MathGame.Dejmenek
+{
+    internal class GamesHistory
+    {
+        private readonly List<Game> _Games = new();
+
+        public GamesHistory() { }
+
+        public void Show() {
+            if (IsAnyGame())
+            {
+                int index = 1;
+                foreach (var game in _Games)
+                {
+                    Console.WriteLine($"Game {index}: Time: {game.FormattedTimeTaken}, Score: {game.Score}/{game.NumberOfQuestions}");
+                    index++;
+                }
+            }
+            else {
+                Console.WriteLine("You haven't played any game yet");
+            }
+            
+        }
+
+        private bool IsAnyGame() {
+            return _Games.Count > 0;
+        }
+
+        public void AddGame(Game game) { 
+            _Games.Add(game);
+        }
+    }
+}
