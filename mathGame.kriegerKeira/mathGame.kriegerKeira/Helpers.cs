a53646b5df76187fdf00761ba4520c9d197a1166
Keira Krieger <keira.f.krieger@gmail.com>
2024-02-13 17:12:02 -0800

    Created menuand GetName

---
+++ mathGame.kriegerKeira/mathGame.kriegerKeira/Helpers.cs
@@ -0,0 +1,20 @@
+﻿public static class Helpers
+{
+    public static string GetName()
+    {
+        Console.Write("Please enter your name: ");
+        var name = Console.ReadLine();
+        while (string.IsNullOrWhiteSpace(name))
+        {
+            Console.WriteLine("Name cannot be empty, press any key to try again");
+            Console.ReadKey();
+            Console.Clear();
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+            Console.Clear();
+        }
+        return name;
+
+    }
+
+}
