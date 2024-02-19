724f363029baf5b926319aaefa3b5db71068b380
Maggie McDonald <erin.aka.maggie@gmail.com>
2024-02-19 13:35:57 +0100

    Create Project & Add starting code

---
+++ MathGame/Program.cs
@@ -0,0 +1,37 @@
+﻿using MathGame;
+
+OpeningSequence();
+
+static void OpeningSequence()
+{
+    Console.WriteLine("Welcome to the Math Game!");
+    Console.WriteLine("Please select which operation you'd like to practice today.");
+    Console.WriteLine(" * a - Addition");
+    Console.WriteLine(" * d - Division");
+    Console.WriteLine(" * m - Multiplication");
+    Console.WriteLine(" * s - Subtraction");
+
+    var operation = Console.ReadLine();
+
+    switch (operation)
+    {
+        case ("a"):
+            break;
+    }
+}
+
+static void Addition()
+{
+    Console.Write("You've chosen addition!");
+    Console.WriteLine(" Let's begin!");
+}
+
+static void SaveGame(string operation, List<int> numbers, bool IsAnswerCorrect)
+{
+
+}
+
+static List<SavedGame> RetrieveGame(string username)
+{
+    return null;
+}
---
+++ MathGame/SavedGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal class SavedGame
+    {
+        public string Username { get; set; }
+        public string Operation { get; set; }
+        public List<int> Operands { get; set; }
+        public bool IsAnswerCorrect { get; set; }
+
+        public SavedGame(string username, string operation, List<int> operands, bool isAnswerCorrect)
+        {
+            Username = username;
+            Operation = operation;
+            Operands = operands;
+            IsAnswerCorrect = isAnswerCorrect;
+        }
+    }
+}
