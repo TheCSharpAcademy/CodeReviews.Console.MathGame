6dad881e8aba6d093e56d99dee2ad77a608e1e9e
Joshua Abbey <Joshuaabbey2022@gmail.com>
2024-02-21 21:59:24 +0000

    psuedo

--- MathGame.Rxxyxd/Program.cs
+++ MathGame.Rxxyxd/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//                              Methods to create
+// 
+// StartGame()
+// GenerateNumbers(int[] numbers[], int questionsCount, bool isDivision = false)
+// AdditionQuiz(int[] numbers)
+// SubtractionQuiz(int[] numbers)
+// MultiplicationQuiz(int[] numbers)
+// DivisionQuiz(int[] numbers)
+// AppendGameHistoryArray(string quizName, int score)
+// GameHistory(string quizName = null)
+//
