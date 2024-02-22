288fcf3ea1c56925e39fa1a839b3392b2d281f1b
Joshua Abbey <Joshuaabbey2022@gmail.com>
2024-02-22 06:44:13 +0000

    Code Cleanup, Division Complete, IsPrime() method added

--- MathGame.Rxxyxd/Program.cs
+++ MathGame.Rxxyxd/Program.cs
@@ -9,7 +9,7 @@ string? userInput = "";
 int menuOption;
 bool inputIsValid;
 bool exit = false;
-
+int questionCount;
 
 // Main Menu
 while (!exit)
@@ -23,81 +23,61 @@ while (!exit)
     Console.WriteLine("5.\tGame History");
     Console.WriteLine();
     Console.WriteLine("Select an option 1-5 or type 'exit': ");
-    try
+
+    userInput = Console.ReadLine();
+
+    if (userInput == null || userInput == "")
     {
-        userInput = Console.ReadLine();
+        throw new NullReferenceException("Menu option cannot be empty."); // May need to change the exception that is thrown here for a more fitting one.
 
-        if (userInput == null || userInput == "")
-        {
-            throw new NullReferenceException("Menu option cannot be empty."); // May need to change the exception that is thrown here for a more fitting one.
+    }
+    else if (userInput.ToLower() == "exit")
+    {
+        exit = true;
+        continue;
+    }
+    inputIsValid = int.TryParse(userInput, out menuOption);
+    if (!inputIsValid)
+    {
+        throw new FormatException("Menu option must be an integral number between 1 and 5.");
+    }
 
-        }
-        else if (userInput.ToLower() == "exit")
-        {
-            exit = true;
-            continue;
-        }
-        inputIsValid = int.TryParse(userInput, out menuOption);
-        if (!inputIsValid)
-        {
-            throw new FormatException("Menu option must be an integral number between 1 and 5.");
-        }
+    try // Removed StartGame() - Felt it did not make sense to have code that did not need to be reused in a method.
+    {
+        questionCount = QuestionCount();
 
-        try
+        switch (menuOption)
         {
-            StartGame(menuOption); // Method is probably not needed. Could be removed in future commits.
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"An error has occured: {ex.Message}");
-            Thread.Sleep(1000);
-            Console.Clear();
+            // Addition Quiz
+            case 1:
+                Quiz(1, questionCount);
+                break;
+            // Subtraction Quiz
+            case 2:
+                Quiz(2, questionCount);
+                break;
+            // Multiplication Quiz
+            case 3:
+                Quiz(3, questionCount);
+                break;
+            // Division Quiz
+            case 4:
+                Quiz(4, questionCount);
+                break;
+            // Game History
+            case 5:
+                GameHistory();
+                break;
+            // menuOption does not equal 1 through 5 (return to menu)
+            default:
+                break;
         }
     }
-    catch (FormatException ex)
+    catch (Exception ex)
     {
-        Console.WriteLine($"Error: {ex.Message}");
+        Console.WriteLine($"An error has occured: {ex.Message}");
         Thread.Sleep(1000);
-    }
-    catch (NullReferenceException ex)
-    {
-        Console.WriteLine($"Error: {ex.Message}");
-        Thread.Sleep(1000);
-    }
-}
-
-
-void StartGame(int userSelection)
-{
-    // StartGame() Variables
-    int questionCount;
-    questionCount = QuestionCount();
-
-    switch (userSelection)
-    {
-        // Addition Quiz
-        case 1:
-            Quiz(1, questionCount);
-            break;
-        // Subtraction Quiz
-        case 2:
-            Quiz(2, questionCount);
-            break;
-        // Multiplication Quiz
-        case 3:
-            Quiz(3, questionCount);
-            break;
-        // Division Quiz
-        case 4:
-            Quiz(4, questionCount);
-            break;
-        // Game History
-        case 5:
-            GameHistory();
-            break;
-        // userSelection does not equal 1 through 5 (return to menu)
-        default:
-            break;
+        Console.Clear();
     }
 }
 
@@ -128,8 +108,8 @@ int QuestionCount()
                 inputIsValid = true;
             }
         }
-        catch (NullReferenceException ex) { Console.WriteLine($"Error: {ex.Message}"); }
-        catch (FormatException ex) { Console.WriteLine($"Error: {ex.Message}"); }
+        catch (NullReferenceException ex) { Console.WriteLine($"Error in QuestionCount(): {ex.Message}"); }
+        catch (FormatException ex) { Console.WriteLine($"Error in QuestionCount(): {ex.Message}"); }
     } while (inputIsValid == false);
     return questionCount;
 }
@@ -152,100 +132,116 @@ void Quiz(int mode, int questionCount)
     int userAnswer;
     bool inputIsValid;
     int score = 0;
-    if (mode >= 5)
+
+    switch (mode)
     {
-        throw new ArgumentOutOfRangeException($"{mode} is not a valid mode.\n Possible Modes are: 1 - Addition\n2 - Subtraction\n3 - Multiplication\n4 - Division");
-    }
-    else
-    {
-        switch (mode)
-        {
-            case 1: // addition
+        case 1: // addition
                 // #1 Generates Numbers for the questions with GenerateNumbers() Method
-                int[] numbers = GenerateNumbers(questionCount);
-                /*
-                #2 Loops through numbers Array, displaying each question until i is equal to questionCount integer.
-                The do-while loop is for input validation. I think there may be a better approach for this.
-                This comment can be applied to cases 1 through 3.
-                */
-                for (int i = 0; i != questionCount * 2; i += 2)
+            int[] numbers = GenerateNumbers(questionCount);
+            /*
+            #2 Loops through numbers Array, displaying each question until i is equal to questionCount integer.
+            The do-while loop is for input validation. I think there may be a better approach for this.
+            This comment can be applied to cases 1 through 3.
+            */
+            for (int i = 0; i != questionCount * 2; i += 2)
+            {
+                do
                 {
-                    do
+                    Console.Clear();
+                    inputIsValid = false;
+                    answer = numbers[i] + numbers[i + 1];
+                    Console.WriteLine($"{numbers[i]} + {numbers[i + 1]}");
+                    userInput = Console.ReadLine();
+                    if (int.TryParse(userInput, out userAnswer))
                     {
-                        Console.Clear();
-                        inputIsValid = false;
-                        answer = numbers[i] + numbers[i + 1];
-                        Console.WriteLine($"{numbers[i]} + {numbers[i + 1]}");
-                        userInput = Console.ReadLine();
-                        if (int.TryParse(userInput, out userAnswer))
-                        {
-                            inputIsValid = true;
-                            score = CheckAnswer(userAnswer, answer, score);
-                        }
-                        else
-                            continue;
-                    } while (inputIsValid == false);
-                }
-                Console.WriteLine($"You got {score}/{questionCount}");
-                Console.WriteLine();
-                Console.WriteLine("Press Enter to continue.");
-                Console.ReadLine();
-                break;
-            case 2: // subtraction
-                numbers = GenerateNumbers(questionCount);
-                for (int i = 0; i != questionCount * 2; i += 2)
+                        inputIsValid = true;
+                        score = CheckAnswer(userAnswer, answer, score);
+                    }
+                    else
+                        continue;
+                } while (inputIsValid == false);
+            }
+            Console.WriteLine($"You got {score}/{questionCount}");
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to continue.");
+            Console.ReadLine();
+            break;
+        case 2: // subtraction
+            numbers = GenerateNumbers(questionCount);
+            for (int i = 0; i != questionCount * 2; i += 2)
+            {
+                do
                 {
-                    do
+                    Console.Clear();
+                    inputIsValid = false;
+                    answer = numbers[i] - numbers[i + 1];
+                    Console.WriteLine($"{numbers[i]} - {numbers[i + 1]}");
+                    userInput = Console.ReadLine();
+                    if (int.TryParse(userInput, out userAnswer))
                     {
-                        Console.Clear();
-                        inputIsValid = false;
-                        answer = numbers[i] - numbers[i + 1];
-                        Console.WriteLine($"{numbers[i]} - {numbers[i + 1]}");
-                        userInput = Console.ReadLine();
-                        if (int.TryParse(userInput, out userAnswer))
-                        {
-                            inputIsValid = true;
-                            score = CheckAnswer(userAnswer, answer, score);
-                        }
-                        else
-                            continue;
-                    } while (inputIsValid == false);
-                }
-                Console.WriteLine($"You got {score}/{questionCount}");
-                Console.WriteLine();
-                Console.WriteLine("Press Enter to continue.");
-                Console.ReadLine();
-                break;
-            case 3: // Multiplication
-                numbers = GenerateNumbers(questionCount);
-                for (int i = 0; i != questionCount * 2; i += 2)
+                        inputIsValid = true;
+                        score = CheckAnswer(userAnswer, answer, score);
+                    }
+                    else
+                        continue;
+                } while (inputIsValid == false);
+            }
+            Console.WriteLine($"You got {score}/{questionCount}");
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to continue.");
+            Console.ReadLine();
+            break;
+        case 3: // Multiplication
+            numbers = GenerateNumbers(questionCount);
+            for (int i = 0; i != questionCount * 2; i += 2)
+            {
+                do
                 {
-                    do
+                    Console.Clear();
+                    inputIsValid = false;
+                    answer = numbers[i] * numbers[i + 1];
+                    Console.WriteLine($"{numbers[i]} x {numbers[i + 1]}");
+                    userInput = Console.ReadLine();
+                    if (int.TryParse(userInput, out userAnswer))
                     {
-                        Console.Clear();
-                        inputIsValid = false;
-                        answer = numbers[i] * numbers[i + 1];
-                        Console.WriteLine($"{numbers[i]} x {numbers[i + 1]}");
-                        userInput = Console.ReadLine();
-                        if (int.TryParse(userInput, out userAnswer))
-                        {
-                            inputIsValid = true;
-                            score = CheckAnswer(userAnswer, answer, score);
-                        }
-                        else
-                            continue;
-                    } while (inputIsValid == false);
-                }
-                Console.WriteLine($"You got {score}/{questionCount}");
-                Console.WriteLine();
-                Console.WriteLine("Press Enter to continue.");
-                Console.ReadLine();
-                break;
-            case 4: // Division - TODO
-                break;
-        }
+                        inputIsValid = true;
+                        score = CheckAnswer(userAnswer, answer, score);
+                    }
+                    else
+                        continue;
+                } while (inputIsValid == false);
+            }
+            Console.WriteLine($"You got {score}/{questionCount}");
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to continue.");
+            Console.ReadLine();
+            break;
+        case 4: // Division - TODO
+            numbers = GenerateNumbers(questionCount, isDivision: true);
+            for (int i = 0; i != questionCount * 2; i += 2)
+            {
+                do
+                {
+                    Console.Clear();
+                    inputIsValid = false;
+                    answer = numbers[i] / numbers[i + 1];
+                    Console.WriteLine($"{numbers[i]} / {numbers[i + 1]}");
+                    userInput = Console.ReadLine();
+                    if (int.TryParse(userInput, out userAnswer))
+                    {
+                        inputIsValid = true;
+                        score = CheckAnswer(userAnswer, answer, score);
+                    }
+                    else
+                        continue;
+                } while (inputIsValid == false);
+            }
+            break;
+        default:
+            throw new ArgumentOutOfRangeException($"{mode} is not a valid mode.\n Possible Modes are: 1 - Addition\n2 - Subtraction\n3 - Multiplication\n4 - Division");
     }
 }
+
 
 // Simple method for scoring and checking that answer and UserInput are equal.
 int CheckAnswer(int userAnswer, int answer, int score)
@@ -254,7 +250,7 @@ int CheckAnswer(int userAnswer, int answer, int score)
     {
         Console.WriteLine("Correct! +1 score!");
         Thread.Sleep(1000);
-        return score+1;
+        return score + 1;
     }
     else
     {
@@ -270,7 +266,22 @@ int[] GenerateNumbers(int questionCount, bool isDivision = false)
     int[] generatedNumbers = new int[questionCount * 2];
     if (isDivision)
     {
-        // TODO
+        for (int i = 0; i != (questionCount * 2); i++)
+        {
+            if (i % 2 == 0)
+                do
+                {
+                    generatedNumbers[i] = random.Next(1, 100);
+                } while (IsPrime(generatedNumbers[i]));
+            else
+            {
+                do
+                {
+                    generatedNumbers[i] = random.Next(2,10);
+
+                }while (generatedNumbers[i-1] % generatedNumbers[i] != 0);
+            }
+        }
     }
 
     else
@@ -287,4 +298,21 @@ int[] GenerateNumbers(int questionCount, bool isDivision = false)
 void GameHistory()
 {
 
+}
+
+// Method determines whether a number is a prime number. - Required for division gamemode.
+bool IsPrime(int number)
+{
+    if (number <=1) return false;
+    if (number == 2) return true;
+    if (number % 2 == 0) return false;
+
+    var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+    for (int i = 3; i <= boundary; i+=2)
+    {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
 }
