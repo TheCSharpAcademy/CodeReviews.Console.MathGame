75750ac90aa93933f15f23f45ae4c2ea1d36d950
Pedro Antonio <pedromila2000@gmail.com>
2024-02-23 16:15:56 -0300

    Submodule fixed

---
+++ MathGamer/Program.cs
@@ -0,0 +1,36 @@
+﻿using MathGamer;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace MathGame
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+
+
+
+            string name = Helpers.GetName();
+            menu.ShowMenu(name);
+        }
+
+
+      
+
+        
+
+
+       
+    }
+}
+
+
+
