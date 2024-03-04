41592400b688ffb25192be882bb68a604f9ba2bc
an2niy85 <an2niy85@mail.ru>
2024-03-04 21:23:35 +0400

    Project finished!

---
+++ AntonKrasnoperov.MathGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AntonKrasnoperov.MathGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Finished!");
+        }
+    }
+}
