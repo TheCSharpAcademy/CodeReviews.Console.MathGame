84b8dabf27d4d73c05d7d3f7ae4dd711c66e91a6
jra <julien.gault.50@gmail.com>
2024-03-28 15:56:55 +0100

    Initial skeleton

--- MathGame.jrazorx/MathGame.jrazorx/Program.cs
+++ MathGame.jrazorx/MathGame.jrazorx/Program.cs
@@ -1,1 +1,68 @@
-﻿
+﻿// Menu item chosen by the user
+char menuItemChosen;
+
+Greetings();
+DisplayMenu();
+menuItemChosen = ChooseMenuItem();
+MenuItemAction(menuItemChosen);
+
+// Greets the user
+void Greetings()
+{
+
+}
+
+// Displays the menu
+void DisplayMenu()
+{
+
+}
+
+// Asks the user to choose a menu item
+// returns user input
+char ChooseMenuItem()
+{
+    return ' ';
+}
+
+// Executes the action corresponding to the menu item chosen by the user
+void MenuItemAction(char menuItemChosen)
+{
+
+}
+
+// Play Addition Game
+void AdditionGame()
+{
+
+}
+
+// Play Subtraction Game
+void SubtractionGame()
+{
+
+}
+
+// Play Multiplication Game
+void MultiplicationGame()
+{
+
+}
+
+// Play Division Game
+void DivisionGame()
+{
+
+}
+
+// Display the List containing the history of previous games played
+void DisplayGameHistory()
+{
+
+}
+
+// Saves the last game played in the List
+void SaveGame()
+{
+
+}
