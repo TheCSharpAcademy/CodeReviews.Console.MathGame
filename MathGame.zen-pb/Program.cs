5249bbabb97e5cace08eaa249d4da17b4c49f72d
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-04-08 21:04:27 +0800

    Add menu and basic functionalities of the program

--- MathGame.zen-pb/Program.cs
+++ MathGame.zen-pb/Program.cs
@@ -1,2 +1,144 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Random randomNumbers = new();
+
+int x, y, answer, userAnswer;
+
+bool validInput = false;
+
+string? userInput;
+
+Console.WriteLine("Welcome to MATH GAME!");
+Console.WriteLine("Choose what type of game you want to play: ");
+Console.WriteLine("1. Multiplication");
+Console.WriteLine("2. Division");
+Console.WriteLine("3. Addition");
+Console.WriteLine("4. Subtraction");
+Console.WriteLine("5. History");
+
+userInput = Console.ReadLine();
+
+if (userInput != null)
+{
+    validInput = int.TryParse(userInput, out int chosenGame);
+
+    switch (chosenGame)
+    {
+        case 1:
+            Multiplication();
+            break;
+
+        case 2:
+            Division();
+            break;
+
+        case 3:
+            Addition();
+            break;
+
+        case 4:
+            Subtraction();
+            break;
+
+        case 5:
+            ShowHistory();
+            break;
+    }
+}
+
+
+void Multiplication()
+{
+    Console.WriteLine("Multiplication - Enter the correct answer!");
+    x = GenerateRandomNumber();
+    y = GenerateRandomNumber();
+    answer = x * y;
+
+    userInput = Console.ReadLine();
+
+    if (userInput != null)
+    {
+        validInput = int.TryParse(userInput, out userAnswer);
+
+        if (userAnswer == answer)
+        {
+            Console.WriteLine("Correct answer!");
+        }
+    }
+}
+
+void Division()
+{
+    Console.WriteLine("Division - Enter the correct answer!");
+    x = GenerateRandomNumber();
+    y = GenerateRandomNumber();
+
+    while (y == 0)
+    {
+        y = GenerateRandomNumber();
+    }
+
+    answer = x / y;
+
+    userInput = Console.ReadLine();
+
+    if (userInput != null)
+    {
+        validInput = int.TryParse(userInput, out userAnswer);
+
+        if (userAnswer == answer)
+        {
+            Console.WriteLine("Correct answer!");
+        }
+    }
+}
+
+void Addition()
+{
+    Console.WriteLine("Addition - Enter the correct answer!");
+    x = GenerateRandomNumber();
+    y = GenerateRandomNumber();
+    answer = x + y;
+
+    userInput = Console.ReadLine();
+
+    if (userInput != null)
+    {
+        validInput = int.TryParse(userInput, out userAnswer);
+
+        if (userAnswer == answer)
+        {
+            Console.WriteLine("Correct answer!");
+        }
+    }
+}
+
+void Subtraction()
+{
+    Console.WriteLine("Subtraction - Enter the correct answer!");
+    x = GenerateRandomNumber();
+    y = GenerateRandomNumber();
+    answer = x - y;
+
+    userInput = Console.ReadLine();
+
+    if (userInput != null)
+    {
+        validInput = int.TryParse(userInput, out userAnswer);
+
+        if (userAnswer == answer)
+        {
+            Console.WriteLine("Correct answer!");
+        }
+    }
+}
+
+int GenerateRandomNumber()
+{
+    return randomNumbers.Next(0, 101);
+}
+
+void ShowHistory()
+{
+    Console.WriteLine("Under Construction...");
+}
