83ee42c48067c6225ac127b42801a99dd6dec71a
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-04-08 22:37:08 +0800

    Add loop to simulate 5 questions each calculation

--- MathGame.zen-pb/Program.cs
+++ MathGame.zen-pb/Program.cs
@@ -5,148 +5,202 @@ Random randomNumbers = new();
 int x, y, answer, userAnswer;
 
 bool validInput = false;
+bool isDone = false;
 
 string? userInput;
 
-Console.Clear();
-Console.WriteLine("Welcome to MATH GAME!\n");
-Console.WriteLine("Choose what type of game you want to play: ");
-Console.WriteLine("1. Multiplication");
-Console.WriteLine("2. Division");
-Console.WriteLine("3. Addition");
-Console.WriteLine("4. Subtraction");
-Console.WriteLine("5. History\n");
-
-userInput = Console.ReadLine();
-
-if (userInput != null)
-{
-    validInput = int.TryParse(userInput, out int chosenGame);
-
-    switch (chosenGame)
-    {
-        case 1:
-            Multiplication();
-            break;
-
-        case 2:
-            Division();
-            break;
-
-        case 3:
-            Addition();
-            break;
-
-        case 4:
-            Subtraction();
-            break;
-
-        case 5:
-            ShowHistory();
-            break;
-    }
-}
-
-
-void Multiplication()
+do
 {
     Console.Clear();
-    Console.WriteLine("Multiplication - Enter the correct answer!");
-    x = GenerateRandomNumber();
-    y = GenerateRandomNumber();
-
-    answer = x * y;
-    Console.WriteLine($"{x} x {y}");
+    Console.WriteLine("Welcome to MATH GAME!\n");
+    Console.WriteLine("Choose what type of game you want to play: ");
+    Console.WriteLine("1. Multiplication");
+    Console.WriteLine("2. Division");
+    Console.WriteLine("3. Addition");
+    Console.WriteLine("4. Subtraction");
+    Console.WriteLine("5. History\n");
 
     userInput = Console.ReadLine();
 
+
     if (userInput != null)
     {
-        validInput = int.TryParse(userInput, out userAnswer);
-
-        if (userAnswer == answer)
+        if (userInput.ToLower() == "exit")
         {
-            Console.WriteLine("Correct answer!");
+            isDone = true;
+        }
+
+        validInput = int.TryParse(userInput, out int chosenGame);
+
+        if (validInput)
+            switch (chosenGame)
+            {
+                case 1:
+                    Multiplication();
+                    break;
+
+                case 2:
+                    Division();
+                    break;
+
+                case 3:
+                    Addition();
+                    break;
+
+                case 4:
+                    Subtraction();
+                    break;
+
+                case 5:
+                    ShowHistory();
+                    break;
+            }
+    }
+
+} while (isDone == false);
+
+void Multiplication()
+{
+    string answerStatement = "Enter the correct answer";
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Clear();
+        Console.WriteLine($"Multiplication({i + 1}/5) - {answerStatement}!");
+        x = GenerateRandomNumber();
+        y = GenerateRandomNumber();
+
+        answer = x * y;
+        Console.WriteLine($"{x} x {y}");
+
+        userInput = Console.ReadLine();
+
+        if (userInput != null)
+        {
+            validInput = int.TryParse(userInput, out userAnswer);
+
+            if (userAnswer == answer)
+            {
+                answerStatement = "Correct";
+            }
+            else
+                answerStatement = "Incorrect";
         }
     }
+
+    Console.Clear();
+    Console.WriteLine($"Multiplication(5/5) - {answerStatement}!");
+    Console.WriteLine("\n\rPress Enter to return to the Main menu.");
+    Console.ReadLine();
 }
 
 void Division()
 {
-    Console.Clear();
-    Console.WriteLine("Division - Enter the correct answer!");
+    string answerStatement = "Enter the correct answer";
 
-    do
+    for (int i = 0; i < 5; i++)
     {
-        x = GenerateRandomNumber();
-        y = GenerateRandomNumber();
+        Console.Clear();
+        Console.WriteLine($"Division({i + 1}/5) - {answerStatement}!");
 
-        while (y == 0)
+        do
         {
+            x = GenerateRandomNumber();
             y = GenerateRandomNumber();
-        }
 
-    } while (y > x || x % y != 0);
+            while (y == 0)
+            {
+                y = GenerateRandomNumber();
+            }
 
-    answer = x / y;
-    Console.WriteLine($"{x} / {y}");
+        } while (y > x || x % y != 0);
 
-    userInput = Console.ReadLine();
+        answer = x / y;
+        Console.WriteLine($"{x} / {y}");
 
-    if (userInput != null)
-    {
-        validInput = int.TryParse(userInput, out userAnswer);
+        userInput = Console.ReadLine();
 
-        if (userAnswer == answer)
+        if (userInput != null)
         {
-            Console.WriteLine("Correct answer!");
+            validInput = int.TryParse(userInput, out userAnswer);
+
+            if (userAnswer == answer)
+            {
+                Console.WriteLine("Correct answer!");
+            }
         }
     }
+
+    Console.Clear();
+    Console.WriteLine($"Division(5/5) - {answerStatement}!");
+    Console.WriteLine("\n\rPress Enter to return to the Main menu.");
+    Console.ReadLine();
 }
 
 void Addition()
 {
-    Console.Clear();
-    Console.WriteLine("Addition - Enter the correct answer!");
-    x = GenerateRandomNumber();
-    y = GenerateRandomNumber();
-    answer = x + y;
+    string answerStatement = "Enter the correct answer";
 
-    Console.WriteLine($"{x} + {y}");
-    userInput = Console.ReadLine();
-
-    if (userInput != null)
+    for (int i = 0; i < 5; i++)
     {
-        validInput = int.TryParse(userInput, out userAnswer);
+        Console.Clear();
+        Console.WriteLine($"Addition({i + 1}/5) - {answerStatement}!");
+        x = GenerateRandomNumber();
+        y = GenerateRandomNumber();
+        answer = x + y;
 
-        if (userAnswer == answer)
+        Console.WriteLine($"{x} + {y}");
+        userInput = Console.ReadLine();
+
+        if (userInput != null)
         {
-            Console.WriteLine("Correct answer!");
+            validInput = int.TryParse(userInput, out userAnswer);
+
+            if (userAnswer == answer)
+            {
+                Console.WriteLine("Correct answer!");
+            }
         }
     }
+
+    Console.Clear();
+    Console.WriteLine($"Addition(5/5) - {answerStatement}!");
+    Console.WriteLine("\n\rPress Enter to return to the Main menu.");
+    Console.ReadLine();
+
 }
 
 void Subtraction()
 {
-    Console.Clear();
-    Console.WriteLine("Subtraction - Enter the correct answer!");
-    x = GenerateRandomNumber();
-    y = GenerateRandomNumber();
+    string answerStatement = "Enter the correct answer";
 
-    answer = x - y;
-    Console.WriteLine($"{x} - {y}");
-
-    userInput = Console.ReadLine();
-
-    if (userInput != null)
+    for (int i = 0; i < 5; i++)
     {
-        validInput = int.TryParse(userInput, out userAnswer);
+        Console.Clear();
+        Console.WriteLine($"Subtraction({i + 1}/5) - {answerStatement}!");
+        x = GenerateRandomNumber();
+        y = GenerateRandomNumber();
 
-        if (userAnswer == answer)
+        answer = x - y;
+        Console.WriteLine($"{x} - {y}");
+
+        userInput = Console.ReadLine();
+
+        if (userInput != null)
         {
-            Console.WriteLine("Correct answer!");
+            validInput = int.TryParse(userInput, out userAnswer);
+
+            if (userAnswer == answer)
+            {
+                Console.WriteLine("Correct answer!");
+            }
         }
     }
+
+    Console.Clear();
+    Console.WriteLine($"Subtraction(5/5) - {answerStatement}!");
+    Console.WriteLine("\n\rPress Enter to return to the Main menu.");
+    Console.ReadLine();
 }
 
 int GenerateRandomNumber()
@@ -156,5 +210,6 @@ int GenerateRandomNumber()
 
 void ShowHistory()
 {
-    Console.WriteLine("Under Construction...");
+    Console.WriteLine("Under Construction...Press Enter to return to Main menu.");
+    Console.ReadLine();
 }
