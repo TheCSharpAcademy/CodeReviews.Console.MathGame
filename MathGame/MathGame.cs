f0d36f6cd60f2c1e69fd0e1bba1bb2926bce882b
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2024-04-09 21:53:55 -0400

    Finished Project

---
+++ MathGame/MathGame.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class MathGame
+    {
+
+        public List<string> RecordOfOperations { get; set; } = new List<string>();
+
+        public int SummationOperation(int a, int b)
+        {
+            RecordOfOperations.Add($"{a} + {b} = {a + b}");
+            return a + b;
+        }
+
+        public int SubtractionOperation(int a, int b)
+        {
+            RecordOfOperations.Add($"{a} - {b} = {a - b}");
+            return a - b;
+        }
+
+        public int MultiplicationOperation(int a, int b)
+        {
+            RecordOfOperations.Add($"{a} * {b} = {a * b}");
+            return a * b;
+        }
+
+        public int DivisionOperation(int a, int b)
+        {
+            while (a < 0 || a > 100)
+            {
+                Console.WriteLine("Please Enter a valid dividend, valid values are from 0 - 100");
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (a % b != 0)
+            {
+                // don't show anything since division resulted in a floating point value
+                return int.MaxValue;
+            }
+            else
+            {
+                RecordOfOperations.Add($"{a} / {b} = {a / b}");
+                return a / b;
+            }
+        }
+
+
+        public void DisplayMenu()
+        {
+            Console.WriteLine("Welcome to the Math Game");
+            Console.WriteLine("1) Summation Operation");
+            Console.WriteLine("2) Subtraction Operation");
+            Console.WriteLine("3) Multiplication Operation");
+            Console.WriteLine("4) Division Operation");
+            Console.WriteLine("5) Show Past Operations");
+            Console.WriteLine("6) Quit");
+        }
+    }
+}
---
+++ MathGame/Program.cs
@@ -0,0 +1,138 @@
+﻿using Game;
+
+var mathGame = new MathGame();
+
+var gameOver = false;
+int selection;
+int result;
+int firstNumber;
+int secondNumber;
+int userResponse;
+int score = 0;
+Random random = new Random();
+
+while (!gameOver)
+{
+    mathGame.DisplayMenu();
+
+    firstNumber = -1;
+    secondNumber = -1;
+
+    try
+    {
+        selection = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("\n");
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Please Enter a Valid Value: 1 - 6");
+        selection = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("\n");
+    }
+
+    firstNumber = random.Next(1, 101);
+    secondNumber = random.Next(1, 101);
+
+    switch (selection)
+    {
+        case 1:
+            Console.WriteLine($"{firstNumber} + {secondNumber} = ??");
+            result = mathGame.SummationOperation(firstNumber, secondNumber);
+            userResponse = GetUserResponse();
+            Console.WriteLine($"The correct result is: ");
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {result}");
+            score = validateResult(result, userResponse, score);
+            Console.WriteLine("\n");
+            break;
+        case 2:
+            Console.WriteLine($"{firstNumber} - {secondNumber} = ??");
+            result = mathGame.SubtractionOperation(firstNumber, secondNumber);
+            userResponse = GetUserResponse();
+
+            Console.WriteLine($"The correct result is: ");
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {result}");
+            score = validateResult(result, userResponse, score);
+            Console.WriteLine("\n");
+            break;
+        case 3:
+            Console.WriteLine($"{firstNumber} * {secondNumber} = ??");
+            result = mathGame.MultiplicationOperation(firstNumber, secondNumber);
+            userResponse = GetUserResponse();
+            Console.WriteLine($"The correct result is: ");
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {result}");
+            score = validateResult(result, userResponse, score);
+
+            Console.WriteLine("\n");
+            break;
+        case 4:
+            Console.WriteLine($"{firstNumber} / {secondNumber} = ??");
+            result = mathGame.DivisionOperation(firstNumber, secondNumber);
+            if (result != int.MaxValue)
+            {
+                userResponse = GetUserResponse();
+                Console.WriteLine($"The correct result is: ");
+                Console.WriteLine($"{firstNumber} / {secondNumber} = {result}");
+                score = validateResult(result, userResponse, score);
+            }
+            else
+            {
+                Console.WriteLine("The result was not an integer.");
+            }
+            Console.WriteLine("\n");
+            break;
+        case 5:
+            Console.WriteLine("Previous Operations:");
+            mathGame.RecordOfOperations.ForEach(Console.WriteLine);
+            Console.WriteLine($"Your total score is: {score}");
+            Console.WriteLine("\n");
+            break;
+        case 6:
+            gameOver = true;
+            Console.WriteLine("\n");
+            break;
+        default:
+            Console.WriteLine("Please enter a valid option: 1 - 6");
+            Console.WriteLine("\n");
+            break;
+    }
+
+
+}
+
+static int GetUserResponse()
+{
+    int response = 0;
+    bool invalid = true;
+    Console.WriteLine("Please enter your response.");
+    while (invalid)
+    {
+        try
+        {
+            response = Convert.ToInt32(Console.ReadLine());
+            invalid = false;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Please enter a valid Integer Value");
+        }
+    }
+    return response;
+}
+
+Console.WriteLine("Thanks for playing this numbers game");
+
+static int validateResult(int result, int userResponse, int score)
+{
+    if (userResponse == result)
+    {
+        Console.WriteLine("You answer correctly; You earned 5 pts.");
+        score += 5;
+    }
+    else
+    {
+        Console.WriteLine($"Try again");
+        score += 0;
+    }
+
+    return score;
+}
