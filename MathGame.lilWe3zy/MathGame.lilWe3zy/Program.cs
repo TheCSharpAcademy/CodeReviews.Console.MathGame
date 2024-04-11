dae6dabe481f2045864f2aad6935271fc9c8dbbf
micro <arminnel@proton.me>
2024-04-11 19:40:06 +0200

    add main menu selection
    - Created generic range value generation based on input difficulty
    - Created generic operation generator based on input difficulty

--- MathGame.lilWe3zy/MathGame.lilWe3zy/Program.cs
+++ MathGame.lilWe3zy/MathGame.lilWe3zy/Program.cs
@@ -1,3 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MathGame.lilWe3zy;
 
-Console.WriteLine("Hello, World!");
+/*
+ * Math game that contains 4 basic operations:
+ *      1. - addition (+)
+ *      2. - subtraction (-)
+ *      3. - multiplication (*)
+ *      4. - division (/)
+ *
+ * [x] - ! Division should result in an integer, and be done with integers in range of 1 - 100
+ * [x] - ! Context menu to choose which operation to perform
+ * [ ] - ! Keep record of previous games to view history
+ *
+ * [ ] - ! Challenge: Difficulty levels
+ *         - Easy - range to 10, 5 questions
+ *         - Medium - range to 50, 10 questions
+ *         - Hard - range to 100, 15 questions
+ * [ ] - ! Challenge: Timer to track how long game takes
+ * [ ] - ! Challenge: Function to allow choosing number of questions
+ * [x] - ! Challenge: Random game option, with random operations
+ */
+
+int selection = UserInterfaces.MainMenu();
+Console.WriteLine(selection);
+return 0;
+
+
+(int, int) GenerateNumberRange(Random random, int difficulty)
+{
+    int firstNumber = 0, secondNumber = 0;
+
+    switch (difficulty)
+    {
+        case 1:
+            firstNumber = random.Next(1, 11);
+            secondNumber = random.Next(1, 11);
+            break;
+        case 2:
+            firstNumber = random.Next(1, 51);
+            secondNumber = random.Next(1, 51);
+            break;
+        case 3:
+            firstNumber = random.Next(1, 101);
+            secondNumber = random.Next(1, 101);
+            break;
+    }
+
+    return (firstNumber, secondNumber);
+}
+
+int GenerateOperation(int operand, int difficulty, Random random)
+{
+    int answer = 0;
+    (int, int) range;
+    switch (operand)
+    {
+        case 1:
+            range = GenerateNumberRange(random, difficulty);
+            Console.WriteLine($"{range.Item1} + {range.Item2} = ?");
+            answer = range.Item1 + range.Item2;
+            break;
+        case 2:
+            range = GenerateNumberRange(random, difficulty);
+            Console.WriteLine($"{range.Item1} - {range.Item2} = ?");
+            answer = range.Item1 - range.Item2;
+            break;
+        case 3:
+            range = GenerateNumberRange(random, difficulty);
+            Console.WriteLine($"{range.Item1} x {range.Item2} = ?");
+            answer = range.Item1 * range.Item2;
+            break;
+        case 4:
+            do
+            {
+                range = GenerateNumberRange(random, difficulty);
+            } while (range.Item1 % range.Item2 != 0);
+
+            answer = range.Item1 / range.Item2;
+            Console.WriteLine($"{range.Item1} / {range.Item2} = ?");
+            break;
+        case 5:
+            // Recursively call function with new operator within range
+            int randomOperator = random.Next(1, 5);
+            answer = GenerateOperation(randomOperator, difficulty, random);
+            break;
+    }
+
+    return answer;
+}
---
+++ MathGame.lilWe3zy/MathGame.lilWe3zy/UserInterfaces.cs
@@ -0,0 +1,102 @@
+﻿namespace MathGame.lilWe3zy;
+
+public static class UserInterfaces
+{
+    public static void DisplayUserMenu(bool printHeader = false)
+    {
+        if (printHeader)
+        {
+            // Redundant verbatim prefix left in to better visualize the header
+            Console.WriteLine(@"    _    ____  __  __    _");
+            Console.WriteLine(@"   / \  / ___||  \/  |  / \");
+            Console.WriteLine(@"  / _ \ \___ \| |\/| | / _ \");
+            Console.WriteLine(@" / ___ \ ___) | |  | |/ ___ \");
+            Console.WriteLine(@"/_/   \_\____/|_|  |_/_/   \_\");
+
+            Console.WriteLine("\nA (c)Sharp Math Application made with <3");
+        }
+
+        Console.WriteLine("\nPlease select an option:");
+        Console.WriteLine("1) New Game");
+        Console.WriteLine("2) View History");
+        Console.WriteLine("3) Exit");
+    }
+
+    public static void DisplayNewGameMenu(bool printHeader = false)
+    {
+        if (printHeader)
+        {
+            // Redundant verbatim prefix left in to better visualize the header
+            Console.WriteLine(@" _  _               ___                ");
+            Console.WriteLine(@"| \| |_____ __ __  / __|__ _ _ __  ___ ");
+            Console.WriteLine(@"| .` / -_) V  V / | (_ / _` | '  \/ -_)");
+            Console.WriteLine(@"|_|\_\___|\_/\_/   \___\__,_|_|_|_\___|");
+        }
+
+        string[] gameOptions =
+        [
+            "Addition Attack!", "Subtraction Subversion", "Multiplying Monsters", "Division Destroyer!",
+            "Randomized Recon"
+        ];
+
+        Console.WriteLine("\nPlease select one of the options below to start the new game:");
+        for (int i = 0; i < gameOptions.Length; i++) Console.WriteLine(FormatGameOption(gameOptions[i], i, 30));
+
+        Console.WriteLine("\n6) Back");
+    }
+
+    private static string FormatGameOption(string gameOption, int index, int printWidth)
+    {
+        string symbol = index switch
+        {
+            0 => "(+)",
+            1 => "(-)",
+            2 => "(x)",
+            3 => "(/)",
+            _ => "(?)"
+        };
+
+        return $"{index + 1}) {gameOption}{symbol.PadLeft(printWidth - gameOption.Length)}";
+    }
+
+    public static int MainMenu()
+    {
+        bool validInput = false;
+        int userSelection = 0;
+
+        DisplayUserMenu(true);
+
+        do
+        {
+            string? userInput = Console.ReadLine();
+            if (userInput == null)
+            {
+                Console.WriteLine("Please enter a valid number");
+                DisplayUserMenu();
+                continue;
+            }
+
+            if (int.TryParse(userInput, out userSelection))
+            {
+                if (userSelection is <= 0 or > 3)
+                {
+                    Console.WriteLine("Please enter a valid number");
+                    DisplayUserMenu();
+                    continue;
+                }
+
+                validInput = true;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
+                DisplayUserMenu();
+            }
+        } while (validInput == false);
+
+        return userSelection;
+    }
+
+    public static void DisplayDifficultySelection() { }
+    public static void DisplayHistorySelection() { }
+}
