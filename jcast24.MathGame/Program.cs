ac18ad0d118c29180fe03a7cc81408b8730079b4
Jesus Castro <castrojesus39@gmail.com>
2024-04-10 18:38:07 -0700

    added files for MathGame

---
+++ jcast24.MathGame/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace jcast24.MathGame
+{
+    class Program
+    {
+        static void Menu()
+        {
+            string? input;
+            while (true)
+            {
+                Console.WriteLine("Please choose an option: ");
+                Console.WriteLine("(a) Addition");
+                Console.WriteLine("(s) Subtraction");
+                Console.WriteLine("(d) Division");
+                Console.WriteLine("(m) Multiplication");
+                        
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "a":
+                        Console.WriteLine("Addition function!");
+                        break;
+                    case "s":
+                        Console.WriteLine("Subtraction function!");
+                        break;
+                    case "d":
+                        Console.WriteLine("Division function!");
+                        break;
+                    case "m":
+                        Console.WriteLine("Multiplication function!");
+                        break;
+                    default:
+                        Console.WriteLine("Please ask again!");
+                        break;
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name: ");
+            var name = Console.ReadLine();
+            var date = DateTime.UtcNow;
+            
+            
+            Console.WriteLine("Welcome to the Math Game!");
+            Menu();
+        }
+
+    }
+}
