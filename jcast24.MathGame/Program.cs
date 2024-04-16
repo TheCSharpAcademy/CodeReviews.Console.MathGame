bdb8b131d9cefef531025dd144b0fcff15f9d2b1
Jesus Castro <castrojesus39@gmail.com>
2024-04-15 22:03:30 -0700

    added ask user for num of games before the menu

--- jcast24.MathGame/Program.cs
+++ jcast24.MathGame/Program.cs
@@ -4,12 +4,13 @@
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("Please enter your name: ");
-            // var name = Console.ReadLine();
-            // var date = DateTime.UtcNow;
             Console.WriteLine("Welcome to the Math Game!");
-            var menu = new Menu();
-            menu.MenuSystem();
+            Console.WriteLine("How many games would you like to play?: ");
+            
+            int input = Convert.ToInt32(Console.ReadLine());
+            
+            var menu = new Menu(); 
+            menu.MenuSystem(input);
         }
 
     }
