3b6b025c1895db8477cef8626d513e03d65b1ba4
Jesus Castro <castrojesus39@gmail.com>
2024-04-16 20:21:34 -0700

    nothing changed

--- jcast24.MathGame/Program.cs
+++ jcast24.MathGame/Program.cs
@@ -7,10 +7,9 @@
             Console.WriteLine("Welcome to the Math Game!");
             Console.WriteLine("How many games would you like to play?: ");
             int input = Convert.ToInt32(Console.ReadLine());
-
+            
             var menu = new Menu(); 
             menu.MenuSystem(input);
         }
-
     }
 }
