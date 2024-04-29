2f8349b2db54f5d7d21955b49eaccef9ef6cb406
Jon Sparrows <133620406+pinksparrows@users.noreply.github.com>
2024-04-29 01:42:26 -0400

    Included proper files this time :)

---
+++ PinkSparrows.MathGame/MathGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using MathGame;
+using System;
+using System.Threading.Tasks.Sources;
+using static System.Formats.Asn1.AsnWriter;
+
+var menu = new Menu();
+
+var date = DateTime.UtcNow;
+
+string name = Helpers.GetName();
+
+menu.ShowMenu(name, date);
+
