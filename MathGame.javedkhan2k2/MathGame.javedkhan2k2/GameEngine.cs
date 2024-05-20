ad98eaecc2bdf0d44da7a1abda6490404d1a40a7
javedkhan2k2 <javed_alam@outlook.com>
2024-05-20 15:54:14 +0200

    MathGame Project initial commit

---
+++ MathGame.javedkhan2k2/MathGame.javedkhan2k2/GameEngine.cs
@@ -0,0 +1,323 @@
+﻿using MathGame.javedkhan2k2.Models;
+
+namespace MathGame.javedkhan2k2;
+
+internal class GameEngine
+{
+    public string PlayerName { get; set; } = default!;
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public int NumberOfQuestions = 5;
+    public GameDifficulty Difficulty = GameDifficulty.Easy;
+    public GameEngine(string playerName, int numberOfQuestions) : this(playerName)
+    {
+        NumberOfQuestions = numberOfQuestions;
+    }
+    public GameEngine(string playerName)
+    {
+        PlayerName = playerName;
+        StartTime = DateTime.UtcNow;
+    }
+
+    internal void RandomGame(string message)
+    {
+        var random = new Random();
+        var score = 0;
+        var startTime = DateTime.UtcNow;
+        int firstNumber;
+        int secondNumber;
+        char op;
+        var difficulty = Convert.ToInt32(Difficulty);
+
+        for (int i = 0; i < NumberOfQuestions; i++)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+
+            op = Helpers.generateGameType();
+            if (op == '/')
+            {
+                (firstNumber, secondNumber) = Helpers.GetDivisionNumbers(Convert.ToInt32(Difficulty));
+            }
+            else
+            {
+                firstNumber = random.Next(1, difficulty);
+                secondNumber = random.Next(1, difficulty);
+            }
+            Console.WriteLine($"{firstNumber} {op} {secondNumber}");
+            var result = Console.ReadLine();
+            result = Helpers.ValidateResult(result);
+            if (int.Parse(result) == Helpers.getResult(firstNumber, secondNumber, op))
+            {
+                Console.WriteLine("Your answer was correct! Type any key for the next question");
+                score++;
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Your answer was incorrect. Type any key for the next question");
+                Console.ReadLine();
+            }
+            if (i == NumberOfQuestions - 1)
+            {
+                Console.WriteLine($"Game over. Your final score is {score}. Press any key to go back to the main menu.");
+                Console.ReadLine();
+            }
+        }
+        Helpers.AddToHistory(new Game
+        {
+            EndTime = DateTime.UtcNow,
+            StartTime = startTime,
+            NumberOfQuestions = NumberOfQuestions,
+            Score = score,
+            Type = GameType.Random,
+            Difficulty = Difficulty
+        });
+    }
+    internal void DivisionGame(string message)
+    {
+        var score = 0;
+        var startTime = DateTime.UtcNow;
+
+        for (int i = 0; i < NumberOfQuestions; i++)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+
+            var (firstNumber, secondNumber) = Helpers.GetDivisionNumbers(Convert.ToInt32(Difficulty));
+            
+
+            Console.WriteLine($"{firstNumber} / {secondNumber}");
+
+            var result = Console.ReadLine();
+            result = Helpers.ValidateResult(result);
+
+            if (int.Parse(result) == firstNumber / secondNumber)
+            {
+                Console.WriteLine("Your answer was correct! Type any key for the next question");
+                score++;
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Your answer was incorrect. Type any key for the next question");
+                Console.ReadLine();
+            }
+
+            if (i == NumberOfQuestions - 1)
+            {
+                Console.WriteLine($"Game over. Your final score is {score}. Press any key to go back to the main menu.");
+                Console.ReadLine();
+            }
+        }
+
+        Helpers.AddToHistory(new Game
+        {
+            EndTime = DateTime.UtcNow,
+            StartTime = startTime,
+            NumberOfQuestions = NumberOfQuestions,
+            Score = score,
+            Type = GameType.Division,
+            Difficulty = Difficulty
+        });
+    }
+
+    internal void MultiplicationGame(string message)
+    {
+        var random = new Random();
+        var score = 0;
+        var startTime = DateTime.UtcNow;
+
+        int firstNumber;
+        int secondNumber;
+
+        for (int i = 0; i < NumberOfQuestions; i++)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+
+            var difficulty = Convert.ToInt32(Difficulty);
+            firstNumber = random.Next(1, difficulty);
+            secondNumber = random.Next(1, difficulty);
+
+            Console.WriteLine($"{firstNumber} * {secondNumber}");
+
+            var result = Console.ReadLine();
+            result = Helpers.ValidateResult(result);
+
+            if (int.Parse(result) == firstNumber * secondNumber)
+            {
+                Console.WriteLine("Your answer was correct! Type any key for the next question");
+                score++;
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Your answer was incorrect. Type any key for the next question");
+                Console.ReadLine();
+            }
+
+            if (i == NumberOfQuestions - 1)
+            {
+                Console.WriteLine($"Game over. Your final score is {score}. Press any key to go back to the main menu.");
+                Console.ReadLine();
+            }
+        }
+
+        Helpers.AddToHistory(new Game
+        {
+            EndTime = DateTime.UtcNow,
+            StartTime = startTime,
+            NumberOfQuestions = NumberOfQuestions,
+            Score = score,
+            Type = GameType.Multiplication,
+            Difficulty = Difficulty
+        });
+    }
+
+    internal void SubtractionGame(string message)
+    {
+        var random = new Random();
+        var score = 0;
+        var startTime = DateTime.UtcNow;
+
+        int firstNumber;
+        int secondNumber;
+
+        for (int i = 0; i < NumberOfQuestions; i++)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+
+            var difficulty = Convert.ToInt32(Difficulty);
+            firstNumber = random.Next(1, difficulty);
+            secondNumber = random.Next(1, difficulty);
+
+            Console.WriteLine($"{firstNumber} - {secondNumber}");
+
+            var result = Console.ReadLine();
+            result = Helpers.ValidateResult(result);
+
+            if (int.Parse(result) == firstNumber - secondNumber)
+            {
+                Console.WriteLine("Your answer was correct! Type any key for the next question");
+                score++;
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Your answer was incorrect. Type any key for the next question");
+                Console.ReadLine();
+            }
+
+            if (i == NumberOfQuestions - 1)
+            {
+                Console.WriteLine($"Game over. Your final score is {score}. Press any key to go back to the main menu.");
+                Console.ReadLine();
+            }
+        }
+
+        Helpers.AddToHistory(new Game
+        {
+            EndTime = DateTime.UtcNow,
+            StartTime = startTime,
+            NumberOfQuestions = NumberOfQuestions,
+            Score = score,
+            Type = GameType.Subtraction,
+            Difficulty = Difficulty
+        });
+    }
+
+    internal void AdditionGame(string message)
+    {
+        var random = new Random();
+        var score = 0;
+        var startTime = DateTime.UtcNow;
+
+        int firstNumber;
+        int secondNumber;
+
+        for (int i = 0; i < NumberOfQuestions; i++)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+
+            var difficulty = Convert.ToInt32(Difficulty);
+            firstNumber = random.Next(1, difficulty);
+            secondNumber = random.Next(1, difficulty);
+
+            Console.WriteLine($"{firstNumber} + {secondNumber}");
+
+            var result = Console.ReadLine();
+            result = Helpers.ValidateResult(result);
+
+
+            if (int.Parse(result) == firstNumber + secondNumber)
+            {
+                Console.WriteLine("Your answer was correct! Type any key for the next question");
+                score++;
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Your answer was incorrect. Type any key for the next question");
+                Console.ReadLine();
+            }
+
+            if (i == NumberOfQuestions - 1)
+            {
+                Console.WriteLine($"Game over. Your final score is {score}. Press any key to go back to the main menu.");
+                Console.ReadLine();
+            }
+        }
+        Helpers.AddToHistory(new Game
+        {
+            EndTime = DateTime.UtcNow,
+            StartTime = startTime,
+            NumberOfQuestions = NumberOfQuestions,
+            Score = score,
+            Type = GameType.Addition,
+            Difficulty = Difficulty
+        });
+    }
+
+    internal void ChangeQuestions()
+    {
+        Console.Clear();
+        Console.WriteLine("Enter new number of questions");
+        var result = Console.ReadLine();
+        result = Helpers.ValidateResult(result);
+        NumberOfQuestions = int.Parse(result);
+        Console.WriteLine("Press any key to return to Main Menu");
+        Console.ReadLine();
+    }
+
+    internal void ChangeDifficulty()
+    {
+        Console.Clear();
+        Console.WriteLine(@$"
+Select the Game Difficulty? Choose from the options below:
+E - Easy
+N - Normal
+H - Hard");
+        Console.WriteLine("---------------------------------------------");
+        var difficultySelected = Console.ReadLine();
+
+        switch (difficultySelected.Trim().ToLower())
+        {
+            case "e":
+                Difficulty = GameDifficulty.Easy;
+                break;
+            case "n":
+                Difficulty = GameDifficulty.Normal;
+                break;
+            case "h":
+                Difficulty = GameDifficulty.Hard;
+                break;
+            default:
+                Console.WriteLine("Invalid Input");
+                break;
+        }
+    }
+
+}
---
+++ MathGame.javedkhan2k2/MathGame.javedkhan2k2/Menu.cs
@@ -0,0 +1,78 @@
+﻿namespace MathGame.javedkhan2k2;
+
+internal class Menu
+{
+    GameEngine gamesClass;
+
+    public Menu(string playerName)
+    {
+        gamesClass = new GameEngine(playerName);
+    }
+
+    internal void ShowMenu(DateTime date)
+    {
+        Console.Clear();
+        Console.WriteLine($"Hello {gamesClass.PlayerName}. It's {date}. This is your math's game. That's great that you're working on improving yourself");
+        Console.WriteLine("Press any key to show menu");
+        Console.ReadLine();
+        Console.WriteLine("\n");
+
+        var isGameOn = true;
+
+        do
+        {
+            Console.Clear();
+            Console.WriteLine(@$"
+What game would you like to play today? Choose from the options below:
+V - View Previous Games ({Helpers.getGamesNumber()})
+C = Change Number of Questions({gamesClass.NumberOfQuestions})
+H = Change Game Difficulty({gamesClass.Difficulty})
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+R = Random Questions
+Q - Quit the program");
+            Console.WriteLine("---------------------------------------------");
+
+            var gameSelected = Console.ReadLine();
+
+            switch (gameSelected.Trim().ToLower())
+            {
+                case "v":
+                    Helpers.PrintGames();                    
+                    break;
+                case "c":
+                    gamesClass.ChangeQuestions();
+                    break;
+                case "h":
+                    gamesClass.ChangeDifficulty();
+                    break;
+                case "a":
+                    gamesClass.AdditionGame("Addition game");
+                    break;
+                case "s":
+                    gamesClass.SubtractionGame("Subtraction game");
+                    break;
+                case "m":
+                    gamesClass.MultiplicationGame("Multiplication game");
+                    break;
+                case "d":
+                    gamesClass.DivisionGame("Division game");
+                    break;
+                case "r":
+                    gamesClass.RandomGame("Random game");
+                    break;
+                case "q":
+                    gamesClass.EndTime = DateTime.UtcNow;
+                    Console.WriteLine($"Goodbye {gamesClass.PlayerName}. You started the Game at {gamesClass.StartTime.ToLongDateString()} and Ended at {gamesClass.EndTime.ToLongDateString()}\n Press any Key");
+                    Console.ReadLine();
+                    isGameOn = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+        } while (isGameOn);
+    }
+}
