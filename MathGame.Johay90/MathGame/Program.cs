fc2f438ab913c23d3918b1121f12b467a5f44250
JONATHAN MUMMERY <johay.jjj@googlemail.com>
2024-05-24 13:59:29 +0100

    Reduced repetitive game operation logic by implementing operation delegates

--- MathGame.Johay90/MathGame/Program.cs
+++ MathGame.Johay90/MathGame/Program.cs
@@ -4,8 +4,10 @@ app.Run();
 
 public class App
 {
-    // TODO future refactoring: Inject these via interfaces later (DIP)
     private UserInteraction UserInteraction { get; set; }
+
+    public delegate int OperationDelegate(int a, int b);
+    private List<Game> games = new();
 
     public App(UserInteraction userInteraction)
     {
@@ -14,109 +16,23 @@ public class App
 
     public void Run()
     {
-        List<Question> questions = new();
-        List<Game> games = new();
-        Game currentGame;
-
         while (true)
         {
             Operation operationType = UserInteraction.OperationsMenu();
 
-            // TODO future refactoring: Should get rid of the code reuse. Could do this by using delegates for each type of question and binding the operation to a specific symbol/delegate
-
             switch (operationType)
             {
                 case Operation.Add:
-                    currentGame = new Game(Operation.Add);
-                    games.Add(currentGame);
-
-                    for (int i = 1; i < 6; i++)
-                    {
-                        Question question = new(Helpers.RandomNumber(1, 100), Helpers.RandomNumber(1, 100), Operation.Add);
-
-                        question.CorrectAnswer = question.number1 + question.number2;
-                        Console.WriteLine($"{i}. {question.number1} + {question.number2}");
-
-                        string? input = Console.ReadLine();
-                        question.UserAnswer = int.TryParse(input, out int result) ? result : 0;
-
-                        currentGame.Games.Add(question);
-                        if (question.CorrectAnswer == question.UserAnswer) currentGame.Score += 1;
-                        
-                        Console.WriteLine();
-                    }
+                    PerformGameOperation(Operation.Add, (a, b) => a + b);
                     break;
                 case Operation.Subtract:
-                    currentGame = new Game(Operation.Subtract);
-                    games.Add(currentGame);
-
-                    for (int i = 1; i < 6; i++)
-                    {
-                        Question question = new(Helpers.RandomNumber(1, 100), Helpers.RandomNumber(1, 100), Operation.Subtract);
-
-                        question.CorrectAnswer = question.number1 - question.number2;
-                        Console.WriteLine($"{i}. {question.number1} - {question.number2}");
-
-                        string? input = Console.ReadLine();
-                        question.UserAnswer = int.TryParse(input, out int result) ? result : 0;
-
-                        currentGame.Games.Add(question);
-                        if (question.CorrectAnswer == question.UserAnswer) currentGame.Score += 1;
-
-                        Console.WriteLine();
-                    }
+                    PerformGameOperation(Operation.Subtract, (a, b) => a - b);
                     break;
                 case Operation.Multiply:
-                    currentGame = new Game(Operation.Multiply);
-                    games.Add(currentGame);
-
-                    for (int i = 1; i < 6; i++)
-                    {
-                        Question question = new(Helpers.RandomNumber(1, 100), Helpers.RandomNumber(1, 100), Operation.Multiply);
-
-                        question.CorrectAnswer = question.number1 * question.number2;
-                        Console.WriteLine($"{i}. {question.number1} * {question.number2}");
-
-                        string? input = Console.ReadLine();
-                        question.UserAnswer = int.TryParse(input, out int result) ? result : 0;
-
-                        currentGame.Games.Add(question);
-                        if (question.CorrectAnswer == question.UserAnswer) currentGame.Score += 1;
-
-                        Console.WriteLine();
-                    }
+                    PerformGameOperation(Operation.Multiply, (a, b) => a * b);
                     break;
                 case Operation.Divide:
-                    currentGame = new Game(Operation.Divide);
-                    games.Add(currentGame);
-                    for (int i = 1; i < 6; i++)
-                    {
-
-                        double num1;
-                        double num2;
-                        double a;
-
-                        do
-                        {
-                            num1 = Helpers.RandomNumber(1, 500);
-                            num2 = Helpers.RandomNumber(1, 500);
-                            a = num1 / num2;
-
-                        } while (a % 1 != 0);
-
-                        Question question = new((int)num1, (int)num2, Operation.Divide);
-
-                        question.CorrectAnswer = question.number1 / question.number2;
-                        Console.WriteLine($"{i}. {question.number1} / {question.number2}");
-
-                        string? input = Console.ReadLine();
-                        question.UserAnswer = int.TryParse(input, out int result) ? result : 0;
-
-                        currentGame.Games.Add(question);
-                        if (question.CorrectAnswer == question.UserAnswer) currentGame.Score += 1;
-
-                        Console.WriteLine();
-                    }
+                    PerformGameOperation(Operation.Divide, (a, b) => a / b);
                     break;
             }
 
@@ -142,4 +58,53 @@ public class App
 
         }
     }
+
+    private void PerformGameOperation(Operation operationType, OperationDelegate operation)
+    {
+        Game currentGame = new(operationType);
+        games.Add(currentGame);
+
+        for (int i = 1; i <= 5; i++)
+        {
+            int num1 = Helpers.RandomNumber(1, 100);
+            int num2 = Helpers.RandomNumber(1, 100);
+            if (operationType == Operation.Divide)
+            {
+                // Ensure the division results in an integer
+                while (num1 % num2 != 0)
+                {
+                    num1 = Helpers.RandomNumber(1, 500);
+                    num2 = Helpers.RandomNumber(1, 500);
+                }
+            }
+
+            Question question = new(num1, num2, operationType)
+            {
+                CorrectAnswer = operation(num1, num2)
+            };
+
+            Console.WriteLine($"{i}. {num1} {GetOperationSymbol(operationType)} {num2}");
+
+            string? input = Console.ReadLine();
+            question.UserAnswer = int.TryParse(input, out int result) ? result : 0;
+
+            currentGame.Games.Add(question);
+            if (question.CorrectAnswer == question.UserAnswer) currentGame.Score += 1;
+
+            Console.WriteLine();
+        }
+    }
+
+    private string GetOperationSymbol(Operation operationType)
+    {
+        return operationType switch
+        {
+            Operation.Add => "+",
+            Operation.Subtract => "-",
+            Operation.Multiply => "*",
+            Operation.Divide => "/",
+            _ => ""
+        };
+    }
+
 }
