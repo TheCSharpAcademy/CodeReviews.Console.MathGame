ed65ed24f25ce51cdfc265c366a79ff77767b6e5
Milan Jakubec <jakubeclp@gmail.com>
2024-05-28 00:52:08 +0200

    feat: main program loop

--- MathGame.jakubecm/Program.cs
+++ MathGame.jakubecm/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿bool exitFlag = false;
+string playerName = "player";
+
+
+Console.WriteLine($"Greetings, {playerName}! Choose your name, or press enter to keep the default name: ");
+var userInput = Console.ReadLine();
+
+if (userInput.ToString() != "")
+{
+    playerName = userInput;
+}
+
+while (!exitFlag)
+{
+    Console.WriteLine($"\nCurrent player: {playerName}");
+    Console.WriteLine("Choose action:");
+    Console.Write("\t1. Addition game\n\t2. Subtraction game\n\t3. Multiplication game\n\t4. Division game\n\t4. View game history\n\t5. Exit program\n");
+    var selectedGame = Console.ReadLine();
+
+    Console.WriteLine(selectedGame);
+}
+
