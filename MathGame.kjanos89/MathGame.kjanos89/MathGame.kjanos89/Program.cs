72a1e618a07ce000ced43f047c9cb854b5d87ff2
kjanos89 <91398032+kjanos89@users.noreply.github.com>
2024-06-02 20:15:23 +0200

    Start of the code

---
+++ MathGame.kjanos89/MathGame.kjanos89/MathGame.kjanos89/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Start();
+
+
+    }
+
+    static void Start()
+    {
+        Console.WriteLine("__________________________________________________________________");
+        Console.WriteLine("Hello! Choose an option from the menu below by pressing the proper key:");
+        Console.WriteLine("A - Addition");
+        Console.WriteLine("S - Subtraction");
+        Console.WriteLine("M - Multiplication");
+        Console.WriteLine("D - Division");
+        Console.WriteLine("Q - Quit the program");
+        Console.WriteLine("__________________________________________________________________");
+        var selection = Console.ReadLine();
+    }
+
+    public void GameSelected(char selected)
+    {
+
+    }
+
+
+}
+
+
+
+
+
+
