95aba23aa67166705370fb22038cf211624342b0
kjanos89 <91398032+kjanos89@users.noreply.github.com>
2024-06-02 20:28:07 +0200

    Skeleton of the menu
    Added placeholder texts, currently everything is static.

--- MathGame.kjanos89/MathGame.kjanos89/MathGame.kjanos89/Program.cs
+++ MathGame.kjanos89/MathGame.kjanos89/MathGame.kjanos89/Program.cs
@@ -1,10 +1,10 @@
-﻿public class Program
+﻿using System.Linq.Expressions;
+
+public class Program
 {
     static void Main(string[] args)
     {
         Start();
-
-
     }
 
     static void Start()
@@ -17,12 +17,37 @@
         Console.WriteLine("D - Division");
         Console.WriteLine("Q - Quit the program");
         Console.WriteLine("__________________________________________________________________");
-        var selection = Console.ReadLine();
+        string selection = Console.ReadLine();
+        char firstChar = selection[0];
+        Console.WriteLine(GameSelected(firstChar));
     }
 
-    public void GameSelected(char selected)
+    public static string GameSelected(char selected)
     {
+        string result = "";
+        switch (selected)
+        {
+            case 'A':
+                result = "You chose Addition.";
+                break;
+            case 'S':
+                result = "You chose Subtraction.";
+                break;
+            case 'M':
+                result = "You chose Multiplication.";
+                break;
+            case 'D':
+                result = "You chose Division.";
+                break;
+            case 'Q':
+                result = "Program closes. Hope to see you soon!";
+                break;
 
+            default:
+                result = "Wrong input, please choose from the menu.";
+                break;
+        }
+        return result;
     }
 
 
