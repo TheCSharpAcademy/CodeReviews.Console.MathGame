311e84ad734da83cbdf5b080e85c106103f06689
Milan Jakubec <jakubeclp@gmail.com>
2024-06-12 13:33:06 +0200

    docs: added comments for clarity

--- MathGame.jakubecm/GameEngine.cs
+++ MathGame.jakubecm/GameEngine.cs
@@ -5,16 +5,18 @@ namespace MathGame.jakubecm
 {
     internal class GameEngine
     {
-
+        /// <summary>
+        /// The game method itself, takes care of asking questions and evaluates answers
+        /// </summary>
+        /// <param name="message">The message that gets printed at the top</param>
+        /// <param name="gameType">The type of game selected from menu, eg. Addition, Subtraction,...</param>
         internal void Game(string message, GameType gameType) 
         {
             var random = new Random();
             var score = 0;
-            int firstNumber;
-            int secondNumber;
-            int questionAmount = Helpers.GetQuestionAmount();
+            int questionAmount = Helpers.GetQuestionAmount(); // Lets the user select number of questions
+            Stopwatch stopWatch = new Stopwatch(); // Timer for tracking how long the user takes to finish
             string currentGameOperator = Helpers.GetGameOperator(gameType);
-            Stopwatch stopWatch = new Stopwatch();
 
             for (int i = 0; i < questionAmount; i++)
             {
@@ -22,8 +24,8 @@ namespace MathGame.jakubecm
                 Console.Clear();
                 Console.WriteLine(message);
 
-                firstNumber = random.Next(1, 9);
-                secondNumber = random.Next(1, 9);
+                int firstNumber = random.Next(1, 9);
+                int secondNumber = random.Next(1, 9);
 
                 if (currentGameOperator == "/")
                 {
@@ -33,21 +35,23 @@ namespace MathGame.jakubecm
                 }
 
                 Console.WriteLine($"{firstNumber} {currentGameOperator} {secondNumber}");
+                int correctAnswer = Helpers.GetOperationResult(currentGameOperator, firstNumber, secondNumber);
                 var result = Console.ReadLine();
                 result = Helpers.ValidateResult(result);
 
-                if (int.Parse(result!) == Helpers.GetOperationResult(currentGameOperator, firstNumber, secondNumber))
+                if (int.Parse(result!) == correctAnswer)
                 {
                     score++;
-                    Console.WriteLine("Your answer was correct! Type any key for the next question.");
+                    Console.WriteLine("Your answer was correct! Enter any key for the next question.");
                     Console.ReadLine();
                 }
                 else
                 {
-                    Console.WriteLine("Your answer was incorrect. Type any key for the next question.");
+                    Console.WriteLine("Your answer was incorrect. Enter any key for the next question.");
                     Console.ReadLine();
                 }
 
+                // In case the game is random, a new operator is rolled for the next question
                 if(gameType == GameType.Random)
                 {
                     currentGameOperator = Helpers.GetRandomGameOperator();
--- MathGame.jakubecm/Helpers.cs
+++ MathGame.jakubecm/Helpers.cs
@@ -4,7 +4,13 @@ namespace MathGame.jakubecm
 {
     internal class Helpers
     {
+        // The list of games for keeping a record
         internal static List<Game> games = new();
+
+        /// <summary>
+        /// Function that generates two random numbers that are divisible
+        /// </summary>
+        /// <returns> An array of two divisible numbers</returns>
         internal static int[] GetDivisionNumbers()
         {
             var random = new Random();
@@ -26,6 +32,13 @@ namespace MathGame.jakubecm
             return result;
         }
 
+        /// <summary>
+        /// Adds the game record with the specified parameters to the list of played games
+        /// </summary>
+        /// <param name="gameScore">The score the player achieved</param>
+        /// <param name="maxScore">Maximal score on the specific game</param>
+        /// <param name="gameType">The GameType of the game</param>
+        /// <param name="elapsedTime">The time the player took to finish the game</param>
         internal static void AddToHistory(int gameScore, int maxScore, GameType gameType, TimeSpan elapsedTime)
         {
             games.Add(new Game
@@ -38,6 +51,10 @@ namespace MathGame.jakubecm
             });
         }
 
+        /// <summary>
+        /// A function that takes the name from the user and checks if it is not empty.
+        /// </summary>
+        /// <returns>The name string the user selects</returns>
         internal static string GetName()
         {
             Console.WriteLine("Please type your name");
@@ -51,6 +68,9 @@ namespace MathGame.jakubecm
             return name!;
         }
 
+        /// <summary>
+        /// Method for printing a history of played games.
+        /// </summary>
         internal static void PrintGames()
         {
             Console.Clear();
@@ -65,6 +85,11 @@ namespace MathGame.jakubecm
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// Method that validates the result of an operation in the game, meaning if the result is a valid integer. If not, it asks for a valid input.
+        /// </summary>
+        /// <param name="result">User's answer to a question in the game</param>
+        /// <returns>A valid string result</returns>
         internal static string ValidateResult(string? result)
         {
             while (string.IsNullOrEmpty(result) || !Int32.TryParse(result, out _))
@@ -76,6 +101,12 @@ namespace MathGame.jakubecm
             return result;
         }
 
+        /// <summary>
+        /// A function for getting a game operator based on the game type selected.
+        /// </summary>
+        /// <param name="gameType">The type of game the user selected</param>
+        /// <returns>The string operator of the operation</returns>
+        /// <exception cref="InvalidDataException">Gets thrown if there is an invalid game type.</exception>
         internal static string GetGameOperator(GameType gameType)
         {
             switch (gameType)
@@ -96,6 +127,10 @@ namespace MathGame.jakubecm
             }
         }
 
+        /// <summary>
+        /// Gets a random operator for the Random Game challenge of this project
+        /// </summary>
+        /// <returns>A random string operator from the pool</returns>
         internal static string GetRandomGameOperator()
         {
             string[] operatorsPool = { "+", "-", "*", "/" };
@@ -105,6 +140,14 @@ namespace MathGame.jakubecm
             return operatorsPool[index];
         }
 
+        /// <summary>
+        /// A helper function to count the result of a operation based on the operator
+        /// </summary>
+        /// <param name="currentGameOperator">The operation operator</param>
+        /// <param name="num1">First operand</param>
+        /// <param name="num2">Second operand</param>
+        /// <returns>The integer result of the operation</returns>
+        /// <exception cref="InvalidDataException">Gets thrown if the operator is invalid</exception>
         internal static int GetOperationResult(string currentGameOperator, int num1, int num2)
         {
             switch (currentGameOperator)
@@ -122,6 +165,10 @@ namespace MathGame.jakubecm
             }
         }
 
+        /// <summary>
+        /// Helper function to get the required amount of questions in a game from the user.
+        /// </summary>
+        /// <returns>An integer representing the amount of requested questions</returns>
         internal static int GetQuestionAmount()
         {
             Console.Clear();
