21bf77775d00e4091a3823d3e2d58eb33f15cf3f
Wolfieeex <Wolfieeex400@users.noreply.github.com>
2024-06-15 17:23:16 +0100

    Quality of code amendments for full menu functionality v.2

--- MathGame.Wolfieeex/MathGame.Wolfieeex/GameEngine.cs
+++ MathGame.Wolfieeex/MathGame.Wolfieeex/GameEngine.cs
@@ -7,22 +7,22 @@
         
         public void AdditionGame()
         {
-            Console.WriteLine("This functionality will be added later.");
+            Console.WriteLine("This functionality will be added later.1");
             Console.ReadLine();
         }
         public void SubtractionGame()
         {
-            Console.WriteLine("This functionality will be added later.");
+            Console.WriteLine("This functionality will be added later.2");
             Console.ReadLine();
         }
         public void MultiplicationGame()
         {
-            Console.WriteLine("This functionality will be added later.");
+            Console.WriteLine("This functionality will be added later.3");
             Console.ReadLine();
         }
         public void DivisionGame()
         {
-            Console.WriteLine("This functionality will be added later.");
+            Console.WriteLine("This functionality will be added later.4");
             Console.ReadLine();
         }
 
