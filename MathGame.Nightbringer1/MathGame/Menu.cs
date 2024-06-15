c701eed71e354494b60b422a3c6a6cc9f1bc07b1
Nathan Jay <NathanJay24@outlook.com>
2024-06-15 21:25:33 +0100

    new files

---
+++ MathGame.Nightbringer1/MathGame/Menu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MathGame
+{
+    public class Menu
+    {
+        void StartMenu (string name, DateTime date)
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Hello {name}. It's {date}. Which Math Game would you like to play today?");
+            Console.WriteLine("\n");
+
+            var isGameOn = true;
+            do
+            {
+                Console.WriteLine($@"What game would you like to play today? choose from the options below: 
+            A- Addition
+            S- Subtraction
+            M- Multiplication
+            D- Division
+            Q- Quit the Program");
+                Console.WriteLine("------------------------------");
+
+                var gameSelected = Console.ReadLine();
+
+                switch (gameSelected.Trim().ToLower())
+                {
+                    case "a":
+                        AdditionGame("Addition Game has been selected");
+                        break;
+                    case "s":
+                        SubtractionGame("Subtraction Game has been selected");
+                        break;
+                    case "m":
+                        MultiplicationGame("Multiplication Game has been selected");
+                        break;
+                    case "d":
+                        DivisionGame("Division Game has been selected");
+                        break;
+                    case "q":
+                        Environment.Exit(1);
+                        break;
+                    default:
+                        Environment.Exit(0);
+                        break;
+                }
+            } while (isGameOn);
+        }
+    }
+}
+
---
+++ MathGame.Nightbringer1/MathGame/Program.cs
@@ -0,0 +1,185 @@
+﻿using System.Threading.Tasks.Sources;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace MathGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type your name");
+
+            var name = Console.ReadLine();
+            var date = DateTime.UtcNow;
+
+            void AdditionGame(string message)
+            {
+                Console.WriteLine($"{message}");
+
+                var random = new Random();
+
+                int score = 0;
+                int lives = 3;
+
+                for (int i = 0; i < 10; i++)
+                {
+
+                    int firstNumber = random.Next(1, 9);
+                    int secondNumber = random.Next(1, 9);
+                    Console.WriteLine($"{firstNumber} + {secondNumber}");
+
+                    var result = Console.ReadLine();
+
+                    if (int.Parse(result) == firstNumber + secondNumber)
+                    {
+                        Console.WriteLine("Your answer was correct");
+                        score++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is incorrect");
+                        lives--;
+                    }
+
+                    if (lives == 0)
+                    {
+                        Console.WriteLine("you are out of lives! try again!");
+                        Console.WriteLine($"You scored {score}!");
+                        Environment.Exit(1);
+                    }
+                }
+                Console.WriteLine($"You scored {score}!");
+            }
+            void SubtractionGame(string message)
+            {
+                Console.WriteLine($"{message}");
+
+                var random = new Random();
+
+                int score = 0;
+                int lives = 3;
+                for (int i = 0; i < 10; i++)
+                {
+
+                    int firstNumber = random.Next(1, 9);
+                    int secondNumber = random.Next(1, 9);
+                    Console.WriteLine($"{firstNumber} - {secondNumber}");
+
+                    var result = Console.ReadLine();
+
+                    if (int.Parse(result) == firstNumber - secondNumber)
+                    {
+                        Console.WriteLine("Your answer was correct");
+                        score++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is incorrect");
+                        lives--;
+                    }
+
+                    if (lives == 0)
+                    {
+                        Console.WriteLine("you are out of lives! try again!");
+                        Console.WriteLine($"You scored {score}!");
+                        Environment.Exit(1);
+                    }
+                }
+                Console.WriteLine($"You scored {score}!");
+            }
+            void MultiplicationGame(string message)
+            {
+                Console.WriteLine($"{message}");
+
+                var random = new Random();
+
+                int score = 0;
+                int lives = 3;
+                for (int i = 0; i < 10; i++)
+                {
+
+                    int firstNumber = random.Next(1, 9);
+                    int secondNumber = random.Next(1, 9);
+                    Console.WriteLine($"{firstNumber} * {secondNumber}");
+
+                    var result = Console.ReadLine();
+
+                    if (int.Parse(result) == firstNumber * secondNumber)
+                    {
+                        Console.WriteLine("Your answer was correct");
+                        score++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is incorrect");
+                        lives--;
+                    }
+
+                    if (lives == 0)
+                    {
+                        Console.WriteLine("you are out of lives! try again!");
+                        Console.WriteLine($"You scored {score}!");
+                        Environment.Exit(1);
+                    }
+                }
+                Console.WriteLine($"You scored {score}!");
+            }
+            void DivisionGame(string message)
+            {
+                int score = 0;
+                int lives = 3;
+                for (int i =0; i < 10; i++)
+                { 
+                    var divisionNumbers = GetDivisionNumbers();
+                    var firstNumber = divisionNumbers[0];
+                    var secondNumber = divisionNumbers[1];
+
+                    Console.WriteLine($"{firstNumber} / {secondNumber}");
+                    var result = Console.ReadLine();
+
+                    if (int.Parse(result) == firstNumber / secondNumber)
+                    {
+                        Console.WriteLine("Your answer was correct");
+                        score++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your answer is incorrect");
+                        lives--;
+                    }
+
+                    if (lives == 0)
+                    {
+                        Console.WriteLine("you are out of lives! try again!");
+                        Console.WriteLine($"You scored {score}!");
+                        Environment.Exit(1);
+                    }
+                }
+                Console.WriteLine($"You scored {score}!");
+            }
+            int[] GetDivisionNumbers()
+            {
+                var random = new Random();
+
+                var firstNumber = random.Next(1, 99);
+                int secondNumber = random.Next(1, 99);
+
+                var result = new int[2];
+
+                
+
+                while (firstNumber % secondNumber != 0)
+                {
+                    firstNumber = random.Next(1, 99);
+                    secondNumber = random.Next(1, 99);
+                }
+                
+                result[0] = firstNumber;
+                result[1] = secondNumber;
+
+
+                return result;
+            }
+        } 
+    }
+}
