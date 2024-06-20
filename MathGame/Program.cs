6ec67295d6ecc81f1fb401a066de6aacd049c54a
Alvin Graham <grah0071@gmail.com>
2024-06-19 23:40:57 -0500

    Create base menu display and experiment with DateTime class

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Top Level Set-Up
+
+Console.Write("Please enter your name: ");
+string? name = Console.ReadLine();
+var date = DateTime.Now;
+
+// Menu
+Console.WriteLine("------------------------------------------------------");
+Console.WriteLine($"Welcome to The Math Game, {name}.\nThe time is {date.Hour}:{date.Minute} on {date.Month}/{date.Day}/{date.Year}.\n");
+Console.WriteLine($@"Please select a game to play from the list below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+R - Random
+Q - Quit");
+Console.WriteLine("------------------------------------------------------");
+
+// Addition Game
+
+// Subtraction Game
+
+// Multiplication Game
+
+// Divsion Game
