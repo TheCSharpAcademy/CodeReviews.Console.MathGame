5b519995b7252d36dc33bfb3dcf6806a637e2cbd
Ukpa Uchechi <ukpagraceu@gmail.com>
2024-06-23 05:01:11 -0700

    removed unneccessary spaces, fixed indentation, implemented dry principle on arithmetic functions and removed method call redundancy

--- MathGame.ukpagrace/Program.cs
+++ MathGame.ukpagrace/Program.cs
@@ -1,279 +1,191 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class MathGame{
+    int rangeOne = 1;
+    int rangeTwo = 10;
+    public string option = "";
+    readonly Random random= new();
+    public List<string> previousGame = [];
+    public bool endGame;
 
-
-
-
-class MathGame{
-
-  int rangeOne = 1;
-  int rangeTwo = 10;
-
-
-  public string Option{get; set;} = "";
-  Random random= new Random();
-
-  public List<string> PreviousGame{get; set;} = new List<string>();
-
-  public bool ContinueGame{get; set;} = true;
-
-
-
-  public void DisplayOption(){
-    Console.WriteLine(Option);
-  }
-
-
-
-  void RandomGame(){
-    int randomNumber = random.Next(1, 5);
-
-    switch(randomNumber){
-      case 1:
-       AdditionGame(); 
-      break;
-      case 2: 
-        SubstractionGame(); 
-      break;
-      case 3:
-       MultiplicationGame();
-      break;
-      case 4:
-       DivisionGame(); 
-      break;
-      default:
-       AdditionGame();
-      break;
+    public void DisplayOption(){
+        Console.WriteLine(option);
     }
-  }
-  void ShowLevelMenu(){
-    Console.WriteLine("choose a level of difficulty\n");
-    Console.WriteLine("E - Easy");
-    Console.WriteLine("M - Meduim");
-    Console.WriteLine("H - Hard");
 
-    Option = Console.ReadLine().Trim().ToLower();
+    public void RandomGame(){
+        int randomNumber = random.Next(1, 5);
+        string operation;
 
-    switch(Option){
-      case "e":
-        rangeOne = 1;
-        rangeTwo = Option == "d" ?   10 : 99;
-  
+        switch(randomNumber){
+        case 1:
+            operation = "+"; 
         break;
-
-      case "m":
-        rangeOne = 100;
-        rangeTwo = 999;
-      break;
-
-      case "h":
-        rangeOne = 1000;
-        rangeTwo = 9999;
-      break; 
-
-      default:
-        Console.WriteLine("Please select an option in the menu");
-      break;
-    }
-  }
-
-
-  void ShowMenu(){
-      Console.WriteLine("What game would you like to play today\n");
-      Console.WriteLine("R - Random");
-      Console.WriteLine("A - Addition");
-      Console.WriteLine("S - Subtraction");
-      Console.WriteLine("M - Multiplication");
-      Console.WriteLine("D - Division");
-
-
-      Option = Console.ReadLine().Trim().ToLower();
-     switch(Option){
-        case "a":
-          AdditionGame();
+        case 2: 
+            operation = "-";
         break;
-
-        case "s":
-          SubstractionGame();
+        case 3:
+            operation = "*";
         break;
-
-        case "m":
-          MultiplicationGame();
+        case 4:
+            operation = "/";
         break;
-
-        case "d":
-          DivisionGame();
-        break;
-
-        case "r":
-          RandomGame();
-        break;
-
         default:
-        Console.WriteLine("Invalid menu option");
+            operation = "+";
         break;
-        
-
+        }
+        PerformMath(operation);
     }
 
-}
+    void ShowLevelMenu(){
+        Console.WriteLine("choose a level of difficulty\n");
+        Console.WriteLine("E - Easy");
+        Console.WriteLine("M - Meduim");
+        Console.WriteLine("H - Hard");
 
-void AdditionGame(){
-  int score = 0;
-
-      for(int i=4; i > 0; i--){
-
-        
-        int firstNumber = random.Next(rangeOne, rangeTwo);
-        int secondNumber = random.Next(rangeOne, rangeTwo);
-
-        Console.WriteLine($"{firstNumber} +  {secondNumber}");
-        int result;
-        bool success = int.TryParse(Console.ReadLine(), out result);
-        if(!success) Console.WriteLine("Enter a number");
-        if(success && result == firstNumber / secondNumber){
-          Console.WriteLine("Correct");
-          score++;
+        option = Console.ReadLine().Trim().ToLower();
+        Console.Clear();
+        switch(option){
+            case "e":
+                rangeOne = 1;
+                rangeTwo = option == "d" ?   99 : 10;
+                break;
+            case "m":
+                rangeOne = 100;
+                rangeTwo = 999;
+            break;
+            case "h":
+                rangeOne = 1000;
+                rangeTwo = 9999;
+            break; 
+            default:
+                Console.WriteLine("Please select an option in the menu");
+            break;
         }
-
-        if(i == 1){
-          Console.WriteLine($"Your score is {score}");
-        }
-      }
-
-       PreviousGame.Add($"Addition   {score}");
-}
-
-void SubstractionGame(){
-  int score = 0;
-
-      for(int i=4; i > 0; i--){
-        int firstNumber = random.Next(rangeOne, rangeTwo);
-        int secondNumber = random.Next(rangeOne, rangeTwo);
-
-        while(firstNumber < secondNumber){
-          firstNumber = random.Next(rangeOne, rangeTwo);
-          secondNumber = random.Next(rangeOne, rangeTwo);
-        }
-
-        Console.WriteLine($"{firstNumber} -  {secondNumber}");
-        int result;
-        bool success = int.TryParse(Console.ReadLine(), out result);
-        if(!success) Console.WriteLine("Enter a number");
-        if(success && result == firstNumber / secondNumber){
-          Console.WriteLine("Correct");
-          score++;
-        }
-
-        if(i == 1){
-          Console.WriteLine($"Your score is {score}");
-        }
-      }
-
-      PreviousGame.Add($"Substraction   {score}");
-}
-
-
-void MultiplicationGame(){
-  int score = 0;
-
-    for(int i=4; i > 0; i--){
-      int firstNumber = random.Next(rangeOne, rangeTwo) ;
-      int secondNumber = random.Next(rangeOne, rangeTwo);
-
-      Console.WriteLine($"{firstNumber} *  {secondNumber}");
-      int result;
-      bool success = int.TryParse(Console.ReadLine(), out result);
-      if(!success) Console.WriteLine("Enter a number");
-      if(success && result == firstNumber / secondNumber){
-        Console.WriteLine("Correct");
-        score++;
-      }
-
-      if(i == 1){
-        Console.WriteLine($"Your score is {score}");
-      } 
     }
-    PreviousGame.Add($"Multiplication {score}");
-}
 
+    public void ShowMenu(){
+        Console.WriteLine("What game would you like to play today\n");
+        Console.WriteLine("R - Random");
+        Console.WriteLine("A - Addition");
+        Console.WriteLine("S - Subtraction");
+        Console.WriteLine("M - Multiplication");
+        Console.WriteLine("D - Division");
 
-void DivisionGame(){
-  int score = 0;
-
-    for(int i=4; i > 0; i--){
-      int firstNumber = random.Next(rangeOne, rangeTwo);
-      int secondNumber = random.Next(rangeOne, rangeTwo);
-
-      while(firstNumber % secondNumber != 0){
-        firstNumber = random.Next(rangeOne, rangeTwo);
-        secondNumber = random.Next(rangeOne, rangeTwo);
-      }
-
-      Console.WriteLine($"{firstNumber} / {secondNumber}");
-      int result;
-      bool success = int.TryParse(Console.ReadLine(), out result);
-      if(!success) Console.WriteLine("Enter a number");
-      if(success && result == firstNumber / secondNumber){
-        Console.WriteLine("Correct");
-        score++;
-      }
-
-      if(i == 1){
-        Console.WriteLine($"Your score is {score}");
-      } 
+        option = Console.ReadLine().Trim().ToLower();
+        Console.Clear();
+        switch(option){
+            case "a":
+                PerformMath("+");
+            break;
+            case "s":
+                PerformMath("-");
+            break;
+            case "m":
+                PerformMath("*");
+            break;
+            case "d":
+                PerformMath("/");
+            break;
+            case "r":
+                RandomGame();
+            break;
+            default:
+                Console.WriteLine("Invalid menu option");
+            break;
+        }
     }
-    PreviousGame.Add($"Division {score}");
-}
 
-   public void RunGame(){
-
-    do{
-      Console.WriteLine("V - View Previous Games");
-      Console.WriteLine("Q - Quit the program");
-      Console.WriteLine("Y - See menu");
-
-      Option = Console.ReadLine().Trim().ToLower();
-
-
-      switch (Option){
-        case "q":
-          ContinueGame = false;
-          break;
-
-        case "v":
-            foreach(string item in PreviousGame){
-              Console.WriteLine(item);
+    public void PerformMath(string operation){
+        int score = 0;
+        string operationPerformed = "No operation performed";
+        for(int i=2; i > 0; i--){
+            int firstNumber = random.Next(rangeOne, rangeTwo) ;
+            int secondNumber = random.Next(rangeOne, rangeTwo);
+            int mathResult = 0;
+            int usersAnswer;
+            while(operation == "-" && firstNumber < secondNumber){
+                firstNumber = random.Next(rangeOne, rangeTwo);
+                secondNumber = random.Next(rangeOne, rangeTwo);
             }
-          break;
 
-          case "y":
-            ShowLevelMenu();
-            ShowMenu();
-            break;
+            while(operation == "/" && firstNumber % secondNumber != 0){
+                firstNumber = random.Next(rangeOne, rangeTwo);
+                secondNumber = random.Next(rangeOne, rangeTwo);
+            }
+            Console.WriteLine($"{firstNumber} {operation}  {secondNumber}");
 
+            while(!int.TryParse(Console.ReadLine() , out usersAnswer)){
+                Console.WriteLine("Enter a number");
+            }
+            Console.Clear();
 
+            switch(operation){
+                case "+":
+                    mathResult = firstNumber + secondNumber;
+                    operationPerformed = "Addition";
+                break;
+                case "-":
+                    mathResult = firstNumber - secondNumber;
+                    operationPerformed = "Subtraction";
+                break;
+                case "*":
+                    mathResult = firstNumber * secondNumber;
+                    operationPerformed = "Multiplication";
+                break;
+                case "/":
+                    mathResult = firstNumber / secondNumber;
+                    operationPerformed = "Division";
+                break;
+                default:
+                break;
+            }
 
+            if(usersAnswer == mathResult){
+                score++;
+            }
 
-        default:
-            Console.WriteLine("Game Type, Score\n");
-            Console.WriteLine("Select an option");
-            break;
+            if(i == 1){
+                Console.WriteLine($"Your score is {score}");
+            }
         }
-      } while (ContinueGame);
-   }
+        previousGame.Add($"{operationPerformed} {score}");
+    }
 
-   
+    public void RunGame(){
+        do{
+            Console.WriteLine("V - View Previous Games");
+            Console.WriteLine("Q - Quit the program");
+            Console.WriteLine("Y - See menu");
+            option = Console.ReadLine().Trim().ToLower();
+            Console.Clear();
 
+            switch (option){
+            case "q":
+                endGame = true;
+                break;
+            case "v":
+                if(previousGame.Count == 0){
+                    Console.WriteLine("No operation has been peformed, perform an operation");
+                }else{
+                    foreach(string item in previousGame){
+                        Console.WriteLine(item);
+                    }
+                }
+            break;
+            case "y":
+                ShowLevelMenu();
+                ShowMenu();
+            break;
+            default:
+                Console.WriteLine("Game Type, Score\n");
+                Console.WriteLine("Select an option");
+            break;
+            }
+        } while (!endGame);
+    }
 
-  public static void Main(string[] args){
-    Console.WriteLine("Please select a option");
-
-
-
-
-   MathGame mathGame= new MathGame();
-
-    mathGame.RunGame();
-  }
+    public static void Main(string[] args){
+        Console.WriteLine("Please select a option");
+        MathGame mathGame= new();
+        mathGame.RunGame();
+    }
 }
 
