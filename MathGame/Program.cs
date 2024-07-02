d37622c9e418adc366116ac6ed0c4360508b2e90
Karolis Pranckevicius <karolis.pranckevicius@gmail.com>
2024-07-02 18:20:21 +0100

    Project finished!

---
+++ MathGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MathGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            Menu menu = new Menu();
+            Console.WriteLine("Welcome! Please type your name:");
+            Console.WriteLine("\n");
+
+            string name = Console.ReadLine();
+            menu.StartMenu(name);
+        }
+    }
+}
