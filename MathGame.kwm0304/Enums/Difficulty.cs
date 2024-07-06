fbc9b1d6576ed9115b0c10309728263a4afae267
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2024-07-06 13:22:37 -0400

    brought project over

---
+++ MathGame.kwm0304/Enums/Difficulty.cs
@@ -0,0 +1,8 @@
+namespace MathGame.CSA.Enums;
+
+public enum Difficulty
+{
+    Easy = 11,
+    Medium = 21,
+    Hard = 101
+}
---
+++ MathGame.kwm0304/Enums/Operation.cs
@@ -0,0 +1,9 @@
+namespace MathGame.CSA.Enums;
+
+public enum Operation
+{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division
+}
