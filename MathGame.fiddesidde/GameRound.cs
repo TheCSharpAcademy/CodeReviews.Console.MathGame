ff4fb7a680e4e67285914b0010fa2aad94da00d8
fiddesidde <fredrik.mellberg@gmail.com>
2024-07-23 10:26:48 +0200

    Fix stray semi colon

--- MathGame.fiddesidde/GameRound.cs
+++ MathGame.fiddesidde/GameRound.cs
@@ -80,7 +80,7 @@ namespace MathGame
                     }
                     _correctResult = FirstOperand / SecondOperand;
                     break;
-            };
+            }
         }
     }
 }
