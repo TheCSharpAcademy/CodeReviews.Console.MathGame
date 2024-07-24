62a074cc66496ce83d8be6bbfc3d7d5060bbcdfc
KroksasC <gustascemerka@gmail.com>
2024-07-24 14:53:08 +0300

    Waiting for review

---
+++ MathGame/MathGame/Helpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal class Helpers
+    {
+        internal static bool Validation(int a, int b)
+        {
+            return a % b == 0;
+        }
+        internal static string Validation2(string answer)
+        {
+            while (string.IsNullOrEmpty(answer) || !Int32.TryParse(answer, out _))
+            {
+                Console.WriteLine("Only integers is acceptable!");
+                answer = Console.ReadLine();
+            }
+            return answer;
+        }
+        
+    }
+}
