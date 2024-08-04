cb61cca6609031e4b15aae14c1dc9fadfe19ddb7
Jackua <kuangja@oregonstate.edu>
2024-08-04 00:05:15 -0400

    Removed unnecessary using

--- Helpers/Problem.cs
+++ Helpers/Problem.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Math_Game.Helpers
+﻿namespace Math_Game.Helpers
 {
     internal class Problem
     {
--- Helpers/Types.cs
+++ Helpers/Types.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Math_Game.Helpers
+﻿namespace Math_Game.Helpers
 {
     internal enum Types
     {
