f56ad5877221f46d905131836b269d6e855eb5f6
nicoag <nicolas.axgimenez@gmail.com>
2025-05-15 16:12:53 -0300

    Added completed math game

---
+++ MathGameConsole.nico-ag99/Game/Model/GameSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Model
+{
+    public enum DifficultyMode
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+}
