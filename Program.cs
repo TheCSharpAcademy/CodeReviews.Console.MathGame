4c237ae9491e9434c1fddfa17273ed24e7dfe2da
Jack <jackmadden13@hotmail.co.uk>
2025-06-03 21:16:27 +0100

    Added methods for the game itself and each operation

--- Program.cs
+++ Program.cs
@@ -1,25 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int a = 0;
-int b = 0;
+using System.ComponentModel.Design;
+using System.Numerics;
 
-string? userInput = "";
+Random random = new Random();
+int a = random.Next(1, 100); // Generates a random number between 1 and 99
+int b = random.Next(1, 100); // Generates a random number between 1 and 99
 
-Console.WriteLine("Welcome to my Math's Game!");
-Console.WriteLine("Please choose values for two numbers");
-Console.WriteLine("Enter value for first number: ");
-if (userInput != null)
+string? guess = "";
+string? calculationSelector = "";
+
+StartGame();
+
+int AdditionGame()
 {
-    userInput = Console.ReadLine();
-    int.TryParse(userInput, out a);
+    int result =  a + b;
+    return result;
 }
-Console.WriteLine("Enter value for second number: ");
-if (userInput)
-Console.WriteLine("Please choose from the following operations");
-Console.WriteLine("1. Add");
-Console.WriteLine("2: Substract");
-Console.WriteLine("3. Multiply");
-Console.WriteLine("4: Divide ");
+int SubtractionGame()
+{
+    int result = a - b;
+    return result;
+}
+int MultiplicationGame()
+{
+    int result = a * b;
+    return result;
+}
+int DivisionGame()
+{
+    try
+    {
+        int result = a / b;
+        return result;
+    }
+    catch (DivideByZeroException e)
+    {
+        throw new DivideByZeroException("Error: Division by zero is not allowed.");
+    }
+}
 
+    string HasWon(int guess)
+{
 
+}
+
+void StartGame()
+{
+    Console.WriteLine("Welcome to my Math's Game!");
+    Console.WriteLine("Please guess the correct result of two random numbers depending on the operation selected");
+    Console.WriteLine("Please now choose from the following operations");
+        Console.WriteLine("1. Add");
+        Console.WriteLine("2: Substract");
+        Console.WriteLine("3: Multiply");
+        Console.WriteLine("4: Divide ");
 
