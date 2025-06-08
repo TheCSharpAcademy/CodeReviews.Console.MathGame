7128fe0d4967dbf70c565bad8e502d348b4e25c7
Jack <jackmadden13@hotmail.co.uk>
2025-06-08 19:17:02 +0100

    Added a list to store the game history, completed methods for different maths games, and created a better user experience

--- Program.cs
+++ Program.cs
@@ -1,57 +1,180 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Numerics;
 
 Random random = new Random();
-int a = random.Next(1, 100); // Generates a random number between 1 and 99
-int b = random.Next(1, 100); // Generates a random number between 1 and 99
+int a = random.Next(1, 101); // Generates a random number between 1 and 100
+int b = random.Next(1, 101); // Generates a random number between 1 and 100
 
 string? guess = "";
-string? calculationSelector = "";
+int answer = 0;
+string? gameSelector = "";
+List<string> gameHistory = new List<string>();
+int successfulGames = 0;
+string gameData = "";
 
 StartGame();
+if (HasWon() == true)
+{ 
+    if (gameSelector == "1")
+    {
+        successfulGames++;
+        gameData = "GameType: Addition" + $"Number of games won: {successfulGames}";
+        gameHistory.Add(gameData);
+
+    }
+    else if (gameSelector == "2")
+        {
+            successfulGames++;
+            gameData = "GameType: Subtraction" + $"Number of games won: {successfulGames}";
+            gameHistory.Add(gameData);
+        }
+    else if (gameSelector == "3")
+        {
+            successfulGames++;
+            gameData = "GameType: Multiplication" + $"Number of games won: {successfulGames}";
+            gameHistory.Add(gameData);
+        }
+    else if (gameSelector == "4")
+        {
+            successfulGames++;
+            gameData = "GameType: Division" + $"Number of games won: {successfulGames}";
+            gameHistory.Add(gameData);
+        }
+
+      }
+    if (successfulGames == 0)
+    {
+        Console.WriteLine("You have not won any games yet. Please try again!");
+    }
+else
+{
+    Console.WriteLine("Your game history is blank. Please play more games to see your history!");
+}   
+Console.WriteLine("Your game history is as follows: ");
+Console.WriteLine("-------------------------------------------------");
+foreach (string game in gameHistory)
+{
+    Console.WriteLine(game);
+}
+Console.WriteLine("-------------------------------------------------");
 
 int AdditionGame()
-{
-    int result =  a + b;
-    return result;
-}
+    {
+        Console.WriteLine($"Welcome to the Addition Game! The numbers are {a} and {b}.");
+        int result = a + b;
+        Console.WriteLine($"{a} + {b} = ?");
+        Console.WriteLine("Please enter your guess: ");
+        guess = Console.ReadLine();
+        return result;
+    }
 int SubtractionGame()
-{
+{ 
+    Console.WriteLine($"Welcome to the Subtraction Game! The numbers are {a} and {b}.");
     int result = a - b;
+    Console.WriteLine($"{a} - {b} = ?");
+    Console.WriteLine("Please enter your guess: ");
+    guess = Console.ReadLine();
     return result;
 }
 int MultiplicationGame()
-{
+{   
+    Console.WriteLine($"Welcome to the Multiplication Game! The numbers are {a} and {b}.");
     int result = a * b;
+    Console.WriteLine($"{a} * {b} = ?");
+    Console.WriteLine("Please enter your guess: ");
+    guess = Console.ReadLine();
     return result;
 }
 int DivisionGame()
 {
+    Console.WriteLine($"Welcome to the Division Game! The numbers are {a} and {b}.");
     try
     {
         int result = a / b;
+        Console.WriteLine($"{a} / {b} = ?");
+        Console.WriteLine("Please enter your guess: ");
+        guess = Console.ReadLine();
         return result;
     }
     catch (DivideByZeroException e)
     {
-        throw new DivideByZeroException("Error: Division by zero is not allowed.");
+        throw new DivideByZeroException($"Error: Division by zero is not allowed.{e.Message}");
     }
 }
 
-    string HasWon(int guess)
+    bool HasWon()
 {
-
+    bool wonGame = false;
+    if (guess != null)
+    {
+        if (int.TryParse(guess, out answer))
+        {
+            if (gameSelector == "1" && AdditionGame() == answer)
+            {
+                Console.WriteLine("Congratulations! You guessed correctly!");
+                wonGame = true;
+            }
+            else if (gameSelector == "2" && SubtractionGame() == answer)
+            {
+                Console.WriteLine("Congratulations! You guessed correctly!");
+                wonGame = true;
+            }
+            else if (gameSelector == "3" && MultiplicationGame() == answer)
+            {
+                Console.WriteLine("Congratulations! You guessed correctly!");
+                wonGame = true;
+            }
+            else if (gameSelector == "4" && DivisionGame() == answer)
+            {
+                Console.WriteLine("Congratulations! You guessed correctly!");
+                wonGame = true;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you did not guess correctly. Better luck next time!");
+                wonGame = false;
+                StartGame();
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("You did not enter a valid guess. Please try again.");
+    }
+    return wonGame;
 }
 
-void StartGame()
-{
-    Console.WriteLine("Welcome to my Math's Game!");
-    Console.WriteLine("Please guess the correct result of two random numbers depending on the operation selected");
-    Console.WriteLine("Please now choose from the following operations");
-        Console.WriteLine("1. Add");
-        Console.WriteLine("2: Substract");
-        Console.WriteLine("3: Multiply");
-        Console.WriteLine("4: Divide ");
+        string StartGame()
+        {
+            Console.WriteLine("Welcome to Jack's Math Game!");
+            Console.WriteLine("Please guess the correct result of two random numbers depending on the operation selected");
+            Console.WriteLine("Please now choose from the following operations");
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2: Substract");
+            Console.WriteLine("3: Multiply");
+            Console.WriteLine("4: Divide ");
+            switch (gameSelector)
+            {
+                case "1":
+                    AdditionGame();
+                    break;
+                case "2":
+                    SubtractionGame();
+                    break;
+                case "3":
+                    MultiplicationGame();
+                    break;
+                case "4":
+                    DivisionGame();
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection. Please try again.");
+                    break;
+            }
+            gameSelector = Console.ReadLine();
+            return gameSelector;
+        }
 
