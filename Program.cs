791d38e83607ef23dabfb5a055dea96be35f6cf3
Jack <jackmadden13@hotmail.co.uk>
2025-06-12 19:29:33 +0100

    Amended error causing a loop when game should have ended and wrote code for a new game

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
 
 Random random = new Random();
 
@@ -17,16 +18,16 @@ StartGame();
 GameStatistics();
 
 int AdditionGame()
-    {
-        int a = random.Next(1, 101); // Generates a random number between 1 and 100
-        int b = random.Next(1, 101); // Generates a random number between 1 and 100
-        Console.WriteLine($"Welcome to the Addition Game! The numbers are {a} and {b}.");
-        int result = a + b;
-        Console.WriteLine($"{a} + {b} = ?");
-        Console.WriteLine("Please enter your guess: ");
-        guess = Console.ReadLine();
-        return result;
-    }
+{
+    int a = random.Next(1, 101); // Generates a random number between 1 and 100
+    int b = random.Next(1, 101); // Generates a random number between 1 and 100
+    Console.WriteLine($"Welcome to the Addition Game! The numbers are {a} and {b}.");
+    int result = a + b;
+    Console.WriteLine($"{a} + {b} = ?");
+    Console.WriteLine("Please enter your guess: ");
+    guess = Console.ReadLine();
+    return result;
+}
 int SubtractionGame()
 {
     int a = random.Next(1, 101); // Generates a random number between 1 and 100
@@ -105,30 +106,73 @@ bool HasWon()
     {
         if (int.TryParse(guess, out answer))
         {
-            if (gameSelector == "1" && AdditionGame() == answer)
+            int correctAnswer = answer;
+            if (wonGame == true)
+            {  Console.WriteLine("You have won this game. Please start a new game to continue playing Y/N.");
+                string? continueGame = Console.ReadLine().ToUpper();
+                if (continueGame == "Y")
+                {
+                    StartGame();
+                }
+                else if (continueGame == "N")
+                {
+                    Console.WriteLine("Thank you for playing! Goodbye!");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter Y or N.");
+
+                }
+            if (gameSelector == "1")
             {
-                Console.WriteLine("Congratulations! You guessed correctly!");
-                wonGame = true;
+                if (correctAnswer == answer)
+                {
+                    Console.WriteLine("Congratulations! You guessed correctly!");
+                    wonGame = true;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you did not guess correctly. Better luck next time!");
+                }
             }
-            else if (gameSelector == "2" && SubtractionGame() == answer)
+            if (gameSelector == "2")
             {
-                Console.WriteLine("Congratulations! You guessed correctly!");
-                wonGame = true;
+                if (correctAnswer == answer)
+                {
+                    Console.WriteLine("Congratulations! You guessed correctly!");
+                    wonGame = true;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you did not guess correctly.Better luck next time!");
+                }
             }
-            else if (gameSelector == "3" && MultiplicationGame() == answer)
+            if (gameSelector == "3")
             {
-                Console.WriteLine("Congratulations! You guessed correctly!");
-                wonGame = true;
+                if (correctAnswer == answer)
+                {
+                    Console.WriteLine("Congratulations! You guessed correctly!");
+                    wonGame = true;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you did not guess correctly. Better luck next time!");
+                }
             }
-            else if (gameSelector == "4" && DivisionGame() == answer)
+
+            if (gameSelector == "4")
             {
-                Console.WriteLine("Congratulations! You guessed correctly!");
-                wonGame = true;
-            }
-            else
-            {
-                Console.WriteLine("Sorry, you did not guess correctly. Better luck next time!");
-                wonGame = false;
+                if (correctAnswer == answer)
+                {
+                    Console.WriteLine("Congratulations! You guessed correctly!");
+                    wonGame = true;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you did not guess correctly. Better luck next time!");
+
+                }
             }
         }
     }
@@ -138,53 +182,54 @@ bool HasWon()
     }
     return wonGame;
 }
-
         
-void GameStatistics()
-{
-    if (HasWon() == true)
-    {
-        if (gameSelector == "1")
-        {
-            successfulGames++;
-            gameData = "GameType: Addition" + $"Number of games won: {successfulGames}";
-            gameHistory.Add(gameData);
 
-        }
-        else if (gameSelector == "2")
-        {
-            successfulGames++;
-            gameData = "GameType: Subtraction" + $"Number of games won: {successfulGames}";
-            gameHistory.Add(gameData);
-        }
-        else if (gameSelector == "3")
-        {
-            successfulGames++;
-            gameData = "GameType: Multiplication" + $"Number of games won: {successfulGames}";
-            gameHistory.Add(gameData);
-        }
-        else if (gameSelector == "4")
-        {
-            successfulGames++;
-            gameData = "GameType: Division" + $"Number of games won: {successfulGames}";
-            gameHistory.Add(gameData);
-        }
 
-    }
-    if (successfulGames == 0)
-    {
-        Console.WriteLine("You have not won any games yet. Please try again!");
-    }
-    else
-    {
-        Console.WriteLine("Your game history is blank. Please play more games to see your history!");
-    }
-    Console.WriteLine("Your game history is as follows: ");
-    Console.WriteLine("-------------------------------------------------");
-    foreach (string game in gameHistory)
-    {
-        Console.WriteLine(game);
-    }
-    Console.WriteLine("-------------------------------------------------");
-}
+                void GameStatistics()
+                {
+                    if (HasWon() == true)
+                    {
+                        if (gameSelector == "1")
+                        {
+                            successfulGames++;
+                            gameData = "GameType: Addition " + $"Number of games won: {successfulGames}";
+                            gameHistory.Add(gameData);
+
+                        }
+                        else if (gameSelector == "2")
+                        {
+                            successfulGames++;
+                            gameData = "GameType: Subtraction " + $"Number of games won: {successfulGames}";
+                            gameHistory.Add(gameData);
+                        }
+                        else if (gameSelector == "3")
+                        {
+                            successfulGames++;
+                            gameData = "GameType: Multiplication " + $"Number of games won: {successfulGames}";
+                            gameHistory.Add(gameData);
+                        }
+                        else if (gameSelector == "4")
+                        {
+                            successfulGames++;
+                            gameData = "GameType: Division " + $"Number of games won: {successfulGames}";
+                            gameHistory.Add(gameData);
+                        }
+
+                    }
+                    if (successfulGames == 0)
+                    {
+                        Console.WriteLine("You have not won any games yet. Please try again!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Well done on winning! Play more games to improve your score!");
+                    }
+                    Console.WriteLine("Your game history is as follows: ");
+                    Console.WriteLine("-------------------------------------------------");
+                    foreach (string game in gameHistory)
+                    {
+                        Console.WriteLine(game);
+                    }
+                    Console.WriteLine("-------------------------------------------------");
+                }
 
