1149db379e094ad893ba9fa061658aa5d3c2175d
aneevel <aneevel15@gmail.com>
2025-12-07 20:55:11 -0600

    create basic loop

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -2,9 +2,25 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the C# Academy Math Game!\nThere are several game modes to choose from. Each game run will contain 5 questions.");
+            bool playing = true;
+
+            while (playing)
+            {
+                Console.WriteLine("Please choose a game mode. Your options are;\n1) Addition\n2) Subtraction\n3) Multiplication\n4) Division");
+
+                bool validInput = Int32.TryParse(Console.ReadLine(), out int userChoice);
+                if (!validInput)
+                {
+                    Console.WriteLine("Invalid input provided. Please make sure you choose an existing game mode.");
+                    continue;
+                }
+                Console.WriteLine($"Entering game mode {userChoice}");
+            }
+
+
         }
     }
 }
