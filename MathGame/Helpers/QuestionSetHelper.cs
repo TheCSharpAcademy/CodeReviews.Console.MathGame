88c0c5ec084a91981446e933b39d346d557e89da
aneevel <aneevel15@gmail.com>
2025-12-08 14:05:38 -0600

    validate division operations

--- MathGame/Helpers/QuestionSetHelper.cs
+++ MathGame/Helpers/QuestionSetHelper.cs
@@ -5,12 +5,20 @@ namespace MathGame.Helpers
     internal class QuestionSetHelper
     {
         // TODO: Should take difficulty level into account
+        // TODO: Make sure to validate division operations (result in integer division, no zero divisor, etc.,)
         public static QuestionSet GenerateQuestionSet(GameMode gameMode, Random random)
         {
-            int firstOperand = random.Next(50);
-            int secondOperand = random.Next(50);
+            int result, firstOperand, secondOperand;
+            bool haveValidQuestion = false;
+            do
+            {
+                firstOperand = random.Next(50);
+                secondOperand = random.Next(50);
 
-            int result;
+                // Most questions are valid - the only strong consideration is for DIVISION
+                haveValidQuestion = IsValidQuestion(firstOperand, secondOperand, gameMode);
+
+            } while (!haveValidQuestion);
 
             switch (gameMode)
             {
@@ -36,6 +44,24 @@ namespace MathGame.Helpers
         static string FormatQuestion(GameMode gameMode, int firstOperand, int secondOperand)
         {
             return $"{firstOperand} {GameModeExtensions.ToSymbol(gameMode)} {secondOperand} = ?"; 
+        }
+
+        static bool IsValidQuestion(int firstOperand, int secondOperand,  GameMode gameMode)
+        {
+            // Any and all operands are valid for ADDITION, SUBTRACTION, MULTIPLICATION
+            switch (gameMode)
+            {
+                case GameMode.ADDITION:
+                case GameMode.SUBTRACTION:
+                case GameMode.MULTIPLICATION:
+                    return true;
+                case GameMode.DIVISION:
+
+                    // Any valid integer division will necessarily have no modulus remainder
+                    return (secondOperand != 0 && (firstOperand %  secondOperand == 0));
+                default:
+                    throw new System.NotImplementedException("Unsupported Game Mode provided!");
+            }
         }
     }
 }
