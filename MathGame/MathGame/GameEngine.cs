f8e3002521b062650651633aca8f54fada302288
Sinaglicek <sinagl.matej@seznam.cz>
2026-03-28 21:30:28 +0100

    New project

---
+++ MathGame/MathGame/GameEngine.cs
@@ -0,0 +1,170 @@
+﻿namespace MathGame.Models
+{
+    internal class GameEngine
+    {
+        internal void AdditionGame(string message)
+        {
+            Console.WriteLine(message);
+            var random = new Random();
+            var score = 0;
+
+            int firstNumber;
+            int secondNumber;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(message);
+                firstNumber = random.Next(1, 9);
+                secondNumber = random.Next(1, 9);
+                Console.WriteLine($"{firstNumber} + {secondNumber}");
+                var result = Console.ReadLine();
+
+                result = Helpers.ValidateResult(result);
+               
+                if (int.Parse(result) == firstNumber + secondNumber)
+                {
+                    Console.WriteLine("Correct! Type any key for next question.");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Wrong! Type any key for next question.");
+                    Console.ReadLine();
+                }
+                if (i == 4)
+                {
+                    Console.WriteLine($"Your game is over. You final score is {score} Type any key to return to the main menu.");
+                    Console.ReadLine();
+                }
+
+            }
+
+            Helpers.AddToHistory(score, GameType.Addition);
+        }
+
+        internal void SubtractionGame(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine(message);
+            var random = new Random();
+            var score = 0;
+
+            int firstNumber;
+            int secondNumber;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(message);
+                firstNumber = random.Next(1, 9);
+                secondNumber = random.Next(1, 9);
+                Console.WriteLine($"{firstNumber} - {secondNumber}");
+                var result = Console.ReadLine();
+
+                result = Helpers.ValidateResult(result);
+
+                if (int.Parse(result) == firstNumber - secondNumber)
+                {
+                    Console.WriteLine("Correct! Type any key for next question.");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Wrong! Type any key for next question.");
+                    Console.ReadLine();
+                }
+                if (i == 4)
+                {
+                    Console.WriteLine($"Your game is over. You final score is {score} Type any key to return to the main menu.");
+                    Console.ReadLine();
+                }
+            }
+
+            Helpers.AddToHistory(score, GameType.Subtraction);
+
+        }
+        internal void MultiplicationGame(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine(message);
+            var random = new Random();
+            var score = 0;
+
+            int firstNumber;
+            int secondNumber;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(message);
+                firstNumber = random.Next(1, 9);
+                secondNumber = random.Next(1, 9);
+                Console.WriteLine($"{firstNumber} * {secondNumber}");
+                var result = Console.ReadLine();
+
+                result = Helpers.ValidateResult(result);
+
+                if (int.Parse(result) == firstNumber * secondNumber)
+                {
+                    Console.WriteLine("Correct! Type any key for next question.");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Wrong! Type any key for next question.");
+                    Console.ReadLine();
+                }
+                if (i == 4)
+                {
+                    Console.WriteLine($"Your game is over. You final score is {score} Type any key to return to the main menu.");
+                    Console.ReadLine();
+                }
+            }
+
+            Helpers.AddToHistory(score, GameType.Multiplication);
+
+        }
+        internal void DivisionGame(string message)
+        {
+            Console.WriteLine(message);
+            Helpers.GetDivisionNumbers();
+            var score = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(message);
+                var divisionNumbers = Helpers.GetDivisionNumbers();
+                var firstNumber = divisionNumbers[0];
+                var secondNumber = divisionNumbers[1];
+
+                Console.WriteLine($"{firstNumber} / {secondNumber}");
+                var result = Console.ReadLine();
+
+                result = Helpers.ValidateResult(result);
+
+                if (int.Parse(result) == firstNumber / secondNumber)
+                {
+                    Console.WriteLine("Correct! Type any key for next question.");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Wrong! Type any key for next question.");
+                    Console.ReadLine();
+                }
+                if (i == 4)
+                {
+                    Console.WriteLine($"Your game is over. You final score is {score} Type any key to return to the main menu.");
+                    Console.ReadLine();
+                }
+            }
+
+            Helpers.AddToHistory(score, GameType.Division);
+        }
+    }
+}
---
+++ MathGame/MathGame/Menu.cs
@@ -0,0 +1,62 @@
+﻿namespace MathGame.Models
+{
+    internal class Menu
+    {
+        GameEngine engine = new();
+
+        internal void ShowMenu(string name, DateTime date)
+        {
+            Console.Clear();
+            Console.WriteLine($"Hello {name}, it's {date.DayOfWeek}. Welcome to your math's game. I hope you will enjoy it!");
+            Console.ReadLine();
+            Console.WriteLine("\n");
+
+            var isGameOn = true;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(@$"What game mode would you like to choose?
+        A - Addition
+        S - Subtraction
+        M - MUltiplication
+        D - Division
+        V - View previous games
+        Q - Quit the program");
+                Console.WriteLine("________________________________");
+
+
+                var gameModeSelected = Console.ReadLine().Trim().ToLower();
+
+                switch (gameModeSelected.Trim().ToLower())
+                {
+                    case "a":
+                        engine.AdditionGame("Addition Game");
+                        break;
+                    case "s":
+                        engine.SubtractionGame("Subtraction Game");
+                        break;
+                    case "m":
+                        engine.MultiplicationGame("Multiplication Game");
+                        break;
+                    case "d":
+                        engine.DivisionGame("Division Game");
+                        break;
+                    case "q":
+                        Console.WriteLine("Quitting the program. Goodbye!");
+                        isGameOn = false;
+                        Environment.Exit(1);
+                        break;
+                    case "v":
+                        Helpers.GetGames();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input!");
+                        break;
+                }
+            } while (isGameOn);
+
+
+        }
+    }
+}
