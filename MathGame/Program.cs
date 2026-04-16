700d2c59db91dd1aad939f0d6411594f2d790feb
Thanos Dimitriadis <exadim@proton.me>
2026-04-16 17:51:55 +0300

    chore: add personal project for code review

---
+++ MathGame/Program.cs
@@ -0,0 +1,78 @@
+﻿using MathGame;
+
+Console.WriteLine("Welcome to Math Game!");
+
+var quiz = new Quiz();
+
+int counter = 0;
+string? userInput = null;
+while (counter < 5)
+{
+    MenuOptions menuChoice = SelectOperationMenu();
+    if (menuChoice == MenuOptions.Exit)
+    {
+        break;
+    }
+    else if (menuChoice == MenuOptions.History)
+    {
+        quiz.PrintHistory();
+        Console.WriteLine("\n");
+        continue;
+    }
+
+    string currentProblem = quiz.GenerateQuestion((int)menuChoice);
+    int userAnswer = 0;
+
+    do
+    {
+        Console.WriteLine(currentProblem);
+        Console.Write("Enter your answer: ");
+        userInput = Console.ReadLine();
+    } while (!int.TryParse(userInput, out userAnswer));
+
+    if (quiz.VerifyAnswer(userAnswer))
+    {
+        Console.WriteLine("The answer was correct!");
+    }
+    else
+    {
+        Console.WriteLine("The answer was incorrect!");
+    }
+
+    counter++;
+    Console.WriteLine("\n");
+}
+
+Console.WriteLine($"Your final score is {quiz.Score}");
+Console.WriteLine("Thank you for playing!");
+
+
+static MenuOptions SelectOperationMenu()
+{
+    Console.WriteLine("Select an operation:");
+    foreach (var menuOption in Enum.GetValues(typeof(MenuOptions)))
+    {
+        Console.WriteLine($"{(int)menuOption}. {menuOption}");
+    }
+
+    string? userInput = null;
+    MenuOptions menuChoice;
+    do
+    {
+        Console.Write("Enter your choice: ");
+        userInput = Console.ReadLine();
+    }
+    while (!(Enum.TryParse(userInput, out menuChoice) && Enum.IsDefined(menuChoice)));
+
+    return menuChoice;
+}
+
+public enum MenuOptions
+{
+    Addition = 1,
+    Subtraction = 2,
+    Multiplication = 3,
+    Division = 4,
+    History = 5,
+    Exit = 6,
+};
---
+++ MathGame/Quiz.cs
@@ -0,0 +1,103 @@
+﻿
+
+namespace MathGame
+{
+    public class Quiz
+    {
+        public int Score { get; private set; }
+
+        private Random _random;
+
+        private Dictionary<int, char> _operations { get; } = new()
+        {
+            [1] = '+',
+            [2] = '-',
+            [3] = '*',
+            [4] = '/'
+        };
+
+        private List<string> History { get; set; }
+
+        private int _firstOperand;
+        
+        private int _secondOperand;
+        
+        private char _operation;
+        
+        private int _result;
+
+        public Quiz()
+        {
+            History = [];
+            _random = new Random();
+        }
+
+        public void PrintHistory()
+        {
+            if (History.Count == 0)
+            {
+                Console.WriteLine("You haven't played any games.");
+                return;
+            }
+
+            Console.WriteLine("Game results:");
+            foreach (var game in History)
+            {
+                Console.WriteLine(game);
+            }
+        }
+
+        public string GenerateQuestion(int menuSelection)
+        {
+            _operation = _operations[menuSelection];
+
+            _firstOperand = _random.Next(0, 100);
+            _secondOperand = _random.Next(0, 100);
+
+            switch (_operation)
+            {
+                case '+':
+                    _result = _firstOperand + _secondOperand;
+                    break;
+                case '-':
+                    _result = _firstOperand - _secondOperand;
+                    break;
+                case '*':
+                    _result = _firstOperand * _secondOperand;
+                    break;
+                case '/':
+                    double quotient;
+                    do
+                    {
+                        _firstOperand = _random.Next(0, 100);
+                        _secondOperand = _random.Next(1, _firstOperand);
+                        quotient = (double) _firstOperand / _secondOperand;
+                    } while (quotient != (int) quotient); // ensures the result is an int
+                    _result = (int)quotient;
+                    break;
+                default:
+                    throw new InvalidOperationException("This isn't what I signed up for");
+            }
+
+            return $"{_firstOperand} {_operation} {_secondOperand} = ?";
+        }
+
+        public bool VerifyAnswer(int userResult)
+        {
+            LogResult();
+            if (_result == userResult)
+            {
+                Score += 1;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void LogResult()
+        {
+            string currentResult = $"{_firstOperand} {_operation} {_secondOperand} = {_result}";
+            History.Add(currentResult);
+        }
+    }
+}
