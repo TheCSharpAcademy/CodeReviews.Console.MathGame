11af12a984597c3571aac387cdfb34a1950266c5
madme <tatrantuyen@gmail.com>
2026-06-17 13:50:39 +0700

    getUserName added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.WriteLine("Welcome to Math game");
+string userName = GetUserName();
+
+StartMenu(userName);
+string getUserName()
+{
+    Console.WriteLine("Please enter your name:");
+    string name = Console.ReadLine();
+    return name;
+}
