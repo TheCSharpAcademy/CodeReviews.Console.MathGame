fa4575fa855e82c6e2f016b9b5e50a2098e0edc6
eroul211 <tudorzamfir2018@yahoo.com>
2026-08-15 17:14:10 +0300

    -fix history does not appear as an menu option - Invalid operation infinite loop -fixerd program does not show negative operation

--- eroul211.MathGame/Program.cs
+++ eroul211.MathGame/Program.cs
@@ -1,75 +1,65 @@
 ﻿MathOperations mathGame = new();
-List<SaveScore> saveScore = new List<SaveScore>();
 while (true)
 {
-    mathGame.Game();
-    SaveMenu();
+    string userInput = string.Empty;
+    Console.Clear();
+    while (mathGame.IsValid)
+    {
+        Console.WriteLine("Choose  an operations :[A]ddition, [S]ubtraction, [M]ultiplication, [D]ivision, [H]istory");
+        userInput = Console.ReadLine()!;
+        mathGame.Game(userInput);
+
+    }
+
+    mathGame.SaveMenu();
 
     Console.Write("Do you want to play again? Y/N :");
-    string userInput = Console.ReadLine()!;
+    userInput = Console.ReadLine()!;
 
     if (userInput != null)
     {
         if (userInput.ToLower() == "n")
             break;
+        else
+            mathGame.IsValid = true;
     }
+
     Console.WriteLine("Press enter to continue...");
     ConsoleKeyInfo key = Console.ReadKey(true);
     while (key.Key != ConsoleKey.Enter) { }
-    ;
 }
 
-
-
-void SaveMenu()
-{
-    Console.WriteLine("Do you want to save your score?Y/N: ");
-    string input = Console.ReadLine()!;
-
-    if (input.ToLower() == "y")
-    {
-        Console.Write("Enter your name: ");
-        input = Console.ReadLine()!;
-
-        if (input != null)
-        {
-            SaveScore save = new SaveScore(mathGame.Score, input);
-            saveScore.Add(save);
-        }
-    }
-    Console.Write("Do you want to print the score board Y/N: ");
-    input = Console.ReadLine()!;
-    if (input != null)
-    {
-        if (input.ToLower() == "y")
-        {
-            foreach (SaveScore save in saveScore)
-            {
-                save.PrintSscore();
-            }
-        }
-    }
-}
 
 public class MathOperations
 {
-    public int Score { get; private set; }
-    private int GetResult(string operation)
+    public int Score { get; private set; } = 0;
+    public bool IsValid { get; set; } = true;
+    private int _result = 0;
+    private List<SaveScore> saveScore = new List<SaveScore>();
+
+    private void GetResult(string userInput)
     {
         Random random = new Random();
         int number1 = random.Next(1, 101);
         int number2 = random.Next(1, 101);
-        switch (operation.ToLower())
+
+        switch (userInput.ToLower())
         {
             case "a" or "addition":
                 Console.Write($"What is the answer for: {number1} + {number2} ");
-                return number1 + number2;
+                _result = number1 + number2;
+                IsValid = false;
+                break;
             case "s" or "subtraction":
                 Console.Write($"What is the answer for: {number1} - {number2} ");
-                return Math.Abs(number1 - number2);
+                _result = number1 - number2;
+                IsValid = false;
+                break;
             case "m" or "Multiplication":
                 Console.Write($"What is the answer for: {number1} * {number2} ");
-                return number1 * number2;
+                _result = number1 * number2;
+                IsValid = false;
+                break;
             case "d" or "division":
                 while (number1 % number2 != 0)
                 {
@@ -77,43 +67,93 @@ public class MathOperations
                     number2 = random.Next(1, 101);
                 }
                 Console.Write($"What is the answer for: {number1} / {number2} ");
-                return number1 / number2;
+                _result = number1 / number2;
+                IsValid = false;
+                break;
+            case "h" or "history":
+                VisualizeSaveHistory();
+                IsValid = true;
+                break;
             default:
-                Console.WriteLine("Invalid Operation1!");
-                return -1;
+                Console.Clear();
+                IsValid = true;
+                break;
         }
     }
-    public void Game()
+    public void SaveMenu()
     {
-        Console.WriteLine("Choose  an operations :[A]ddition, [S]ubtraction, [M]ultiplication, or [D]ivision.");
-        string userInput = Console.ReadLine()!;
-        int rounds = 1;
-        Score = 0;
+        Console.WriteLine("Do you want to save your score?Y/N: ");
+        string input = Console.ReadLine()!;
 
-        while (rounds <= 5)
+        if (input.ToLower() == "y")
         {
-            int result = GetResult(userInput);
-            if (result != -1)
+            Console.Write("Enter your name: ");
+            input = Console.ReadLine()!;
+
+            if (input != null)
             {
-                string guess = Console.ReadLine()!;
-                if (int.TryParse(guess, out int value))
+                SaveScore save = new SaveScore(Score, input);
+                saveScore.Add(save);
+                Score = 0;
+            }
+        }
+
+    }
+    public void VisualizeSaveHistory()
+    {
+        Console.Write("Do you want to print the score board Y/N: ");
+        string input = Console.ReadLine()!;
+        if (input != null)
+        {
+            if (input.ToLower() == "y")
+            {
+                if (saveScore.Any())
                 {
-                    if (result == value)
+                    foreach (SaveScore save in saveScore)
                     {
-                        rounds++;
-                        Score++;
-                        Console.WriteLine("Correct!!");
+                        save.PrintSscore();
                     }
-                    else
-                    {
-                        Console.WriteLine("Wrong!!");
-                        rounds++;
-                    }
+                }
+                else
+                {
+                    Console.WriteLine("No save scored registred!");
                 }
             }
         }
-        Console.WriteLine($"Your score is : Score: {Score}");
     }
+    public void Game(string userInput)
+    {
+        int rounds = 1;
+
+        while (rounds <= 5)
+        {
+            GetResult(userInput);
+            string guess = string.Empty;
+            if (IsValid)
+            {
+                break;
+            }
+            guess = Console.ReadLine()!;
+
+            if (int.TryParse(guess, out int value))
+            {
+                if (_result == value)
+                {
+                    rounds++;
+                    Score++;
+                    Console.WriteLine("Correct!!");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong!!");
+                    rounds++;
+                }
+                Console.WriteLine($"Your score is : Score: {Score}");
+            }
+
+        }
+    }
+
 }
 public class SaveScore
 {
