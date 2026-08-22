dc9115472b83e137c5a2de70e8c094d591f0bec2
Tetris <szymon.nowakowski2807@gmail.com>
2026-08-22 23:23:34 +0200

    Add switch to let user choose option

--- MathGame.tetris_y/MathGame.tetris_y/Program.cs
+++ MathGame.tetris_y/MathGame.tetris_y/Program.cs
@@ -9,4 +9,30 @@
 
     Console.Write("Enter your choice (1-5):");
     int choice = Convert.ToInt32(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            //PerformAddition();
+            break;
+        case 2:
+            //PerformSubtraction();
+            break;
+        case 3:
+            //PerformMultiplication();
+            break;
+        case 4:
+            //PerformDivision();
+            break;
+        case 5:
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Thank you for playing! Goodbye!\n");
+            Console.ResetColor();
+            return;
+        default:
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid choice. Please try again.\n");
+            Console.ResetColor();
+            break;
+    }
 }
