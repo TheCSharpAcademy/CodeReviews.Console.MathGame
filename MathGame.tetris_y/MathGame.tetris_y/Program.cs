7c76152689f83444136c35381a9c7286931119c8
Tetris <szymon.nowakowski2807@gmail.com>
2026-08-22 23:18:42 +0200

    Add main menu

--- MathGame.tetris_y/MathGame.tetris_y/Program.cs
+++ MathGame.tetris_y/MathGame.tetris_y/Program.cs
@@ -1,4 +1,12 @@
-﻿class Program
+﻿while (true)
 {
+    Console.WriteLine("===== Welcome to the Math Game! =====");
+    Console.WriteLine("1. Addition");
+    Console.WriteLine("2. Subtraction");
+    Console.WriteLine("3. Multiplication");
+    Console.WriteLine("4. Division");
+    Console.WriteLine("5. Exit");
 
+    Console.Write("Enter your choice (1-5):");
+    int choice = Convert.ToInt32(Console.ReadLine());
 }
