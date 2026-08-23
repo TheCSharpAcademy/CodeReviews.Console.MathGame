03ce9a7f57fda50165430fee48f44adf1738e01a
Tetris <szymon.nowakowski2807@gmail.com>
2026-08-23 15:18:02 +0200

    Add game history

--- MathGame.tetris_y/MathGame.tetris_y/Program.cs
+++ MathGame.tetris_y/MathGame.tetris_y/Program.cs
@@ -5,6 +5,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<string> previousGames = new List<string>();
+
         while (true)
         {
             Console.WriteLine("===== Welcome to the Math Game! =====");
@@ -12,27 +14,31 @@ class Program
             Console.WriteLine("2. Subtraction");
             Console.WriteLine("3. Multiplication");
             Console.WriteLine("4. Division");
-            Console.WriteLine("5. Exit");
+            Console.WriteLine("5. Display game history");
+            Console.WriteLine("0. Exit");
 
-            Console.Write("Enter your choice (1-5):");
+            Console.Write("Enter your choice (1-5): ");
             int choice = Convert.ToInt32(Console.ReadLine());
 
             switch (choice)
             {
                 case 1:
                     Console.Clear();
-                    PerformCalculation(1);
+                    previousGames.AddRange(PerformCalculation("+"));
                     break;
                 case 2:
-                    PerformCalculation(2);
+                    previousGames.AddRange(PerformCalculation("-"));
                     break;
                 case 3:
-                    PerformCalculation(3);
+                    previousGames.AddRange(PerformCalculation("*"));
                     break;
                 case 4:
-                    PerformCalculation(4);
+                    previousGames.AddRange(PerformCalculation("/"));
                     break;
                 case 5:
+                    DisplayHistory(previousGames);
+                    break;
+                case 0:
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Thank you for playing! Goodbye!\n");
                     Console.ResetColor();
@@ -46,36 +52,51 @@ class Program
         }
     }
 
-    static void PerformCalculation(int operation)
+    static void DisplayHistory(List<string> history)
+    {
+        Console.Clear();
+
+        foreach(string game in history)
+        {
+            Console.WriteLine(game);
+        }
+
+        Console.Write("Press any key to continue...");
+        Console.ReadLine();
+    }
+
+    static List<string> PerformCalculation(string operation)
     {
         Random random = new Random();
+        List<string> history = new List<string>();
 
         for (int i = 0; i < 5; i++)
         {
-            int num1, num2;
+            int num1 = 0;
+            int num2 = 0;
             int correctAnswer = 0;
 
             switch (operation)
             {
-                case 1:
+                case "+":
                     num1 = random.Next(1, 101);
                     num2 = random.Next(1, 101);
                     correctAnswer = num1 + num2;
                     Console.Write($"What is {num1} + {num2}?: ");
                     break;
-                case 2:
+                case "-":
                     num1 = random.Next(1, 101);
                     num2 = random.Next(1, 101);
                     correctAnswer = num1 - num2;
                     Console.Write($"What is {num1} - {num2}?: ");
                     break;
-                case 3:
+                case "*":
                     num1 = random.Next(1, 101);
                     num2 = random.Next(1, 101);
                     correctAnswer = num1 * num2;
                     Console.Write($"What is {num1} * {num2}?: ");
                     break;
-                case 4:
+                case "/":
                     do
                     {
                         num1 = random.Next(1, 101);
@@ -92,26 +113,18 @@ class Program
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Correct!\n");
-                Console.ResetColor();
             }
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"Incorrect. The correct answer is {correctAnswer}.\n");
-                Console.ResetColor();
             }
-        }
-    }
 
-    static bool checkDivisonResult(int num1, int num2)
-    {
-        if (num1 % num2 == 0)
-        {
-            return true;
-        } else
-        {
-            return false;
+            history.Add($"Operation: {num1} {operation} {num2} Correct Answer: {correctAnswer} Your Answer: {userAnswer}");
+
+            Console.ResetColor();
         }
 
+        return history;
     }
 }
